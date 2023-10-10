534e283af2990e59e745a054f452f63a9664a14b
Kevin Kz <k@oplabs.co>
2023-10-10 14:51:28 -0400

    Allow code reuse by extracting JsonTxBuilder to an abstract contract.

--- script/NestedSignFromJson.s.sol
+++ script/NestedSignFromJson.s.sol
@@ -1,24 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {HelloWorld} from "src/HelloWorld.sol";
+import {JsonTxBuilderBase} from "src/JsonTxBuilderBase.sol";
 import {NestedMultisigBuilder} from "@base-contracts/script/universal/NestedMultisigBuilder.sol";
 import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 import {console} from "forge-std/Console.sol";
 
-contract NestedSignFromJson is NestedMultisigBuilder {
-    string json;
-
-    function _loadJson(string memory _path) internal {
-        console.log("Reading transaction bundle %s", _path);
-        try vm.readFile(_path) returns (string memory data) {
-            json = data;
-        } catch {
-            console.log("Error: unable to transaction bundle.");
-            return;
-        }
-    }
+contract NestedSignFromJson is NestedMultisigBuilder, JsonTxBuilderBase {
 
     function signJson(string memory _path, address _signerSafe) public {
         _loadJson(_path);
@@ -36,26 +25,7 @@ contract NestedSignFromJson is NestedMultisigBuilder {
     }
 
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
-        // This is a hack.
-        uint transaction_count = 9999;
-        for(uint i = 0; transaction_count == 9999; i++) {
-            try vm.parseJsonAddress(json, string(abi.encodePacked("$.transactions[", vm.toString(i), "].to"))) returns (address) {
-            } catch {
-                transaction_count = i;
-            }
-        }
-
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](transaction_count);
-
-        for (uint i = 0; i < transaction_count; i++) {
-            calls[i] = IMulticall3.Call3({
-                target: stdJson.readAddress(json, string(abi.encodePacked("$.transactions[", vm.toString(i), "].to"))),
-                allowFailure: false,
-                callData: stdJson.readBytes(json, string(abi.encodePacked("$.transactions[", vm.toString(i), "].data")))
-                });
-        }
-
-        return calls;
+        return _buildCallsFromJson();
     }
 
     function _ownerSafe() internal view override returns (address) {
--- script/SignFromJson.s.sol
+++ script/SignFromJson.s.sol
@@ -1,49 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {HelloWorld} from "src/HelloWorld.sol";
+import {JsonTxBuilderBase} from "src/JsonTxBuilderBase.sol";
 import {MultisigBuilder} from "@base-contracts/script/universal/MultisigBuilder.sol";
 import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 import {console} from "forge-std/Console.sol";
 
-contract SignFromJson is MultisigBuilder {
-    string json;
+contract SignFromJson is MultisigBuilder, JsonTxBuilderBase {
 
     function signJson(string memory _path) public {
-        console.log("Reading transaction bundle %s", _path);
-        try vm.readFile(_path) returns (string memory data) {
-            json = data;
-        } catch {
-            console.log("Error: unable to transaction bundle.");
-            return;
-        }
-
+        _loadJson(_path);
         sign();
     }
 
     function runJson(string memory _path, bytes memory _signatures) public {
-        console.log("Reading transaction bundle %s", _path);
-        try vm.readFile(_path) returns (string memory data) {
-            json = data;
-        } catch {
-            console.log("Error: unable to transaction bundle.");
-            return;
-        }
-
+        _loadJson(_path);
         run(_signatures);
     }
 
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
-
-        calls[0] = IMulticall3.Call3({
-            target: stdJson.readAddress(json, "$.transactions[0].to"),
-            allowFailure: false,
-            callData: stdJson.readBytes(json, "$.transactions[0].data")
-        });
-
-        return calls;
+        return _buildCallsFromJson();
     }
 
     // todo: allow passing this as a script argument.
