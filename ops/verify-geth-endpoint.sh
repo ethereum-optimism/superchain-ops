8f7ed26cd47f77d6bccb33e511b5fea4fbd4cf5a
Tushar Shah <tushar@oplabs.co>
2023-11-14 18:07:44 -0700

    add block height checks

--- ops/verify-geth-endpoint.sh
+++ ops/verify-geth-endpoint.sh
@@ -6,14 +6,41 @@ set -euo pipefail
 URL=$1
 TIME_DIFF_THRESHOLD=$2
 
+get_current_block() {
+    data='{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+    # shellcheck disable=SC2086
+    response=$(curl -s -X POST -H "Content-Type: application/json" --data "$data" $URL)
+    hex=$(echo "$response" | jq -r '.result')
+    trimmed_hex=${hex#0x}
+    echo $((16#$trimmed_hex))
+}
+
 # Check that the node is synced
 data='{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}'
-response=$(curl -s -X POST -H "Content-Type: application/json" --data "$data" $URL)
-echo $response
-syncing=$(echo $response | jq -r '.result')
+response=$(curl -s -X POST -H "Content-Type: application/json" --data "$data" "$URL")
+syncing=$(echo "$response" | jq -r '.result')
 if [ "$syncing" != "false" ]; then
   echo "$URL is syncing."
   exit 1
 fi
+echo "Replica is not syncing."
+
+first_block=$(get_current_block)
+echo "First block: $first_block"
+
+if [ "$first_block" -eq 0 ]; then
+  echo "Block height is 0."
+  exit 1
+fi
+
+sleep 15
+
+second_block=$(get_current_block)
+echo "Second block: $second_block"
+if [ "$first_block" -ge "$second_block" ]; then
+  echo "Block height is not increasing."
+  exit 1
+fi
+
 
 echo "RPC endpoint at $URL is healthy."
