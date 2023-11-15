a0c278eaa2092937dc331f63bace6ae100385b1d
Tushar Shah <tushar@oplabs.co>
2023-11-14 18:10:55 -0700

    parent 0d7c3ca044a0bedee67155ef410cf549357728d6 author Tushar Shah <tushar@oplabs.co> 1700010655 -0700 committer Tushar Shah <tushar@oplabs.co> 1700070934 -0700
    parent 0d7c3ca044a0bedee67155ef410cf549357728d6
    author Tushar Shah <tushar@oplabs.co> 1700010655 -0700
    committer Tushar Shah <tushar@oplabs.co> 1700070343 -0700
    
    CircleCI jobs & workflow

--- ops/verify-geth-endpoint.sh
+++ ops/verify-geth-endpoint.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 # Script to check the syncing status and block timestamp of an Ethereum RPC endpoint
 
 URL=$1
-TIME_DIFF_THRESHOLD=$2
 
 get_current_block() {
     data='{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
     # shellcheck disable=SC2086
     response=$(curl -s -X POST -H "Content-Type: application/json" --data "$data" $URL)
+    echo "response: $response"
     hex=$(echo "$response" | jq -r '.result')
     trimmed_hex=${hex#0x}
     echo $((16#$trimmed_hex))
@@ -18,6 +18,7 @@ get_current_block() {
 # Check that the node is synced
 data='{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}'
 response=$(curl -s -X POST -H "Content-Type: application/json" --data "$data" "$URL")
+echo "response: $response"
 syncing=$(echo "$response" | jq -r '.result')
 if [ "$syncing" != "false" ]; then
   echo "$URL is syncing."
@@ -25,22 +26,12 @@ if [ "$syncing" != "false" ]; then
 fi
 echo "Replica is not syncing."
 
-first_block=$(get_current_block)
-echo "First block: $first_block"
+block=$(get_current_block)
+echo "block: $block"
 
-if [ "$first_block" -eq 0 ]; then
+if [ "$block" -eq 0 ]; then
   echo "Block height is 0."
   exit 1
 fi
-
-sleep 15
-
-second_block=$(get_current_block)
-echo "Second block: $second_block"
-if [ "$first_block" -ge "$second_block" ]; then
-  echo "Block height is not increasing."
-  exit 1
-fi
-
 
 echo "RPC endpoint at $URL is healthy."
