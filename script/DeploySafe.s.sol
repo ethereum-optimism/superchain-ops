ddec07b5bb5f86df5e7d6e7fa26e0215a51355d0
clabby <ben@clab.by>
2024-02-12 13:59:46 -0500

    Init goerli FPAC upgrade

--- script/DeploySafe.s.sol
+++ script/DeploySafe.s.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.0;
 import {stdJson} from "forge-std/StdJson.sol";
 import {console} from "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
-import {GnosisSafe} from "safe-contracts/GnosisSafe.sol";
-import {GnosisSafeProxyFactory} from "safe-contracts/proxies/GnosisSafeProxyFactory.sol";
+import {Safe} from "safe-contracts/Safe.sol";
+import {SafeProxyFactory} from "safe-contracts/proxies/SafeProxyFactory.sol";
 
 contract DeploySafe is Script {
     // These values are constant across chains
-    GnosisSafeProxyFactory immutable SAFE_PROXY_FACTORY =
-        GnosisSafeProxyFactory(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
-    GnosisSafe immutable SAFE_SINGLETON = GnosisSafe(payable(0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552));
+    SafeProxyFactory immutable SAFE_PROXY_FACTORY =
+        SafeProxyFactory(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
+    Safe immutable SAFE_SINGLETON = Safe(payable(0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552));
 
     function run(address[] calldata signers, uint256 threshold, string calldata name, string calldata dir) public {
         bytes memory initData = abi.encodeWithSelector(
-            GnosisSafe.setup.selector, signers, threshold, address(0), hex"", address(0), address(0), 0, address(0)
+            Safe.setup.selector, signers, threshold, address(0), hex"", address(0), address(0), 0, address(0)
         );
         address addr = address(
             SAFE_PROXY_FACTORY.createProxyWithNonce(
