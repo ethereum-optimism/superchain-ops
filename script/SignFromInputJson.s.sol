2d6c7bfbc4bef82205fb4ec3a135b744631e7745
Kevin Kz <k@oplabs.co>
2024-03-24 09:29:43 -0700

    Add just prepare

--- script/SignFromInputJson.s.sol
+++ script/SignFromInputJson.s.sol
@@ -7,10 +7,10 @@ import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 import {console} from "forge-std/console.sol";
 
-contract SignFromJson is MultisigBuilder, JsonTxBuilderBase {
+contract SignFromInputJson is MultisigBuilder, JsonTxBuilderBase {
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
-        _loadJson(vm.envOr("INPUT_JSON_PATH", "input.json"));
-        return _buildCallsFromJson();
+        string memory jsonContent = vm.readFile(vm.envOr("INPUT_JSON_PATH", string("input.json")));
+        return _buildCallsFromJson(jsonContent);
     }
 
     // todo: allow passing this as a script argument.
