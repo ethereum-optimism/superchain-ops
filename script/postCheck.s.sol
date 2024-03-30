32f3cb1cfb7e7dea8bb2d884b6010a219ca151cd
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-03-30 20:04:16 -0300

    tasks: update MCP L1 postCheck

---
+++ script/postCheck.s.sol
@@ -0,0 +1,335 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {SignFromJson} from "./SignFromJson.s.sol";
+import {ProxyAdmin} from "@eth-optimism-bedrock/src/universal/ProxyAdmin.sol";
+import {ResourceMetering} from "@eth-optimism-bedrock/src/L1/ResourceMetering.sol";
+import {DeployConfig} from "@eth-optimism-bedrock/scripts/DeployConfig.s.sol";
+import {Deployer} from "@eth-optimism-bedrock/scripts/Deployer.sol";
+import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
+import {Constants} from "@eth-optimism-bedrock/src/libraries/Constants.sol";
+import {L1StandardBridge} from "@eth-optimism-bedrock/src/L1/L1StandardBridge.sol";
+import {L2OutputOracle} from "@eth-optimism-bedrock/src/L1/L2OutputOracle.sol";
+import {ProtocolVersion, ProtocolVersions} from "@eth-optimism-bedrock/src/L1/ProtocolVersions.sol";
+import {SuperchainConfig} from "@eth-optimism-bedrock/src/L1/SuperchainConfig.sol";
+import {OptimismPortal} from "@eth-optimism-bedrock/src/L1/OptimismPortal.sol";
+import {L1CrossDomainMessenger} from "@eth-optimism-bedrock/src/L1/L1CrossDomainMessenger.sol";
+import {OptimismMintableERC20Factory} from "@eth-optimism-bedrock/src/universal/OptimismMintableERC20Factory.sol";
+import {L1ERC721Bridge} from "@eth-optimism-bedrock/src/L1/L1ERC721Bridge.sol";
+import {Predeploys} from "@eth-optimism-bedrock/src/libraries/Predeploys.sol";
+import {Types} from "@eth-optimism-bedrock/scripts/Types.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {ISystemConfigV0} from "@eth-optimism-bedrock/scripts/interfaces/ISystemConfigV0.sol";
+import {console2 as console} from "forge-std/console2.sol";
+
+contract PostCheck is SignFromJson {
+    Types.ContractSet prox = Types.ContractSet({
+        L1CrossDomainMessenger: 0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1,
+        L1StandardBridge: 0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1,
+        L2OutputOracle: 0xdfe97868233d1aa22e815a266982f2cf17685a27,
+        DisputeGameFactory: address(0),
+        OptimismMintableERC20Factory: 0x75505a97BD334E7BD3C476893285569C4136Fa0F,
+        OptimismPortal: 0xbEb5Fc579115071764c7423A4f12eDde41f106Ed,
+        OptimismPortal2: 0xbEb5Fc579115071764c7423A4f12eDde41f106Ed,
+        SystemConfig: 0x229047fed2591dbec1eF1118d64F7aF3dB9EB290,
+        L1ERC721Bridge: 0x5a7749f83b81B301cAb5f48EB8516B986DAef23D,
+        ProtocolVersions: 0x8062AbC286f5e7D9428a0Ccb9AbD71e50d93b935,
+        SuperchainConfig: 0x95703e0982140D16f8ebA6d158FccEde42f04a4C
+    });
+    DeployConfig cfg = new DeployConfig();
+    uint256 l2OutputOracleStartingTimestamp = 0;
+
+    constructor() {
+        // Load the DeployConfig from the JSON file
+        cfg.read(string.concat(vm.projectRoot(), "/deploy-config/DeployConfigMainnet.json"));
+    }
+
+    function _postCheck() internal view override {
+        postDeployAssertions(prox, cfg, l2OutputOracleStartingTimestamp, vm);
+    }
+
+    /// @notice Asserts the correctness of an L1 deployment. This function expects that all contracts
+    ///         within the `prox` ContractSet are proxies that have been setup and initialized.
+    function postDeployAssertions(
+        Types.ContractSet memory _prox,
+        DeployConfig _cfg,
+        uint256 _l2OutputOracleStartingTimestamp,
+        Vm _vm
+    ) internal view {
+        console.log("Running post-deploy assertions");
+        ResourceMetering.ResourceConfig memory rcfg;
+        rcfg.maxResourceLimit = SystemConfig(_prox.SystemConfig).resourceConfig().maxResourceLimit;
+        rcfg.elasticityMultiplier = SystemConfig(_prox.SystemConfig).resourceConfig().elasticityMultiplier;
+        rcfg.baseFeeMaxChangeDenominator = SystemConfig(_prox.SystemConfig).resourceConfig().baseFeeMaxChangeDenominator;
+        rcfg.minimumBaseFee = SystemConfig(_prox.SystemConfig).resourceConfig().minimumBaseFee;
+        rcfg.systemTxMaxGas = SystemConfig(_prox.SystemConfig).resourceConfig().systemTxMaxGas;
+        rcfg.maximumBaseFee = SystemConfig(_prox.SystemConfig).resourceConfig().maximumBaseFee;
+        ResourceMetering.ResourceConfig memory dflt;
+        dflt.maxResourceLimit = Constants.DEFAULT_RESOURCE_CONFIG().maxResourceLimit;
+        dflt.elasticityMultiplier = Constants.DEFAULT_RESOURCE_CONFIG().elasticityMultiplier;
+        dflt.baseFeeMaxChangeDenominator = Constants.DEFAULT_RESOURCE_CONFIG().baseFeeMaxChangeDenominator;
+        dflt.minimumBaseFee = Constants.DEFAULT_RESOURCE_CONFIG().minimumBaseFee;
+        dflt.systemTxMaxGas = Constants.DEFAULT_RESOURCE_CONFIG().systemTxMaxGas;
+        dflt.maximumBaseFee = Constants.DEFAULT_RESOURCE_CONFIG().maximumBaseFee;
+        require(keccak256(abi.encode(rcfg)) == keccak256(abi.encode(dflt)));
+
+        checkSystemConfig({_contracts: _prox, _cfg: _cfg, _isProxy: true});
+        checkL1CrossDomainMessenger({_contracts: _prox, _vm: _vm, _isProxy: true});
+        checkL1StandardBridge({_contracts: _prox, _isProxy: true});
+        checkL2OutputOracle({
+            _contracts: _prox,
+            _cfg: _cfg,
+            _l2OutputOracleStartingTimestamp: _l2OutputOracleStartingTimestamp,
+            _isProxy: true
+        });
+        checkOptimismMintableERC20Factory({_contracts: _prox, _isProxy: true});
+        checkL1ERC721Bridge({_contracts: _prox, _isProxy: true});
+        checkOptimismPortal({_contracts: _prox, _cfg: _cfg, _isProxy: true});
+        checkProtocolVersions({_contracts: _prox, _cfg: _cfg, _isProxy: true});
+    }
+
+    /// @notice Asserts that the SystemConfig is setup correctly
+    function checkSystemConfig(Types.ContractSet memory _contracts, DeployConfig _cfg, bool _isProxy) internal view {
+        console.log("Running chain assertions on the SystemConfig");
+        SystemConfig config = SystemConfig(_contracts.SystemConfig);
+
+        ResourceMetering.ResourceConfig memory resourceConfig;
+        resourceConfig.maxResourceLimit = config.resourceConfig().maxResourceLimit;
+        resourceConfig.elasticityMultiplier = config.resourceConfig().elasticityMultiplier;
+        resourceConfig.baseFeeMaxChangeDenominator = config.resourceConfig().baseFeeMaxChangeDenominator;
+        resourceConfig.minimumBaseFee = config.resourceConfig().minimumBaseFee;
+        resourceConfig.systemTxMaxGas = config.resourceConfig().systemTxMaxGas;
+        resourceConfig.maximumBaseFee = config.resourceConfig().maximumBaseFee;
+
+        if (_isProxy) {
+            require(config.owner() == _cfg.finalSystemOwner());
+            require(config.overhead() == _cfg.gasPriceOracleOverhead());
+            require(config.scalar() == _cfg.gasPriceOracleScalar());
+            require(config.batcherHash() == bytes32(uint256(uint160(_cfg.batchSenderAddress()))));
+            require(config.gasLimit() == uint64(_cfg.l2GenesisBlockGasLimit()));
+            require(config.unsafeBlockSigner() == _cfg.p2pSequencerAddress());
+            // Check _config
+            ResourceMetering.ResourceConfig memory rconfig;
+            rconfig.maxResourceLimit = Constants.DEFAULT_RESOURCE_CONFIG().maxResourceLimit;
+            rconfig.elasticityMultiplier = Constants.DEFAULT_RESOURCE_CONFIG().elasticityMultiplier;
+            rconfig.baseFeeMaxChangeDenominator = Constants.DEFAULT_RESOURCE_CONFIG().baseFeeMaxChangeDenominator;
+            rconfig.minimumBaseFee = Constants.DEFAULT_RESOURCE_CONFIG().minimumBaseFee;
+            rconfig.systemTxMaxGas = Constants.DEFAULT_RESOURCE_CONFIG().systemTxMaxGas;
+            rconfig.maximumBaseFee = Constants.DEFAULT_RESOURCE_CONFIG().maximumBaseFee;
+
+            require(resourceConfig.maxResourceLimit == rconfig.maxResourceLimit);
+            require(resourceConfig.elasticityMultiplier == rconfig.elasticityMultiplier);
+            require(resourceConfig.baseFeeMaxChangeDenominator == rconfig.baseFeeMaxChangeDenominator);
+            require(resourceConfig.systemTxMaxGas == rconfig.systemTxMaxGas);
+            require(resourceConfig.minimumBaseFee == rconfig.minimumBaseFee);
+            require(resourceConfig.maximumBaseFee == rconfig.maximumBaseFee);
+            // Depends on start block being set to 0 in `initialize`
+            uint256 cfgStartBlock = _cfg.systemConfigStartBlock();
+            require(config.startBlock() == (cfgStartBlock == 0 ? block.number : cfgStartBlock));
+            require(config.batchInbox() == _cfg.batchInboxAddress());
+            // Check _addresses
+            require(config.l1CrossDomainMessenger() == _contracts.L1CrossDomainMessenger);
+            require(config.l1ERC721Bridge() == _contracts.L1ERC721Bridge);
+            require(config.l1StandardBridge() == _contracts.L1StandardBridge);
+            require(config.l2OutputOracle() == _contracts.L2OutputOracle);
+            require(config.optimismPortal() == _contracts.OptimismPortal);
+            require(config.optimismMintableERC20Factory() == _contracts.OptimismMintableERC20Factory);
+        } else {
+            require(config.owner() == address(0xdead));
+            require(config.overhead() == 0);
+            require(config.scalar() == 0);
+            require(config.batcherHash() == bytes32(0));
+            require(config.gasLimit() == 1);
+            require(config.unsafeBlockSigner() == address(0));
+            // Check _config
+            require(resourceConfig.maxResourceLimit == 1);
+            require(resourceConfig.elasticityMultiplier == 1);
+            require(resourceConfig.baseFeeMaxChangeDenominator == 2);
+            require(resourceConfig.systemTxMaxGas == 0);
+            require(resourceConfig.minimumBaseFee == 0);
+            require(resourceConfig.maximumBaseFee == 0);
+            // Check _addresses
+            require(config.startBlock() == type(uint256).max);
+            require(config.batchInbox() == address(0));
+            require(config.l1CrossDomainMessenger() == address(0));
+            require(config.l1ERC721Bridge() == address(0));
+            require(config.l1StandardBridge() == address(0));
+            require(config.l2OutputOracle() == address(0));
+            require(config.optimismPortal() == address(0));
+            require(config.optimismMintableERC20Factory() == address(0));
+        }
+    }
+
+    /// @notice Asserts that the L1CrossDomainMessenger is setup correctly
+    function checkL1CrossDomainMessenger(Types.ContractSet memory _contracts, Vm _vm, bool _isProxy) internal view {
+        console.log("Running chain assertions on the L1CrossDomainMessenger");
+        L1CrossDomainMessenger messenger = L1CrossDomainMessenger(_contracts.L1CrossDomainMessenger);
+
+        require(address(messenger.OTHER_MESSENGER()) == Predeploys.L2_CROSS_DOMAIN_MESSENGER);
+        require(address(messenger.otherMessenger()) == Predeploys.L2_CROSS_DOMAIN_MESSENGER);
+
+        if (_isProxy) {
+            require(address(messenger.PORTAL()) == _contracts.OptimismPortal);
+            require(address(messenger.portal()) == _contracts.OptimismPortal);
+            require(address(messenger.superchainConfig()) == _contracts.SuperchainConfig);
+            bytes32 xdmSenderSlot = _vm.load(address(messenger), bytes32(uint256(204)));
+            require(address(uint160(uint256(xdmSenderSlot))) == Constants.DEFAULT_L2_SENDER);
+        } else {
+            require(address(messenger.PORTAL()) == address(0));
+            require(address(messenger.portal()) == address(0));
+            require(address(messenger.superchainConfig()) == address(0));
+        }
+    }
+
+    /// @notice Asserts that the L1StandardBridge is setup correctly
+    function checkL1StandardBridge(Types.ContractSet memory _contracts, bool _isProxy) internal view {
+        console.log("Running chain assertions on the L1StandardBridge");
+        L1StandardBridge bridge = L1StandardBridge(payable(_contracts.L1StandardBridge));
+
+        if (_isProxy) {
+            require(address(bridge.MESSENGER()) == _contracts.L1CrossDomainMessenger);
+            require(address(bridge.messenger()) == _contracts.L1CrossDomainMessenger);
+            require(address(bridge.OTHER_BRIDGE()) == Predeploys.L2_STANDARD_BRIDGE);
+            require(address(bridge.otherBridge()) == Predeploys.L2_STANDARD_BRIDGE);
+            require(address(bridge.superchainConfig()) == _contracts.SuperchainConfig);
+        } else {
+            require(address(bridge.MESSENGER()) == address(0));
+            require(address(bridge.messenger()) == address(0));
+            require(address(bridge.OTHER_BRIDGE()) == Predeploys.L2_STANDARD_BRIDGE);
+            require(address(bridge.otherBridge()) == Predeploys.L2_STANDARD_BRIDGE);
+            require(address(bridge.superchainConfig()) == address(0));
+        }
+    }
+
+    /// @notice Asserts that the L2OutputOracle is setup correctly
+    function checkL2OutputOracle(
+        Types.ContractSet memory _contracts,
+        DeployConfig _cfg,
+        uint256 _l2OutputOracleStartingTimestamp,
+        bool _isProxy
+    ) internal view {
+        console.log("Running chain assertions on the L2OutputOracle");
+        L2OutputOracle oracle = L2OutputOracle(_contracts.L2OutputOracle);
+
+        if (_isProxy) {
+            require(oracle.SUBMISSION_INTERVAL() == _cfg.l2OutputOracleSubmissionInterval());
+            require(oracle.submissionInterval() == _cfg.l2OutputOracleSubmissionInterval());
+            require(oracle.L2_BLOCK_TIME() == _cfg.l2BlockTime());
+            require(oracle.l2BlockTime() == _cfg.l2BlockTime());
+            require(oracle.PROPOSER() == _cfg.l2OutputOracleProposer());
+            require(oracle.proposer() == _cfg.l2OutputOracleProposer());
+            require(oracle.CHALLENGER() == _cfg.l2OutputOracleChallenger());
+            require(oracle.challenger() == _cfg.l2OutputOracleChallenger());
+            require(oracle.FINALIZATION_PERIOD_SECONDS() == _cfg.finalizationPeriodSeconds());
+            require(oracle.finalizationPeriodSeconds() == _cfg.finalizationPeriodSeconds());
+            require(oracle.startingBlockNumber() == _cfg.l2OutputOracleStartingBlockNumber());
+            require(oracle.startingTimestamp() == _l2OutputOracleStartingTimestamp);
+        } else {
+            require(oracle.SUBMISSION_INTERVAL() == 1);
+            require(oracle.submissionInterval() == 1);
+            require(oracle.L2_BLOCK_TIME() == 1);
+            require(oracle.l2BlockTime() == 1);
+            require(oracle.PROPOSER() == address(0));
+            require(oracle.proposer() == address(0));
+            require(oracle.CHALLENGER() == address(0));
+            require(oracle.challenger() == address(0));
+            require(oracle.FINALIZATION_PERIOD_SECONDS() == 0);
+            require(oracle.finalizationPeriodSeconds() == 0);
+            require(oracle.startingBlockNumber() == 0);
+            require(oracle.startingTimestamp() == 0);
+        }
+    }
+
+    /// @notice Asserts that the OptimismMintableERC20Factory is setup correctly
+    function checkOptimismMintableERC20Factory(Types.ContractSet memory _contracts, bool _isProxy) internal view {
+        console.log("Running chain assertions on the OptimismMintableERC20Factory");
+        OptimismMintableERC20Factory factory = OptimismMintableERC20Factory(_contracts.OptimismMintableERC20Factory);
+
+        if (_isProxy) {
+            require(factory.BRIDGE() == _contracts.L1StandardBridge);
+            require(factory.bridge() == _contracts.L1StandardBridge);
+        } else {
+            require(factory.BRIDGE() == address(0));
+            require(factory.bridge() == address(0));
+        }
+    }
+
+    /// @notice Asserts that the L1ERC721Bridge is setup correctly
+    function checkL1ERC721Bridge(Types.ContractSet memory _contracts, bool _isProxy) internal view {
+        console.log("Running chain assertions on the L1ERC721Bridge");
+        L1ERC721Bridge bridge = L1ERC721Bridge(_contracts.L1ERC721Bridge);
+
+        require(address(bridge.OTHER_BRIDGE()) == Predeploys.L2_ERC721_BRIDGE);
+        require(address(bridge.otherBridge()) == Predeploys.L2_ERC721_BRIDGE);
+
+        if (_isProxy) {
+            require(address(bridge.MESSENGER()) == _contracts.L1CrossDomainMessenger);
+            require(address(bridge.messenger()) == _contracts.L1CrossDomainMessenger);
+            require(address(bridge.superchainConfig()) == _contracts.SuperchainConfig);
+        } else {
+            require(address(bridge.MESSENGER()) == address(0));
+            require(address(bridge.messenger()) == address(0));
+            require(address(bridge.superchainConfig()) == address(0));
+        }
+    }
+
+    /// @notice Asserts the OptimismPortal is setup correctly
+    function checkOptimismPortal(Types.ContractSet memory _contracts, DeployConfig _cfg, bool _isProxy) internal view {
+        console.log("Running chain assertions on the OptimismPortal");
+
+        OptimismPortal portal = OptimismPortal(payable(_contracts.OptimismPortal));
+
+        address guardian = _cfg.superchainConfigGuardian();
+        if (guardian.code.length == 0) {
+            console.log("Guardian has no code: %s", guardian);
+        }
+
+        if (_isProxy) {
+            require(address(portal.L2_ORACLE()) == _contracts.L2OutputOracle);
+            require(address(portal.l2Oracle()) == _contracts.L2OutputOracle);
+            require(address(portal.SYSTEM_CONFIG()) == _contracts.SystemConfig);
+            require(address(portal.systemConfig()) == _contracts.SystemConfig);
+            require(portal.GUARDIAN() == guardian);
+            require(portal.guardian() == guardian);
+            require(address(portal.superchainConfig()) == address(_contracts.SuperchainConfig));
+            require(portal.paused() == SuperchainConfig(_contracts.SuperchainConfig).paused());
+            require(portal.l2Sender() == Constants.DEFAULT_L2_SENDER);
+        } else {
+            require(address(portal.L2_ORACLE()) == address(0));
+            require(address(portal.l2Oracle()) == address(0));
+            require(address(portal.SYSTEM_CONFIG()) == address(0));
+            require(address(portal.systemConfig()) == address(0));
+            require(address(portal.superchainConfig()) == address(0));
+            require(portal.l2Sender() == Constants.DEFAULT_L2_SENDER);
+        }
+    }
+
+    /// @notice Asserts that the ProtocolVersions is setup correctly
+    function checkProtocolVersions(Types.ContractSet memory _contracts, DeployConfig _cfg, bool _isProxy)
+        internal
+        view
+    {
+        console.log("Running chain assertions on the ProtocolVersions");
+        ProtocolVersions versions = ProtocolVersions(_contracts.ProtocolVersions);
+        if (_isProxy) {
+            require(versions.owner() == _cfg.finalSystemOwner());
+            require(ProtocolVersion.unwrap(versions.required()) == _cfg.requiredProtocolVersion());
+            require(ProtocolVersion.unwrap(versions.recommended()) == _cfg.recommendedProtocolVersion());
+        } else {
+            require(versions.owner() == address(0xdead));
+            require(ProtocolVersion.unwrap(versions.required()) == 0);
+            require(ProtocolVersion.unwrap(versions.recommended()) == 0);
+        }
+    }
+
+    /// @notice Asserts that the SuperchainConfig is setup correctly
+    function checkSuperchainConfig(Types.ContractSet memory _contracts, DeployConfig _cfg, bool _isPaused)
+        internal
+        view
+    {
+        console.log("Running chain assertions on the SuperchainConfig");
+        SuperchainConfig superchainConfig = SuperchainConfig(_contracts.SuperchainConfig);
+        require(superchainConfig.guardian() == _cfg.superchainConfigGuardian());
+        require(superchainConfig.paused() == _isPaused);
+    }
+}
--- tasks/eth/006-MCP-L1/postCheck.s.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
-
-import {SignFromJson} from "../../../script/SignFromJson.s.sol";
-
-contract PostCheck is SignFromJson {
-    function _postCheck() internal view override {}
-}
