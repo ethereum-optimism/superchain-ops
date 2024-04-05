815a5c42b99497a0de537aa28a7a658198926ba7
Matt Solomon <matt@mattsolomon.dev>
2024-04-05 15:08:16 -0700

    feat: run simulation of tx in foundry

--- script/NestedSignFromJson.s.sol
+++ script/NestedSignFromJson.s.sol
@@ -8,25 +8,30 @@ import {stdJson} from "forge-std/StdJson.sol";
 import {console} from "forge-std/console.sol";
 
 contract NestedSignFromJson is NestedMultisigBuilder, JsonTxBuilderBase {
+    address globalSignerSafe; // Hack to avoid passing signerSafe as an input to many functions.
+
     /// @dev Signs the approveHash transaction from the Nested Safe to the System Owner Safe.
     function signJson(string memory _path, address _signerSafe) public {
         _loadJson(_path);
         sign(_signerSafe);
-        _postCheck();
+        globalSignerSafe = _signerSafe;
+        _postCheckWithSim();
     }
 
     /// @dev Submits signatures to call approveHash on the System Owner Safe.
     function approveJson(string memory _path, address _signerSafe, bytes memory _signatures) public {
         _loadJson(_path);
         approve(_signerSafe, _signatures);
-        _postCheck();
+        globalSignerSafe = _signerSafe;
+        _postCheckWithSim();
     }
 
     /// @dev Executes the transaction from the System Owner Safe.
     function runJson(string memory _path) public {
         _loadJson(_path);
         run();
-        _postCheck();
+        // globalSignerSafe = _signerSafe; // TODO how to handle this one?
+        // _postCheckWithSim();
     }
 
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
@@ -38,4 +43,9 @@ contract NestedSignFromJson is NestedMultisigBuilder, JsonTxBuilderBase {
     }
 
     function _postCheck() internal view virtual override {}
+
+    // A thorough postCheck would apply the needed state overrides and run the simulation, then
+    // execute those assertions against the resulting state. Using `vm.store` changes state therefore
+    // the postCheck methods cannot be `view`, which is why we have this alternate version.
+    function _postCheckWithSim() internal virtual {}
 }
--- tasks/eth/006-MCP-L1/NestedSignFromJson.s.sol
+++ tasks/eth/006-MCP-L1/NestedSignFromJson.s.sol
@@ -12,11 +12,15 @@ import {OptimismPortal} from "@eth-optimism-bedrock/src/L1/OptimismPortal.sol";
 import {L1CrossDomainMessenger} from "@eth-optimism-bedrock/src/L1/L1CrossDomainMessenger.sol";
 import {OptimismMintableERC20Factory} from "@eth-optimism-bedrock/src/universal/OptimismMintableERC20Factory.sol";
 import {L1ERC721Bridge} from "@eth-optimism-bedrock/src/L1/L1ERC721Bridge.sol";
+import {AddressManager} from "@eth-optimism-bedrock/src/legacy/AddressManager.sol";
 import {Predeploys} from "@eth-optimism-bedrock/src/libraries/Predeploys.sol";
 import {Types} from "@eth-optimism-bedrock/scripts/Types.sol";
 import {EIP1967Helper} from "@eth-optimism-bedrock/test/mocks/EIP1967Helper.sol";
+import {IGnosisSafe, Enum} from "@eth-optimism-bedrock/scripts/interfaces/IGnosisSafe.sol";
 import {console2 as console} from "forge-std/console2.sol";
 import {stdJson} from "forge-std/StdJson.sol";
+import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
+import {Vm, VmSafe} from "forge-std/Vm.sol";
 
 contract NestedSignFromJson is OriginalNestedSignFromJson {
     /// @notice Verify against https://docs.optimism.io/chain/security/privileged-roles#system-config-owner
@@ -70,11 +74,11 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
     /// @notice Verify against https://github.com/ethereum-optimism/optimism/blob/e2307008d8bc3f125f97814243cc72e8b47c117e/packages/contracts-bedrock/deploy-config/mainnet.json#L44
     uint256 constant systemConfigStartBlock = 17422444;
 
-    /// @notice Verify against https://github.com/ethereum-optimism/optimism/blob/e2307008d8bc3f125f97814243cc72e8b47c117e/packages/contracts-bedrock/deploy-config/mainnet.json#L45
-    uint256 constant requiredProtocolVersion = 0x0000000000000000000000000000000000000003000000010000000000000000;
+    /// @notice Verify against https://github.com/ethereum-optimism/superchain-ops/tree/b350daad1f969c7371acdfd4b3785545304baf99/tasks/eth/005-protocol-versions-ecotone
+    uint256 constant requiredProtocolVersion = 0x0000000000000000000000000000000000000006000000000000000000000000;
 
-    /// @notice Verify against https://github.com/ethereum-optimism/optimism/blob/e2307008d8bc3f125f97814243cc72e8b47c117e/packages/contracts-bedrock/deploy-config/mainnet.json#L46
-    uint256 constant recommendedProtocolVersion = 0x0000000000000000000000000000000000000003000000010000000000000000;
+    /// @notice Verify against https://github.com/ethereum-optimism/superchain-ops/tree/b350daad1f969c7371acdfd4b3785545304baf99/tasks/eth/005-protocol-versions-ecotone
+    uint256 constant recommendedProtocolVersion = 0x0000000000000000000000000000000000000006000000000000000000000000;
 
     /// @notice Verify against https://github.com/ethereum-optimism/optimism/blob/e2307008d8bc3f125f97814243cc72e8b47c117e/packages/contracts-bedrock/snapshots/storageLayout/L1CrossDomainMessenger.json#L93-L99
     uint256 constant xdmSenderSlotNumber = 204;
@@ -122,8 +126,12 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
 
         // Check _addresses
         require(configToCheck.l1CrossDomainMessenger() == proxies.L1CrossDomainMessenger, "1700");
-        require(configToCheck.l1CrossDomainMessenger().code.length != 0, "1701");
-        require(EIP1967Helper.getImplementation(configToCheck.l1CrossDomainMessenger()).code.length != 0, "1702");
+        require(configToCheck.l1CrossDomainMessenger().code.length != 0, "1740");
+
+        // L1CrossDomainMessenger is a ResolvedDelegateProxy and that has no getters, so we hardcode some info needed here.
+        AddressManager addressManager = AddressManager(0xdE1FCfB0851916CA5101820A69b13a4E276bd81F); // https://github.com/ethereum-optimism/superchain-registry/blob/4c005f16ee1b100afc08a35a2e418d849bea044a/superchain/extra/addresses/mainnet/op.json#L2
+        address l1xdmImplementation = addressManager.getAddress("OVM_L1CrossDomainMessenger");
+        require(l1xdmImplementation.code.length != 0, "1750");
 
         require(configToCheck.l1ERC721Bridge() == proxies.L1ERC721Bridge, "1800");
         require(configToCheck.l1ERC721Bridge().code.length != 0, "1801");
@@ -152,14 +160,10 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
 
         require(proxies.L1CrossDomainMessenger.code.length != 0, "2300");
 
-        require(EIP1967Helper.getImplementation(proxies.L1CrossDomainMessenger).code.length != 0, "2301");
-
         L1CrossDomainMessenger messengerToCheck = L1CrossDomainMessenger(proxies.L1CrossDomainMessenger);
 
         require(address(messengerToCheck.OTHER_MESSENGER()) == Predeploys.L2_CROSS_DOMAIN_MESSENGER, "2400");
         require(address(messengerToCheck.otherMessenger()) == Predeploys.L2_CROSS_DOMAIN_MESSENGER, "2500");
-        require(address(messengerToCheck.otherMessenger()).code.length != 0, "2501");
-        require(EIP1967Helper.getImplementation(address(messengerToCheck.otherMessenger())).code.length != 0, "2502");
 
         require(address(messengerToCheck.PORTAL()) == proxies.OptimismPortal, "2600");
         require(address(messengerToCheck.portal()) == proxies.OptimismPortal, "2700");
@@ -187,12 +191,10 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
         require(address(bridgeToCheck.MESSENGER()) == proxies.L1CrossDomainMessenger, "3000");
         require(address(bridgeToCheck.messenger()) == proxies.L1CrossDomainMessenger, "3100");
         require(address(bridgeToCheck.messenger()).code.length != 0, "3101");
-        require(EIP1967Helper.getImplementation(address(bridgeToCheck.messenger())).code.length != 0, "3102");
+        // This messenger is the L1CrossDomainMessenger and we've already checked it's implementation for code above.
 
         require(address(bridgeToCheck.OTHER_BRIDGE()) == Predeploys.L2_STANDARD_BRIDGE, "3200");
         require(address(bridgeToCheck.otherBridge()) == Predeploys.L2_STANDARD_BRIDGE, "3300");
-        require(address(bridgeToCheck.otherBridge()).code.length != 0, "3302");
-        require(EIP1967Helper.getImplementation(address(bridgeToCheck.otherBridge())).code.length != 0, "3203");
 
         require(address(bridgeToCheck.superchainConfig()) == proxies.SuperchainConfig, "3400");
         require(address(bridgeToCheck.superchainConfig()).code.length != 0, "3401");
@@ -256,13 +258,10 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
 
         require(address(bridgeToCheck.OTHER_BRIDGE()) == Predeploys.L2_ERC721_BRIDGE, "5200");
         require(address(bridgeToCheck.otherBridge()) == Predeploys.L2_ERC721_BRIDGE, "5300");
-        require(address(bridgeToCheck.otherBridge()).code.length != 0, "5301");
-        require(EIP1967Helper.getImplementation(address(bridgeToCheck.otherBridge())).code.length != 0, "5302");
 
         require(address(bridgeToCheck.MESSENGER()) == proxies.L1CrossDomainMessenger, "5400");
         require(address(bridgeToCheck.messenger()) == proxies.L1CrossDomainMessenger, "5500");
         require(address(bridgeToCheck.messenger()).code.length != 0, "5502");
-        require(EIP1967Helper.getImplementation(address(bridgeToCheck.messenger())).code.length != 0, "5503");
 
         require(address(bridgeToCheck.superchainConfig()) == proxies.SuperchainConfig, "5600");
         require(address(bridgeToCheck.superchainConfig()).code.length != 0, "5601");
@@ -291,8 +290,7 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
 
         require(portalToCheck.GUARDIAN() == superchainConfigGuardian, "6200");
         require(portalToCheck.guardian() == superchainConfigGuardian, "6300");
-        require(portalToCheck.guardian().code.length != 0, "6301");
-        require(EIP1967Helper.getImplementation(portalToCheck.guardian()).code.length != 0, "6302");
+        require(portalToCheck.guardian().code.length != 0, "6350"); // This is a Safe, no need to check the implementation.
 
         require(address(portalToCheck.superchainConfig()) == address(proxies.SuperchainConfig), "6400");
         require(address(portalToCheck.superchainConfig()).code.length != 0, "6401");
@@ -308,15 +306,11 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
         console.log("Running chain assertions on the ProtocolVersions");
 
         require(proxies.ProtocolVersions.code.length != 0, "6700");
-
         require(EIP1967Helper.getImplementation(proxies.ProtocolVersions).code.length != 0, "6701");
 
         ProtocolVersions protocolVersionsToCheck = ProtocolVersions(proxies.ProtocolVersions);
-
         require(protocolVersionsToCheck.owner() == superchainConfigGuardian, "6800");
-
         require(ProtocolVersion.unwrap(protocolVersionsToCheck.required()) == requiredProtocolVersion, "6900");
-
         require(ProtocolVersion.unwrap(protocolVersionsToCheck.recommended()) == recommendedProtocolVersion, "7000");
     }
 
@@ -325,22 +319,24 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
         console.log("Running chain assertions on the SuperchainConfig");
 
         require(proxies.SuperchainConfig.code.length != 0, "7100");
-
         require(EIP1967Helper.getImplementation(proxies.SuperchainConfig).code.length != 0, "7101");
 
         SuperchainConfig superchainConfigToCheck = SuperchainConfig(proxies.SuperchainConfig);
 
         require(superchainConfigToCheck.guardian() == superchainConfigGuardian, "7200");
-        require(superchainConfigToCheck.guardian().code.length != 0, "7201");
-        require(EIP1967Helper.getImplementation(superchainConfigToCheck.guardian()).code.length != 0, "7202");
-
+        require(superchainConfigToCheck.guardian().code.length != 0, "7250");
         require(superchainConfigToCheck.paused() == false, "7300");
     }
 
     /// @notice Checks the correctness of the deployment
-    function _postCheck() internal view override {
-        console.log("Running post-deploy assertions");
+    function _postCheckWithSim() internal override {
+        console.log("Running the simulation locally");
+        vm.startStateDiffRecording();
+        runSim();
+        Vm.AccountAccess[] memory accountAccesses = vm.stopAndReturnStateDiff();
 
+        console.log("Running post-deploy assertions");
+        checkStateDiff(accountAccesses);
         checkSystemConfig();
         checkL1CrossDomainMessenger();
         checkL1StandardBridge();
@@ -350,6 +346,60 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
         checkOptimismPortal();
         checkProtocolVersions();
         checkSuperchainConfig();
+        console.log("All assertions passed!");
+    }
+
+    function checkStateDiff(Vm.AccountAccess[] memory accountAccesses) internal view {
+        require(accountAccesses.length > 0, "No account accesses");
+
+        for (uint256 i; i < accountAccesses.length; i++) {
+            Vm.AccountAccess memory accountAccess = accountAccesses[i];
+            require(
+                accountAccess.account.code.length != 0,
+                string.concat("Account has no code: ", vm.toString(accountAccess.account))
+            );
+            require(
+                accountAccess.oldBalance == accountAccess.account.balance,
+                string.concat("Unexpected balance change: ", vm.toString(accountAccess.account))
+            );
+            require(
+                accountAccess.kind != VmSafe.AccountAccessKind.SelfDestruct,
+                string.concat("Self-destructed account: ", vm.toString(accountAccess.account))
+            );
+
+            for (uint256 j; j < accountAccess.storageAccesses.length; j++) {
+                Vm.StorageAccess memory storageAccess = accountAccess.storageAccesses[j];
+                uint256 value = uint256(storageAccess.newValue);
+
+                // If a value looks like an address and is not an L2 predeploy, check if it has code.
+                bool isLikelyAddress = value <= type(uint160).max
+                    && value >= uint160(0x00000000fFFFffffffFfFfFFffFfFffFFFfFffff)
+                    && value < uint256(uint160(0x4200000000000000000000000000000000000000))
+                    && value > uint256(uint160(0x420000000000000000000000000000000000FffF));
+                if (isLikelyAddress) {
+                    require(
+                        address(uint160(value)).code.length != 0,
+                        string.concat("Address in storage has no code: ", vm.toString(value))
+                    );
+                }
+                require(
+                    storageAccess.account.code.length != 0,
+                    string.concat("Storage account has no code: ", vm.toString(storageAccess.account))
+                );
+                require(
+                    !storageAccess.reverted,
+                    string.concat("Storage access reverted: ", vm.toString(storageAccess.account))
+                );
+                require(
+                    storageAccess.account.code.length != 0,
+                    string.concat("Storage account has no code: ", vm.toString(storageAccess.account))
+                );
+                require(
+                    !storageAccess.reverted,
+                    string.concat("Storage access reverted: ", vm.toString(storageAccess.account))
+                );
+            }
+        }
     }
 
     /// @notice Reads the contract addresses from lib/superchain-registry/superchain/extra/addresses/mainnet/op.json
@@ -386,5 +436,82 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
 
         _proxies.ProtocolVersions = stdJson.readAddress(addressesJson, "$.protocol_versions_addr");
         _proxies.SuperchainConfig = stdJson.readAddress(addressesJson, "$.superchain_config_addr");
+    }
+
+    function runSim() internal {
+        // The logic for computing state overrides and calldata was copied from
+        // `NestedMultisigBuilder._simulateForSigner`. Then we add the nested `for` loop to
+        // `vm.store` the state overrides and the `vm.prank` section to simulate the execution.
+        require(globalSignerSafe != address(0), "Signer safe not set");
+        address _safe = _ownerSafe();
+        IGnosisSafe safe = IGnosisSafe(payable(_safe));
+        IGnosisSafe signerSafe_ = IGnosisSafe(payable(globalSignerSafe));
+
+        // Apply state overrides.
+        SimulationStateOverride[] memory stateOverrides = new SimulationStateOverride[](2);
+        stateOverrides[0] = overrideSafeThreshold(_safe);
+        stateOverrides[1] = overrideSafeThresholdAndOwner(globalSignerSafe, address(multicall));
+
+        for (uint256 i; i < stateOverrides.length; i++) {
+            SimulationStateOverride memory stateOverride = stateOverrides[i];
+            SimulationStorageOverride[] memory storageOverrides = stateOverride.overrides;
+            for (uint256 j; j < storageOverrides.length; j++) {
+                SimulationStorageOverride memory storageOverride = storageOverrides[j];
+                vm.store(stateOverride.contractAddress, storageOverride.key, storageOverride.value);
+            }
+        }
+
+        // Build the call.
+        IMulticall3.Call3[] memory nestedCalls = _buildCalls();
+        bytes memory data = abi.encodeCall(IMulticall3.aggregate3, (nestedCalls));
+        bytes32 hash = _getTransactionHash(_safe, data);
+
+        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](2);
+        bytes memory approveHashData = abi.encodeCall(IMulticall3.aggregate3, (toArray(
+            IMulticall3.Call3({
+                target: _safe,
+                allowFailure: false,
+                callData: abi.encodeCall(safe.approveHash, (hash))
+            })
+        )));
+        bytes memory approveHashExec = abi.encodeCall(
+            signerSafe_.execTransaction,
+            (
+                address(multicall),
+                0,
+                approveHashData,
+                Enum.Operation.DelegateCall,
+                0,
+                0,
+                0,
+                address(0),
+                payable(address(0)),
+                prevalidatedSignature(address(multicall))
+            )
+        );
+        calls[0] = IMulticall3.Call3({target: globalSignerSafe, allowFailure: false, callData: approveHashExec});
+
+        // simulate the final state changes tx, so that signer can verify the final results
+        bytes memory finalExec = abi.encodeCall(
+            safe.execTransaction,
+            (
+                address(multicall),
+                0,
+                data,
+                Enum.Operation.DelegateCall,
+                0,
+                0,
+                0,
+                address(0),
+                payable(address(0)),
+                prevalidatedSignature(globalSignerSafe)
+            )
+        );
+        calls[1] = IMulticall3.Call3({target: _safe, allowFailure: false, callData: finalExec});
+
+        bytes memory finalData = abi.encodeCall(IMulticall3.aggregate3, (calls));
+        vm.prank(msg.sender);
+        (bool ok, bytes memory returnData) = address(multicall).call(finalData);
+        require(ok, string.concat("Foundry simulation failed: ", vm.toString(returnData)));
     }
 }
