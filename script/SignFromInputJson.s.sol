a89222be6475b916e7b9c160e79d8ace4f6ceb83
Maurelian <maurelian@protonmail.ch>
2024-04-29 21:04:34 -0400

    Add Presigned pause specific checks to SignFromInputJson
    Add overrides for SIMULATE_WITHOUT_LEDGER sender
    
    Add note about compiler warning

--- script/SignFromInputJson.s.sol
+++ script/SignFromInputJson.s.sol
@@ -8,7 +8,19 @@ import {stdJson} from "forge-std/StdJson.sol";
 import {console} from "forge-std/console.sol";
 import {Vm} from "forge-std/Vm.sol";
 
+/// @title SignFromInputJson
+/// @notice A script that reads a JSON file and builds a series of transactions from it. This script is
+///     intended to be used only in the presigned pause runbooks.
 contract SignFromInputJson is MultisigBuilder, JsonTxBuilderBase {
+    function _addGenericOverrides() internal view override returns (SimulationStateOverride memory override_) {
+        // If SIMULATE_WITHOUT_LEDGER is set, we add an override to allow the script to run using the same
+        // test address as defined in presigned-pause.just
+        if (vm.envOr("SIMULATE_WITHOUT_LEDGER", false) || vm.envOr("SIMULATE_WITHOUT_LEDGER", uint256(0)) == 1) {
+            console.log("Adding override for test sender");
+            override_ = overrideSafeThresholdAndOwner(_ownerSafe(), vm.envAddress("TEST_SENDER"));
+        }
+    }
+
     function _buildCalls() internal view override returns (IMulticall3.Call3[] memory) {
         string memory jsonContent = vm.readFile(vm.envOr("INPUT_JSON_PATH", string("input.json")));
         return _buildCallsFromJson(jsonContent);
@@ -19,14 +31,43 @@ contract SignFromInputJson is MultisigBuilder, JsonTxBuilderBase {
         return vm.envAddress("OWNER_SAFE");
     }
 
+    /// @notice This function is called after the simulation of the transactions is done.
+    ///     It checks that the transactions only write to the nonce of the OWNER_SAFE contract and the paused slot of
+    ///     the SuperchainConfig contract.
     function _postCheck(Vm.AccountAccess[] memory accesses, SimulationPayload memory simPayload)
         internal
         view
         virtual
         override
     {
-        accesses; // Silences compiler warnings.
-        simPayload;
-        console.log("\x1b[1;33mWARNING:\x1b[0m _postCheck not implemented");
+        for (uint256 i; i < accesses.length; i++) {
+            Vm.AccountAccess memory accountAccess = accesses[i];
+
+            for (uint256 j; j < accountAccess.storageAccesses.length; j++) {
+                Vm.StorageAccess memory storageAccess = accountAccess.storageAccesses[j];
+
+                if (storageAccess.isWrite) {
+                    console.log("Checking Owner Safe storage writes");
+                    if (storageAccess.account == vm.envAddress("OWNER_SAFE")) {
+                        require(storageAccess.slot == bytes32(uint256(5)), "The only allowed write is to the nonce");
+                        require(
+                            uint256(storageAccess.previousValue) == uint256(storageAccess.newValue) - 1,
+                            "The nonce should have increased by 1"
+                        );
+                    } else if (storageAccess.account == vm.envAddress("SUPERCHAIN_CONFIG_ADDR")) {
+                        console.log("Checking SuperchainConfig storage writes");
+                        require(
+                            storageAccess.slot
+                                == bytes32(0x54176ff9944c4784e5857ec4e5ef560a462c483bf534eda43f91bb01a470b1b6),
+                            "The only allowed write is to paused slot."
+                        );
+                        require(uint256(storageAccess.newValue) == 1, "The contract should be paused.");
+                    } else {
+                        revert("No other storage writes are allowed");
+                    }
+                }
+            }
+        }
+        simPayload; // Silences unused variable warning.
     }
 }
