0f0fb53c0dfb01b1326579b67a7d73ba909bbe92
Maurelian <maurelian@protonmail.ch>
2024-06-03 16:12:26 -0400

    Add overrides using changes in base-contracts

--- tasks/eth/008-presigned-pause/PresignPauseFromJson.s.sol
+++ tasks/eth/008-presigned-pause/PresignPauseFromJson.s.sol
@@ -7,8 +7,20 @@ import {console} from "forge-std/console.sol";
 contract PresignPauseFromJson is OriginalPresignPauseFromJson {
     address guardianSafe = vm.envAddress("GUARDIAN_SAFE_ADDR");
 
+    /// @notice Adds the Guardian Safe and DeputyGuardianModule to the simulation state.
+    function _addMultipleGenericOverrides()
+        internal
+        view
+        override
+        returns (SimulationStateOverride[] memory overrides_)
+    {
+        overrides_ = new SimulationStateOverride[](2);
+        overrides_[0] = _addSuperchainConfigOverrides();
+        overrides_[1] = _addGuardianSafeOverrides();
+    }
+
     /// @notice Sets the Guardian on the SuperchainConfig to the Security Council
-    function _addGenericOverrides2() internal view override returns (SimulationStateOverride memory override_) {
+    function _addSuperchainConfigOverrides() internal view returns (SimulationStateOverride memory override_) {
         bytes32 guardianSlot = bytes32(uint256(keccak256("superchainConfig.guardian")) - 1);
 
         override_.contractAddress = vm.envAddress("SUPERCHAIN_CONFIG_ADDR");
@@ -18,7 +30,7 @@ contract PresignPauseFromJson is OriginalPresignPauseFromJson {
     }
 
     /// @notice Inserts the DeputyGuardianModule into the Guardian Safe's modules list
-    function _addGenericOverrides3() internal view override returns (SimulationStateOverride memory override_) {
+    function _addGuardianSafeOverrides() internal view returns (SimulationStateOverride memory override_) {
         address deputyGuardianModule = vm.envAddress("DEPUTY_GUARDIAN_MODULE_ADDR");
         override_.contractAddress = guardianSafe;
         override_.overrides = new SimulationStorageOverride[](2);
@@ -26,8 +38,8 @@ contract PresignPauseFromJson is OriginalPresignPauseFromJson {
         // This is `modules[0x1]`, so the key can be derived from
         // `cast index address 0x0000000000000000000000000000000000000001 1`.
         override_.overrides[0] = SimulationStorageOverride({
-            key: 0xcc69885fda6bcc1a4ace058b4a62bf5e179ea78fd58a1ccd71c22cc9b688792f,
-            value: bytes32(uint256(uint160(dgm)))
+            key: keccak256(abi.encode(bytes32(uint256(1)), bytes32(uint256(1)))),
+            value: bytes32(uint256(uint160(deputyGuardianModule)))
         });
 
         // Ensure the DeputyGuardianModule is pointing to the sentinel module.
