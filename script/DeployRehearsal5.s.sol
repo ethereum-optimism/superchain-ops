2804242dd670126ee2b10bf3e160790f2a369aff
Maurelian <maurelian@protonmail.ch>
2024-06-20 23:26:49 -0400

    Full deploy script

--- script/DeployRehearsal5.s.sol
+++ script/DeployRehearsal5.s.sol
@@ -3,16 +3,25 @@ pragma solidity ^0.8.0;
 
 import {GnosisSafe} from "safe-contracts-v1.3.0/GnosisSafe.sol";
 import {GnosisSafeProxyFactory} from "safe-contracts-v1.3.0/proxies/GnosisSafeProxyFactory.sol";
+import {ModuleManager} from "safe-contracts-v1.3.0/base/ModuleManager.sol";
+import {Enum} from "safe-contracts-v1.3.0/common/Enum.sol";
 
 import {SuperchainConfig} from "@eth-optimism-bedrock/src/L1/SuperchainConfig.sol";
 // import {DeputyGuardianModule} from "@eth-optimism-bedrock/src/Safe/DeputyGuardianModule.sol";
+
+import {ProxyAdmin} from "@eth-optimism-bedrock/src/universal/ProxyAdmin.sol";
+import {Proxy} from "@eth-optimism-bedrock/src/universal/Proxy.sol";
+import {EIP1967Helper} from "@eth-optimism-bedrock/test/mocks/EIP1967Helper.sol";
 
 import {Script} from "forge-std/Script.sol";
 
 import {console2 as console} from "forge-std/console2.sol";
 
 contract DeployRehearsal5 is Script {
-    GnosisSafe council_safe;
+    GnosisSafe councilSafe;
+    GnosisSafe guardianSafe;
+    SuperchainConfig superchainConfigProxy;
+    address dummyDeputyGuardianModule = 0xAbCD1234ABCD1234AbCD1234ABcD1234ABCD1234;
 
     /// @notice The name of the script, used to ensure the right deploy artifacts
     ///         are used.
@@ -21,7 +30,7 @@ contract DeployRehearsal5 is Script {
     }
 
     function setUp() public {
-        council_safe = GnosisSafe(payable(vm.envAddress("COUNCIL_SAFE")));
+        councilSafe = GnosisSafe(payable(vm.envAddress("COUNCIL_SAFE")));
 
         console.log("Deploying from %s", name());
         console.log("Deployment context: %s", vm.envOr("DEPLOYMENT_CONTEXT", string("Deployment context not set")));
@@ -29,15 +38,13 @@ contract DeployRehearsal5 is Script {
 
     function run() public {
         console.log("Deploying contracts for rehearsal");
-
+        vm.startBroadcast();
         deployGuardianSafe();
-        deploySuperchainConfig();
-        deployDeputyGuardianModule();
+        deploySuperchainConfigProxy();
+        vm.stopBroadcast();
     }
 
     function deployGuardianSafe() public {
-        bytes32 salt = keccak256(abi.encode("rehearsal-5", block.number));
-
         address[] memory owners = new address[](1);
         owners[0] = msg.sender;
 
@@ -48,11 +55,37 @@ contract DeployRehearsal5 is Script {
         // These are the standard create2 deployed contracts. They should be available on any network we are working on.
         GnosisSafeProxyFactory safeProxyFactory = GnosisSafeProxyFactory(0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2);
         GnosisSafe safeSingleton = GnosisSafe(payable(0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552));
-        address safe = address(safeProxyFactory.createProxyWithNonce(address(safeSingleton), initData, uint256(salt)));
+        uint256 salt = uint256(keccak256(abi.encode("rehearsal-5", block.number)));
+        guardianSafe =
+            GnosisSafe(payable(address(safeProxyFactory.createProxyWithNonce(address(safeSingleton), initData, salt))));
 
-        console.log("New Guardian Safe deployed at %s", address(safe));
+        // This is the signature format used the caller is also the signer.
+        bytes memory signature = abi.encodePacked(uint256(uint160(msg.sender)), bytes32(0), uint8(1));
+        bytes memory data = abi.encodeCall(ModuleManager.enableModule, (dummyDeputyGuardianModule));
+        guardianSafe.execTransaction({
+            to: address(guardianSafe),
+            value: 0,
+            data: data,
+            operation: Enum.Operation.Call,
+            safeTxGas: 0,
+            baseGas: 0,
+            gasPrice: 0,
+            gasToken: address(0),
+            refundReceiver: payable(address(0)),
+            signatures: signature
+        });
+
+        console.log("New Guardian Safe deployed at %s", address(guardianSafe));
     }
 
-    function deploySuperchainConfig() public {}
-    function deployDeputyGuardianModule() public {}
+    function deploySuperchainConfigProxy() public {
+        Proxy proxy = new Proxy({_admin: msg.sender});
+        address impl = address(new SuperchainConfig());
+        proxy.upgradeToAndCall({
+            _implementation: impl,
+            _data: abi.encodeCall(SuperchainConfig.initialize, (address(guardianSafe), true))
+        });
+        superchainConfigProxy = SuperchainConfig(address(proxy));
+        console.log("New SuperchainConfig Proxy deployed at %s", address(proxy));
+    }
 }
