5a62fe451329829f0f26f44790e135748a7eb177
soyboy <85043086+sbvegan@users.noreply.github.com>
2024-07-26 07:33:10 -0600

    adding mmz sepolia key handover task (#271)
    * adding mmz sepolia key handover task
    
    * adding task to ci/cd
    
    * update status
    
    * Adding pre and post checks.
    
    * Avoid hardcoding addresses.
    
    * Update tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
    
    Co-authored-by: Blaine Malone <blainemalone01@gmail.com>
    
    * Update tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
    
    Co-authored-by: Blaine Malone <blainemalone01@gmail.com>
    
    * Update tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
    
    Co-authored-by: Blaine Malone <blainemalone01@gmail.com>
    
    * Update tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
    
    Co-authored-by: Blaine Malone <blainemalone01@gmail.com>
    
    * Update tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
    
    Co-authored-by: Blaine Malone <blainemalone01@gmail.com>
    
    * finishing description
    
    * Update tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
    
    Co-authored-by: Maurelian <maurelian@protonmail.ch>
    
    * adding missing bracket
    
    * addressing review
    
    * Apply suggestions from code review
    
    Co-authored-by: Maurelian <maurelian@protonmail.ch>
    
    ---------
    
    Co-authored-by: Kevin Kz <k@oplabs.co>
    Co-authored-by: Kevin Z Chen <kevin.zchn@gmail.com>
    Co-authored-by: Blaine Malone <blainemalone01@gmail.com>
    Co-authored-by: Maurelian <maurelian@protonmail.ch>

---
+++ tasks/sep/mmz-002-key-handover/README.md
@@ -0,0 +1,27 @@
+# Mode, Metal, and Zora Sepolia's Key Handover Upgrade
+
+Status: READY TO SIGN
+
+## Objective
+
+This is the playbook for executing the Key Handover upgrade on Mode, Metal, and Zora Sepolia.
+This updates the `ProxyAdminOwner` to the same ProxyAdmin owner multisig account as OP Sepolia account.
+
+## Simulation
+
+Please see the "Simulating and Verifying the Transaction" instructions in [SINGLE.md](../../../SINGLE.md).
+When simulating, ensure the logs say `Using script Using script /your/path/to/superchain-ops/tasks/<NETWORK_DIR>/<RUNBOOK_DIR>/SignFromJson.s.sol`.
+Thus ensures all safety checks are run. If the default `SignFromJson.s.sol` script is shown
+(without the full path), something is wrong and the safety checks will not run.
+
+### State Validations
+
+Please see the instructions for [validation](./VALIDATION.md).
+
+### Execution
+
+At this point you may resume following the execution instructions in the "Execute the Transaction" section of [SINGLE.md](../../../SINGLE.md).
+
+When executing, ensure the logs say `Using script Using script /your/path/to/superchain-ops/tasks/<NETWORK_DIR>/<RUNBOOK_DIR>/SignFromJson.s.sol`.
+Thus ensures all safety checks are run. If the default `SignFromJson.s.sol` script is shown 
+(without the full path), something is wrong and the safety checks will not run.
---
+++ tasks/sep/mmz-002-key-handover/SignFromJson.s.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {SignFromJson as OriginalSignFromJson} from "script/SignFromJson.s.sol";
+import {ProxyAdmin} from "@eth-optimism-bedrock/src/universal/ProxyAdmin.sol";
+import {console2 as console} from "forge-std/console2.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {Vm, VmSafe} from "forge-std/Vm.sol";
+
+contract SignFromJson is OriginalSignFromJson {
+
+    ProxyAdmin opProxyAdmin;
+    ProxyAdmin metalProxyAdmin;
+    ProxyAdmin modeProxyAdmin;
+    ProxyAdmin zoraProxyAdmin;
+
+    /// @notice Sets up the contract
+    function setUp() public {
+        opProxyAdmin = ProxyAdmin(readProxyAdminAddress("11155420"));
+        metalProxyAdmin = ProxyAdmin(readProxyAdminAddress("1740"));
+        modeProxyAdmin = ProxyAdmin(readProxyAdminAddress("919"));
+        zoraProxyAdmin = ProxyAdmin(readProxyAdminAddress("999999999"));
+        require(opProxyAdmin.owner() != metalProxyAdmin.owner());
+        require(opProxyAdmin.owner() != modeProxyAdmin.owner());
+        require(opProxyAdmin.owner() != zoraProxyAdmin.owner());
+    }
+
+
+    /// @notice Checks the correctness of the deployment
+    function _postCheck(Vm.AccountAccess[] memory /* accesses */, SimulationPayload memory /* simPayload */ )
+        internal
+        view
+        override
+    {
+        console.log("Running post-deploy assertions");
+        require(opProxyAdmin.owner() == metalProxyAdmin.owner());
+        require(opProxyAdmin.owner() == modeProxyAdmin.owner());
+        require(opProxyAdmin.owner() == zoraProxyAdmin.owner());
+        console.log("All assertions passed!");
+    }
+
+    function readProxyAdminAddress(string memory chainId) internal view returns (address) {
+        string memory addressesJson;
+
+        // Read addresses json
+        string memory path = "/lib/superchain-registry/superchain/extra/addresses/addresses.json";
+
+        try vm.readFile(string.concat(vm.projectRoot(), path)) returns (string memory data) {
+            addressesJson = data;
+        } catch {
+            revert(string.concat("Failed to read ", path));
+        }
+
+        return stdJson.readAddress(addressesJson, string.concat("$.", chainId, ".ProxyAdmin"));
+    }
+    
+    function checkStateDiff(Vm.AccountAccess[] memory accountAccesses) internal view override {
+        super.checkStateDiff(accountAccesses);
+
+        address metalProxyAdminOwner = metalProxyAdmin.owner();
+        address modeProxyAdminOwner = modeProxyAdmin.owner();
+        address zoraProxyAdminOwner = zoraProxyAdmin.owner();
+        address opProxyAdminOwner = 0xE75Cd021F520B160BF6b54D472Fa15e52aFe5aDD;
+
+        for (uint256 i; i < accountAccesses.length; i++) {
+            Vm.AccountAccess memory accountAccess = accountAccesses[i];
+
+            // Assert that only the expected accounts have been written to.
+            for (uint256 j; j < accountAccess.storageAccesses.length; j++) {
+                Vm.StorageAccess memory storageAccess = accountAccess.storageAccesses[j];
+                if (storageAccess.isWrite) {
+                    address account = storageAccess.account;
+                    require(
+                        // We set the guardian slot on the Superchain Config.
+                        account == metalProxyAdminOwner || account == modeProxyAdminOwner
+                            || account == zoraProxyAdminOwner
+                            || account == opProxyAdminOwner,
+                        "state-100"
+                    );
+                }
+            }
+        }
+    }
+}
