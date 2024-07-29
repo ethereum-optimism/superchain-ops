32bac7db07fe8cb30225814d597bb018e362fcbb
Maurelian <maurelian@protonmail.ch>
2024-07-29 14:32:02 -0400

    Rename DeployHelloWorld to DeployRehearsal1

--- script/DeployRehearsal1.s.sol
+++ script/DeployRehearsal1.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {Script} from "forge-std/Script.sol";
 import {HelloWorld} from "../src/HelloWorld.sol";
 
-contract DeployHelloWorld is Script {
+contract DeployRehearsal1 is Script {
     function run(address auth) public returns (HelloWorld helloWorld) {
         // Deploy HelloWorld.sol
         vm.startBroadcast();
