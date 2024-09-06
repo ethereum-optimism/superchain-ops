ddc2754c0f39dbcf52404a4661a8f7c3a33670bb
ethnical <ethnicalnightmare@gmail.com>
2024-09-06 20:29:09 +0200

    move and rename the eth_017 script from script by the `presignPauseFromJson` into the task directory.

--- tasks/eth/017-presigned-pause/PresignPauseFromJson.s.sol
+++ tasks/eth/017-presigned-pause/PresignPauseFromJson.s.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {PresignPauseFromJson as OriginalPresignPauseFromJson} from "./PresignPauseFromJson.s.sol";
+import {PresignPauseFromJson as OriginalPresignPauseFromJson} from "script/PresignPauseFromJson.s.sol";
 import {console} from "forge-std/console.sol";
 
-/// @title PresignPauseFromJson_eth_017
-/// @notice This script is intended for one time use, and can be deleted after the tasks/eth/017-presign-pause runbook is completed.
-contract PresignPauseFromJson_eth_017 is OriginalPresignPauseFromJson {
+/// @title PresignPauseFromJson for the task 017
+contract PresignPauseFromJson is OriginalPresignPauseFromJson {
     address guardianSafe = vm.envAddress("GUARDIAN_SAFE_ADDR");
 
     /// @notice Adds the new DeputyGuardianModule to the guardianSafe to the simulation state.
@@ -21,21 +20,34 @@ contract PresignPauseFromJson_eth_017 is OriginalPresignPauseFromJson {
     }
 
     /// @notice Inserts the DeputyGuardianModule into the Guardian Safe's modules list
-    function _addGuardianSafeOverrides() internal view returns (SimulationStateOverride memory override_) {
-        address deputyGuardianModule = vm.envAddress("DEPUTY_GUARDIAN_MODULE_ADDR");
+    function _addGuardianSafeOverrides()
+        internal
+        view
+        returns (SimulationStateOverride memory override_)
+    {
+        address deputyGuardianModule = vm.envAddress(
+            "DEPUTY_GUARDIAN_MODULE_ADDR"
+        );
         override_.contractAddress = guardianSafe;
         override_.overrides = new SimulationStorageOverride[](2);
         // Ensure the sentinel module (`address(0x01)`) is pointing to the `DeputyGuardianModule`
         // This is `modules[0x1]`, so the key can be derived from
         // `cast index address 0x0000000000000000000000000000000000000001 1`.
         override_.overrides[0] = SimulationStorageOverride({
-            key: keccak256(abi.encode(bytes32(uint256(1)), bytes32(uint256(1)))),
+            key: keccak256(
+                abi.encode(bytes32(uint256(1)), bytes32(uint256(1)))
+            ),
             value: bytes32(uint256(uint160(deputyGuardianModule)))
         });
 
         // Ensure the DeputyGuardianModule is pointing to the sentinel module.
         override_.overrides[1] = SimulationStorageOverride({
-            key: keccak256(abi.encode(bytes32(uint256(uint160(deputyGuardianModule))), bytes32(uint256(1)))),
+            key: keccak256(
+                abi.encode(
+                    bytes32(uint256(uint160(deputyGuardianModule))),
+                    bytes32(uint256(1))
+                )
+            ),
             value: bytes32(uint256(1))
         });
     }
