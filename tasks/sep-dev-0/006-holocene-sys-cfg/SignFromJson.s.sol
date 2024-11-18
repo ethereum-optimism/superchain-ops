5aae447b81633dc59e70c5b1c42655ef8cbea56b
clabby <ben@clab.by>
2024-11-18 11:22:55 -0500

    mds review

--- tasks/sep-dev-0/006-holocene-sys-cfg/SignFromJson.s.sol
+++ tasks/sep-dev-0/006-holocene-sys-cfg/SignFromJson.s.sol
@@ -19,7 +19,6 @@ contract NestedSignFromJson is OriginalSignFromJson {
 
     // Safe contract for this task.
     address immutable proxyAdminOwnerSafe = vm.envAddress("OWNER_SAFE");
-    address immutable livenessGuard = 0x24424336F04440b1c28685a38303aC33C9D14a25;
 
     address proxyAdmin;
     SystemConfig systemConfig;
@@ -44,18 +43,20 @@ contract NestedSignFromJson is OriginalSignFromJson {
             revert(string.concat("Failed to read ", path));
         }
 
-        proxyAdmin = stdJson.readAddress(inputJson, "$.transactions[9].to");
-        systemConfigImpl = stdJson.readAddress(inputJson, "$.transactions[9].contractInputsValues._implementation");
-        systemConfig = SystemConfig(stdJson.readAddress(inputJson, "$.transactions[9].contractInputsValues._proxy"));
+        string memory l2ChainId = "11155421";
 
-        l1XDMProxy = stdJson.readAddress(inputJson, "$.transactions[1].contractInputsValues._addr");
-        l1ERC721BridgeProxy = stdJson.readAddress(inputJson, "$.transactions[2].contractInputsValues._addr");
-        l1StandardBridgeProxy = stdJson.readAddress(inputJson, "$.transactions[3].contractInputsValues._addr");
-        optimismPortalProxy = stdJson.readAddress(inputJson, "$.transactions[4].contractInputsValues._addr");
-        optimismMintableERC20FactoryProxy = stdJson.readAddress(inputJson, "$.transactions[5].contractInputsValues._addr");
+        proxyAdmin = readContractAddress("ProxyAdmin", l2ChainId);
+        systemConfigImpl = stdJson.readAddress(inputJson, "$.transactions[9].contractInputsValues._implementation");
+        systemConfig = SystemConfig(readContractAddress("SystemConfigProxy", l2ChainId));
+
+        l1XDMProxy = readContractAddress("L1CrossDomainMessengerProxy", l2ChainId);
+        l1ERC721BridgeProxy = readContractAddress("L1ERC721BridgeProxy", l2ChainId);
+        l1StandardBridgeProxy = readContractAddress("L1StandardBridgeProxy", l2ChainId);
+        optimismPortalProxy = readContractAddress("OptimismPortalProxy", l2ChainId);
+        optimismMintableERC20FactoryProxy = readContractAddress("OptimismMintableERC20FactoryProxy", l2ChainId);
         batchInbox = stdJson.readAddress(inputJson, "$.transactions[6].contractInputsValues._addr");
         startBlock = stdJson.readUint(inputJson, "$.transactions[7].contractInputsValues._block");
-        dgfProxy = stdJson.readAddress(inputJson, "$.transactions[8].contractInputsValues._addr");
+        dgfProxy = readContractAddress("DisputeGameFactoryProxy", l2ChainId);
     }
 
     function getCodeExceptions() internal view override returns (address[] memory exceptions) {
@@ -64,10 +65,9 @@ contract NestedSignFromJson is OriginalSignFromJson {
     }
 
     function getAllowedStorageAccess() internal view override returns (address[] memory allowed) {
-        allowed = new address[](3);
+        allowed = new address[](2);
         allowed[0] = address(systemConfig);
         allowed[1] = proxyAdminOwnerSafe;
-        allowed[2] = livenessGuard;
     }
 
     function _postCheck(Vm.AccountAccess[] memory accesses, Simulation.Payload memory) internal override {
@@ -84,13 +84,28 @@ contract NestedSignFromJson is OriginalSignFromJson {
         address impl = Proxy(payable(address(systemConfig))).implementation();
         require(impl == systemConfigImpl, "sc-100");
 
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.L1_CROSS_DOMAIN_MESSENGER_SLOT())))) == l1XDMProxy, "sc-200");
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.L1_ERC_721_BRIDGE_SLOT())))) == l1ERC721BridgeProxy, "sc-300");
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.L1_STANDARD_BRIDGE_SLOT())))) == l1StandardBridgeProxy, "sc-400");
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.OPTIMISM_PORTAL_SLOT())))) == optimismPortalProxy, "sc-500");
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.OPTIMISM_MINTABLE_ERC20_FACTORY_SLOT())))) == optimismMintableERC20FactoryProxy, "sc-600");
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.BATCH_INBOX_SLOT())))) == batchInbox, "sc-700");
-        require(uint256(vm.load(address(systemConfig), systemConfig.START_BLOCK_SLOT())) == startBlock, "sc-800");
-        require(address(uint160(uint256(vm.load(address(systemConfig), systemConfig.DISPUTE_GAME_FACTORY_SLOT())))) == dgfProxy, "sc-900");
+        require(systemConfig.l1CrossDomainMessenger() == l1XDMProxy, "sc-200");
+        require(systemConfig.l1ERC721Bridge() == l1ERC721BridgeProxy, "sc-300");
+        require(systemConfig.l1StandardBridge() == l1StandardBridgeProxy, "sc-400");
+        require(systemConfig.optimismPortal() == optimismPortalProxy, "sc-500");
+        require(systemConfig.optimismMintableERC20Factory() == optimismMintableERC20FactoryProxy, "sc-600");
+        require(systemConfig.batchInbox() == batchInbox, "sc-700");
+        require(systemConfig.startBlock() == startBlock, "sc-800");
+        require(systemConfig.disputeGameFactory() == dgfProxy, "sc-900");
+    }
+
+    function readContractAddress(string memory contractName, string memory chainId) internal view returns (address) {
+        string memory addressesJson;
+
+        // Read addresses json
+        string memory path = "/lib/superchain-registry/superchain/extra/addresses/addresses.json";
+
+        try vm.readFile(string.concat(vm.projectRoot(), path)) returns (string memory data) {
+            addressesJson = data;
+        } catch {
+            revert(string.concat("Failed to read ", path));
+        }
+
+        return stdJson.readAddress(addressesJson, string.concat("$.", chainId, ".", contractName));
     }
 }
