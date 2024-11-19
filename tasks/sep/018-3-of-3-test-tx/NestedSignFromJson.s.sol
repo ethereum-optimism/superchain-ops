21976334431aa0e4022d0d6176845f864c1e7460
Blaine Malone <blainemalone01@gmail.com>
2024-11-19 11:15:10 -0500

    fix: initial commit.

---
+++ tasks/sep/018-3-of-3-test-tx/NestedSignFromJson.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {NestedSignFromJson as OriginalNestedSignFromJson} from "script/NestedSignFromJson.s.sol";
+import {GnosisSafe} from "safe-contracts/GnosisSafe.sol";
+import {Vm, VmSafe} from "forge-std/Vm.sol";
+import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
+import {console2 as console} from "forge-std/console2.sol";
+
+contract NestedSignFromJson is OriginalNestedSignFromJson {
+
+    // Chains for this task.
+    string constant l1ChainName = "sepolia";
+
+    GnosisSafe securityCouncilSafe = GnosisSafe(payable(vm.envAddress("COUNCIL_SAFE")));
+    GnosisSafe foundationSafe = GnosisSafe(payable(vm.envAddress("FOUNDATION_SAFE")));
+    GnosisSafe chainGovernorSafe = GnosisSafe(payable(vm.envAddress("CHAIN_GOVERNOR_SAFE")));
+    GnosisSafe ownerSafe = GnosisSafe(payable(vm.envAddress("OWNER_SAFE")));
+
+    /// @notice Sets up the contract
+    function setUp() public {
+    }
+
+    function getAllowedStorageAccess() internal pure override returns (address[] memory allowed) {
+        allowed = new address[](0);
+    }
+
+    /// @notice Checks the correctness of the deployment
+    function _postCheck(Vm.AccountAccess[] memory accesses, Simulation.Payload memory /* simPayload */ )
+        internal
+        view
+        override
+    {
+        console.log("Running post-deploy assertions");
+
+        checkStateDiff(accesses);
+
+        console.log("All assertions passed!");
+    }
+
+
+}
---
+++ tasks/sep/018-3-of-3-test-tx/README.md
@@ -0,0 +1,19 @@
+# Sepolia 3-of-3 Multisig Test Transaction
+
+Status: [DRAFT]()
+
+## Objective
+
+
+## Simulation
+
+Please see the "Simulating and Verifying the Transaction" instructions in [NESTED.md](../../../NESTED.md).
+When simulating, ensure the logs say `Using script /your/path/to/superchain-ops/tasks/sep/018-3-of-3-test-tx/NestedSignFromJson.s.sol`.
+This ensures all safety checks are run. If the default `NestedSignFromJson.s.sol` script is shown (without the full path), something is wrong and the safety checks will not run.
+
+## State Validation
+
+Please see the instructions for [validation](./VALIDATION.md).
+
+## Execution
+
