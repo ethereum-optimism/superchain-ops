a911d8dd6190c210dda647eb787092e73dc505ba
Blaine Malone <blainemalone01@gmail.com>
2024-11-19 13:00:41 -0500

    fix: evm version needs updated to work with 1.4.1 GnosisSafe.

--- tasks/sep/018-3-of-3-test-tx/NestedSignFromJson.s.sol
+++ tasks/sep/018-3-of-3-test-tx/NestedSignFromJson.s.sol
@@ -16,17 +16,19 @@ contract NestedSignFromJson is OriginalNestedSignFromJson {
     GnosisSafe foundationSafe = GnosisSafe(payable(vm.envAddress("FOUNDATION_SAFE")));
     GnosisSafe chainGovernorSafe = GnosisSafe(payable(vm.envAddress("CHAIN_GOVERNOR_SAFE")));
     GnosisSafe ownerSafe = GnosisSafe(payable(vm.envAddress("OWNER_SAFE")));
+    address simpleStorageSetter = address(0x3cc081914Be1bc73BF12c7Ff88dd37f40D324c43);
 
     /// @notice Sets up the contract
     function setUp() public {
     }
 
     function getAllowedStorageAccess() internal view override returns (address[] memory allowed) {
-        allowed = new address[](4);
+        allowed = new address[](5);
         allowed[0] = address(chainGovernorSafe);
         allowed[1] = address(ownerSafe);
         allowed[2] = address(securityCouncilSafe);
         allowed[3] = address(foundationSafe);
+        allowed[4] = address(simpleStorageSetter);
     }
 
     function getCodeExceptions() internal pure override returns (address[] memory) {
