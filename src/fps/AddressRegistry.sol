1f6d99781b9777c39564670a7aaa48c89297fd5f
prateek <prateekgupta105@gmail.com>
2024-12-12 18:48:31 +0530

    use l2 instance chain id in registry mapping

--- src/fps/AddressRegistry.sol
+++ src/fps/AddressRegistry.sol
@@ -37,150 +37,143 @@ contract AddressRegistry is IAddressRegistry, Test {
     /// @dev Structure for reading chain list details from toml file
     struct Superchain {
         uint256 chainId;
-        string identifier;
         string name;
     }
 
-    /// @notice Maps an identifier and chain ID to a stored address entry.
+    /// @notice Maps an identifier and l2 instance chain ID to a stored address entry.
     mapping(string => mapping(uint256 => RegistryEntry)) private registry;
 
-    uint256 public supportedChainIds; // List of supported chain IDs
+    /// @notice The task implementation chain id.
+    uint256 public supportedChainId;
 
     /// @notice Array of supported superchains and their configurations
     Superchain[] public superchains;
 
-    /// @notice Initializes the contract by loading addresses from JSON files.
-    /// @param addressFolderPath Path to the folder containing JSON files for addresses.
-    /// @param chainId The chain IDs to load addresses for.
-    constructor(string memory addressFolderPath, string memory superchainListPath, uint256 chainId) {
-        supportedChainIds = chainId;
+    /// @notice Initializes the contract by loading addresses from TOML files and setting up the superchains.
+    /// @param addressFolderPath Path to the folder containing TOML files for addresses.
+    /// @param superchainListFilePath Path to the TOML file containing the list of superchains.
+    constructor(string memory addressFolderPath, string memory superchainListFilePath) {
+        supportedChainId = block.chainid;
 
-        require(block.chainid == chainId, "Chain ID mismatch in config");
-
-        string memory filePath = string(abi.encodePacked(addressFolderPath, "/", vm.toString(chainId), ".toml"));
-        bytes memory fileContent = vm.parseToml(vm.readFile(filePath), ".addresses");
-
-        InputAddress[] memory parsedAddresses = abi.decode(fileContent, (InputAddress[]));
-
-        for (uint256 i = 0; i < parsedAddresses.length; i++) {
-            string memory identifier = parsedAddresses[i].identifier;
-            address contractAddress = parsedAddresses[i].addr;
-            bool isContract = parsedAddresses[i].isContract;
-
-            require(contractAddress != address(0), "Invalid address: cannot be zero");
-            require(chainId != 0, "Invalid chain ID: cannot be zero");
-            require(
-                registry[identifier][chainId].addr == address(0),
-                "Address already registered with this identifier and chain ID"
-            );
-
-            // Validate if the address is correctly marked as a contract or EOA
-            _typeCheckAddress(contractAddress, chainId, isContract);
-
-            registry[identifier][chainId] = RegistryEntry(contractAddress, isContract);
-            vm.label(contractAddress, identifier); // Add label for debugging purposes
-        }
-
-        bytes memory superchainListContent = vm.parseToml(vm.readFile(superchainListPath), ".chains");
+        bytes memory superchainListContent = vm.parseToml(vm.readFile(superchainListFilePath), ".chains");
         superchains = abi.decode(superchainListContent, (Superchain[]));
 
         string memory superchainAddressesPath = "lib/superchain-registry/superchain/extra/addresses/addresses.json";
+        string memory superchainAddressesContent = vm.readFile(superchainAddressesPath);
 
         for (uint256 i = 0; i < superchains.length; i++) {
-            require(superchains[i].chainId != 0, "Invalid chain ID in superchains");
-            require(bytes(superchains[i].identifier).length > 0, "Empty identifier in superchains");
-            require(bytes(superchains[i].name).length > 0, "Empty name in superchains");
+            uint256 superchainId = superchains[i].chainId;
+            string memory superchainName = superchains[i].name;
+            require(superchainId != 0, "Invalid chain ID in superchains");
+            require(bytes(superchainName).length > 0, "Empty name in superchains");
 
-            string memory superchainAddressesContent = vm.readFile(superchainAddressesPath);
+            string memory filePath = string(abi.encodePacked(addressFolderPath, "/", vm.toString(superchainId), ".toml"));
+            bytes memory fileContent = vm.parseToml(vm.readFile(filePath), ".addresses");
+
+            InputAddress[] memory parsedAddresses = abi.decode(fileContent, (InputAddress[]));
+
+            for (uint256 j = 0; j < parsedAddresses.length; j++) {
+                string memory identifier = parsedAddresses[j].identifier;
+                address contractAddress = parsedAddresses[j].addr;
+                bool isContract = parsedAddresses[j].isContract;
+
+                require(contractAddress != address(0), "Invalid address: cannot be zero");
+                require(
+                    registry[identifier][superchainId].addr == address(0),
+                    "Address already registered with this identifier and chain ID"
+                );
+
+                _typeCheckAddress(contractAddress, isContract);
+
+                registry[identifier][superchainId] = RegistryEntry(contractAddress, isContract);
+                string memory prefixedIdentifier = string(abi.encodePacked(vm.replace(vm.toUppercase(superchainName), " ", "_"), "_", identifier));
+                vm.label(contractAddress, prefixedIdentifier); // Add label for debugging purposes
+            }
+
             string[] memory keys =
-                vm.parseJsonKeys(superchainAddressesContent, string.concat("$.", vm.toString(superchains[i].chainId)));
+                vm.parseJsonKeys(superchainAddressesContent, string.concat("$.", vm.toString(superchainId)));
 
             for (uint256 j = 0; j < keys.length; j++) {
+                string memory key = keys[j];
                 address addr = vm.parseJsonAddress(
-                    superchainAddressesContent, string.concat("$.", vm.toString(superchains[i].chainId), ".", keys[j])
+                    superchainAddressesContent, string.concat("$.", vm.toString(superchainId), ".", key)
                 );
-                string memory prefixedKey = string.concat(superchains[i].identifier, "_", keys[j]);
-                registry[prefixedKey][chainId] = RegistryEntry(addr, true);
-                vm.label(addr, prefixedKey);
+
+                require(addr != address(0), "Invalid address: cannot be zero");
+                require(
+                    registry[key][superchainId].addr == address(0),
+                    "Address already registered with this identifier and chain ID"
+                );
+
+                // todo: update this to accomodate for non-contract addresses in superchain registry
+                // _typeCheckAddress(addr, true);
+
+                registry[key][superchainId] = RegistryEntry(addr, true);
+                string memory prefixedIdentifier = string(abi.encodePacked(vm.replace(vm.toUppercase(superchainName), " ", "_"), "_", key));
+                vm.label(addr, prefixedIdentifier);
             }
         }
+
     }
 
-    /// @notice Retrieves an address by its identifier for the current chain.
-    /// @param identifier Unique name for the address.
-    /// @return The associated address.
-    function getAddress(string memory identifier) public view returns (address) {
-        _checkChainSupported(block.chainid);
 
+    /// @notice Retrieves an address by its identifier for a specified l2 chain instance.
+    /// @param identifier The unique name associated with the address.
+    /// @param l2chainId The chain ID of the L2 superchain.
+    /// @return The address associated with the given identifier on the specified chain.
+    function getAddress(string memory identifier, uint256 l2chainId) public view returns (address) {
         // Fetch the stored registry entry
-        RegistryEntry memory entry = registry[identifier][block.chainid];
+        RegistryEntry memory entry = registry[identifier][l2chainId];
         address resolvedAddress = entry.addr;
 
         require(resolvedAddress != address(0), "Address not found");
 
-        // Perform type checks only for the current chain
-        _typeCheckAddress(resolvedAddress, block.chainid, entry.isContract);
-
         return resolvedAddress;
     }
 
-    /// @notice Checks if an address by its identifier is a contract on the current chain.
-    /// @param identifier Unique name for the address.
+    /// @notice Checks if an address by its identifier is a contract for a given l2 chain instance.
+    /// @param identifier The unique name associated with the address.
+    /// @param l2chainId The chain ID of the L2 superchain.
     /// @return True if the address is a contract, false otherwise.
-    function isAddressContract(string memory identifier) public view returns (bool) {
-        _checkAddressRegistered(identifier, block.chainid);
-        _checkChainSupported(block.chainid);
+    function isAddressContract(string memory identifier, uint256 l2chainId) public view returns (bool) {
+        _checkAddressRegistered(identifier, l2chainId);
 
-        return registry[identifier][block.chainid].isContract;
+        return registry[identifier][l2chainId].isContract;
     }
 
-    /// @notice Checks if an address by its identifier exists on the current chain.
-    /// @param identifier Unique name for the address.
+    /// @notice Checks if an address by its identifier exists for a specified L2 chain instance.
+    /// @param identifier The unique name associated with the address.
+    /// @param l2chainId The chain ID of the L2 superchain.
     /// @return True if the address exists, false otherwise.
-    function isAddressRegistered(string memory identifier) public view returns (bool) {
-        _checkChainSupported(block.chainid);
+    function isAddressRegistered(string memory identifier, uint256 l2chainId) public view returns (bool) {
 
-        return registry[identifier][block.chainid].addr != address(0);
+        return registry[identifier][l2chainId].addr != address(0);
     }
 
     /// @notice Verifies that an address is registered for a given identifier on a specific chain.
     /// @dev Ensures the address exists in the registry for the given identifier and chain ID.
     ///      Reverts with an error message if the address is not registered.
     /// @param identifier The unique name associated with the address.
-    /// @param chainId The blockchain network identifier.
-    function _checkAddressRegistered(string memory identifier, uint256 chainId) private view {
+    /// @param l2chainId l2 superchain chain id.
+    function _checkAddressRegistered(string memory identifier, uint256 l2chainId) private view {
         require(
-            isAddressRegistered(identifier),
+            isAddressRegistered(identifier, l2chainId),
             string(
-                abi.encodePacked("Address not found for identifier ", identifier, " on chain ", vm.toString(chainId))
+                abi.encodePacked("Address not found for identifier ", identifier, " on chain ", vm.toString(l2chainId))
             )
         );
     }
 
-    /// @notice Validates the type of an address (contract or externally owned account) for the current chain.
+    /// @notice Validates the type of an address (contract or externally owned account).
     /// @dev Ensures the address either contains or does not contain bytecode based on the `isContract` flag.
-    ///      This validation is only performed when the `chainId` matches the current chain's ID.
     ///      Reverts with an error if the address does not meet the expected type.
-    /// Only allows for type checking on the current chain. Otherwise reverts. This prevents fork management errors.
     /// @param addr The address to validate.
-    /// @param chainId The blockchain network id.
     /// @param isContract True if the address is expected to be a contract, false if it is expected to be an externally owned account (EOA).
-    function _typeCheckAddress(address addr, uint256 chainId, bool isContract) private view {
-        require(chainId == block.chainid, "Type check only supported for the current chain");
-
+    function _typeCheckAddress(address addr, bool isContract) private view {
         if (isContract) {
             require(addr.code.length > 0, "Address must contain code");
         } else {
             require(addr.code.length == 0, "Address must not contain code");
         }
-    }
-
-    /// @notice Checks whether the specified chain ID is supported by the contract.
-    /// @dev Reverts if the given `chainId` is not found in the `supportedChainIds` set.
-    /// @param chainId The identifier of the chain to check for support.
-    function _checkChainSupported(uint256 chainId) private view {
-        require(
-            supportedChainIds == chainId, string(abi.encodePacked("Chain ID ", vm.toString(chainId), " not supported"))
-        );
     }
 }
