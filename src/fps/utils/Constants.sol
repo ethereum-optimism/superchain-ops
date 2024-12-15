65282912dab58b481a7f592510c261688d2205f2
Elliot <elliotfriedman3@gmail.com>
2024-12-14 22:43:46 -0800

    naming: OPTIMISM_CHAINID -> OP_CHAINID
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/fps/utils/Constants.sol
+++ src/fps/utils/Constants.sol
@@ -2,7 +2,7 @@ pragma solidity 0.8.15;
 
 // Mainnet Chain Ids
 uint256 constant BASE_CHAIN_ID = 8453;
-uint256 constant OPTIMISM_CHAIN_ID = 10;
+uint256 constant OP_CHAIN_ID = 10;
 uint256 constant MODE_CHAIN_ID = 34443;
 uint256 constant ORDERLY_CHAIN_ID = 291;
 uint256 constant RACE_CHAIN_ID = 6805;
@@ -13,7 +13,7 @@ uint256 constant BINARY_CHAIN_ID = 624;
 
 // Testnet Chain Ids
 uint256 constant BASE_SEPOLIA_CHAIN_ID = 84532;
-uint256 constant OPTIMISM_SEPOLIA_CHAIN_ID = 11155420;
+uint256 constant OP_SEPOLIA_CHAIN_ID = 11155420;
 uint256 constant MODE_SEPOLIA_CHAIN_ID = 919;
 uint256 constant BASE_DEVNET_CHAIN_ID = 11763072;
 uint256 constant METAL_SEPOLIA_CHAIN_ID = 1740;
--- test/AddressRegistry.t.sol
+++ test/AddressRegistry.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.15;
 import {Test} from "forge-std/Test.sol";
 
 import {AddressRegistry} from "src/fps/AddressRegistry.sol";
-import {BASE_CHAIN_ID, OPTIMISM_CHAIN_ID} from "src/fps/utils/Constants.sol";
+import {BASE_CHAIN_ID, OP_CHAIN_ID} from "src/fps/utils/Constants.sol";
 
 contract MainnetAddressRegistryTest is Test {
     AddressRegistry private addresses;
@@ -20,93 +20,92 @@ contract MainnetAddressRegistryTest is Test {
     }
 
     function testContractState() public view {
-        assertEq(addresses.supportedChainId(), block.chainid, "Chain ID incorrect");
-        assertTrue(addresses.supportedL2ChainIds(OPTIMISM_CHAIN_ID), "Optimism chain ID not supported");
+        assertTrue(addresses.supportedL2ChainIds(OP_CHAIN_ID), "Optimism chain ID not supported");
         assertTrue(addresses.supportedL2ChainIds(BASE_CHAIN_ID), "Base chain ID not supported");
     }
 
     function testLocalAddressesLoaded() public view {
         assertEq(
-            addresses.getAddress("DEPLOYER_EOA", OPTIMISM_CHAIN_ID),
+            addresses.getAddress("DEPLOYER_EOA", OP_CHAIN_ID),
             0x9679E26bf0C470521DE83Ad77BB1bf1e7312f739,
             "DEPLOYER_EOA address mismatch"
         );
         assertEq(
-            addresses.getAddress("COMPOUND_GOVERNOR_BRAVO", OPTIMISM_CHAIN_ID),
+            addresses.getAddress("COMPOUND_GOVERNOR_BRAVO", OP_CHAIN_ID),
             0xc0Da02939E1441F497fd74F78cE7Decb17B66529,
             "COMPOUND_GOVERNOR_BRAVO address mismatch"
         );
         assertEq(
-            addresses.getAddress("COMPOUND_CONFIGURATOR", OPTIMISM_CHAIN_ID),
+            addresses.getAddress("COMPOUND_CONFIGURATOR", OP_CHAIN_ID),
             0x316f9708bB98af7dA9c68C1C3b5e79039cD336E3,
             "COMPOUND_CONFIGURATOR address mismatch"
         );
 
         assertTrue(
-            addresses.isAddressContract("COMPOUND_GOVERNOR_BRAVO", OPTIMISM_CHAIN_ID),
+            addresses.isAddressContract("COMPOUND_GOVERNOR_BRAVO", OP_CHAIN_ID),
             "Address governor bravo should be a contract"
         );
         assertTrue(
-            addresses.isAddressContract("COMPOUND_CONFIGURATOR", OPTIMISM_CHAIN_ID),
+            addresses.isAddressContract("COMPOUND_CONFIGURATOR", OP_CHAIN_ID),
             "Address configurator should be a contract"
         );
         assertFalse(
-            addresses.isAddressContract("DEPLOYER_EOA", OPTIMISM_CHAIN_ID), "EOA address should not be a contract"
+            addresses.isAddressContract("DEPLOYER_EOA", OP_CHAIN_ID), "EOA address should not be a contract"
         );
 
         assertTrue(
-            addresses.isAddressRegistered("DEPLOYER_EOA", OPTIMISM_CHAIN_ID), "DEPLOYER_EOA should be registered"
+            addresses.isAddressRegistered("DEPLOYER_EOA", OP_CHAIN_ID), "DEPLOYER_EOA should be registered"
         );
         assertTrue(
-            addresses.isAddressRegistered("COMPOUND_GOVERNOR_BRAVO", OPTIMISM_CHAIN_ID),
+            addresses.isAddressRegistered("COMPOUND_GOVERNOR_BRAVO", OP_CHAIN_ID),
             "COMPOUND_GOVERNOR_BRAVO should be registered"
         );
         assertTrue(
-            addresses.isAddressRegistered("COMPOUND_CONFIGURATOR", OPTIMISM_CHAIN_ID),
+            addresses.isAddressRegistered("COMPOUND_CONFIGURATOR", OP_CHAIN_ID),
             "COMPOUND_CONFIGURATOR should be registered"
         );
         assertFalse(
-            addresses.isAddressRegistered("NON_EXISTENT_ADDRESS", OPTIMISM_CHAIN_ID),
+            addresses.isAddressRegistered("NON_EXISTENT_ADDRESS", OP_CHAIN_ID),
             "Non-existent address should not be registered"
         );
     }
 
     function testSuperchainAddressesLoaded() public view {
         assertEq(
-            addresses.getAddress("OptimismPortalProxy", OPTIMISM_CHAIN_ID),
+            addresses.getAddress("OptimismPortalProxy", OP_CHAIN_ID),
             0xbEb5Fc579115071764c7423A4f12eDde41f106Ed,
             "OP Portal address mismatch"
         );
         assertEq(
-            addresses.getAddress("L1StandardBridgeProxy", OPTIMISM_CHAIN_ID),
+            addresses.getAddress("L1StandardBridgeProxy", OP_CHAIN_ID),
             0x99C9fc46f92E8a1c0deC1b1747d010903E884bE1,
             "OP Bridge address mismatch"
         );
         assertEq(
-            addresses.getAddress("L1CrossDomainMessengerProxy", OPTIMISM_CHAIN_ID),
+            addresses.getAddress("L1CrossDomainMessengerProxy", OP_CHAIN_ID),
             0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1,
             "OP Messenger address mismatch"
         );
 
         assertTrue(
-            addresses.isAddressContract("OptimismPortalProxy", OPTIMISM_CHAIN_ID), "OP Portal should be a contract"
+            addresses.isAddressContract("OptimismPortalProxy", OP_CHAIN_ID), "OP Portal should be a contract"
         );
         assertTrue(
-            addresses.isAddressContract("L1StandardBridgeProxy", OPTIMISM_CHAIN_ID), "OP Bridge should be a contract"
+            addresses.isAddressContract("L1StandardBridgeProxy", OP_CHAIN_ID), "OP Bridge should be a contract"
         );
         assertTrue(
-            addresses.isAddressContract("L1CrossDomainMessengerProxy", OPTIMISM_CHAIN_ID),
+            addresses.isAddressContract("L1CrossDomainMessengerProxy", OP_CHAIN_ID),
             "OP Messenger should be a contract"
         );
 
         assertTrue(
-            addresses.isAddressRegistered("OptimismPortalProxy", OPTIMISM_CHAIN_ID), "OP Portal should be registered"
+            addresses.isAddressRegistered("OptimismPortalProxy", OP_CHAIN_ID), "OP Portal should be registered"
         );
         assertTrue(
-            addresses.isAddressRegistered("L1StandardBridgeProxy", OPTIMISM_CHAIN_ID), "OP Bridge should be registered"
+            addresses.isAddressRegistered("L1StandardBridgeProxy", OP_CHAIN_ID), "OP Bridge should be registered"
         );
         assertTrue(
-            addresses.isAddressRegistered("L1CrossDomainMessengerProxy", OPTIMISM_CHAIN_ID),
+            addresses.isAddressRegistered("L1CrossDomainMessengerProxy", OP_CHAIN_ID),
             "OP Messenger should be registered"
         );
 
@@ -156,7 +155,7 @@ contract MainnetAddressRegistryTest is Test {
 
     function testGetNonExistentAddressFails() public {
         vm.expectRevert("Address not found");
-        addresses.getAddress("NON_EXISTENT_ADDRESS", OPTIMISM_CHAIN_ID);
+        addresses.getAddress("NON_EXISTENT_ADDRESS", OP_CHAIN_ID);
     }
 
     function testInvalidL2ChainIdIsAddressContractFails() public {
@@ -166,7 +165,7 @@ contract MainnetAddressRegistryTest is Test {
 
     function testGetIsAddressContractNonExistentAddressFails() public {
         vm.expectRevert("Address not found for identifier NON_EXISTENT_ADDRESS on chain 10");
-        addresses.isAddressContract("NON_EXISTENT_ADDRESS", OPTIMISM_CHAIN_ID);
+        addresses.isAddressContract("NON_EXISTENT_ADDRESS", OP_CHAIN_ID);
     }
 
     /// Construction failure tests
