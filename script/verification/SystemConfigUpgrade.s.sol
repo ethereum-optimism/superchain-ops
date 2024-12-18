c04293ca419ac03ac44c0277592b80a935548be4
geoknee <georgeknee@googlemail.com>
2024-12-18 15:24:58 +0000

    change validation script to use Seb's new framework

---
+++ script/verification/SystemConfigUpgrade.s.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {console2 as console} from "forge-std/console2.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {LibString} from "solady/utils/LibString.sol";
+import {VerificationBase, SuperchainRegistry} from "script/verification/Verification.s.sol";
+import "@eth-optimism-bedrock/src/dispute/lib/Types.sol";
+import {ISystemConfig} from "./ISystemConfig.sol";
+import {IResourceMetering} from "./IResourceMetering.sol";
+import {MIPS} from "@eth-optimism-bedrock/src/cannon/MIPS.sol";
+import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
+import {NestedMultisigBuilder} from "@base-contracts/script/universal/NestedMultisigBuilder.sol";
+
+contract SystemConfigUpgrade is VerificationBase, SuperchainRegistry {
+    using LibString for string;
+
+    struct SysCfgVars {
+        address owner;
+        uint256 scalar;
+        bytes32 batcherHash;
+        uint256 gasLimit;
+        address unsafeBlockSigner;
+        IResourceMetering.ResourceConfig resourceConfig;
+        address batchInbox;
+        address gasPayingToken;
+        address l1CrossDomainMessenger;
+        address l1StandardBridge;
+        address l1ERC721Bridge;
+        address disputeGameFactory;
+        address optimismPortal;
+        address optimismMintableERC20Factory;
+    }
+
+    SysCfgVars expected;
+
+    constructor(string memory l1ChainName, string memory l2ChainName, string memory release)
+        SuperchainRegistry(l1ChainName, l2ChainName, release)
+    {
+        expected = getSysCfgVars();
+        addAllowedStorageAccess(proxies.SystemConfig);
+    }
+
+    function getSysCfgVars() internal view returns (SysCfgVars memory) {
+        ISystemConfig sysCfg = ISystemConfig(proxies.SystemConfig);
+
+        (address gasPayingToken,) = sysCfg.gasPayingToken();
+
+        return SysCfgVars({
+            owner: sysCfg.owner(),
+            scalar: sysCfg.scalar(),
+            batcherHash: sysCfg.batcherHash(),
+            gasLimit: sysCfg.gasLimit(),
+            unsafeBlockSigner: sysCfg.unsafeBlockSigner(),
+            resourceConfig: sysCfg.resourceConfig(),
+            batchInbox: sysCfg.batchInbox(),
+            gasPayingToken: gasPayingToken,
+            l1CrossDomainMessenger: sysCfg.l1CrossDomainMessenger(),
+            l1StandardBridge: sysCfg.l1StandardBridge(),
+            l1ERC721Bridge: sysCfg.l1ERC721Bridge(),
+            disputeGameFactory: sysCfg.disputeGameFactory(),
+            optimismPortal: sysCfg.optimismPortal(),
+            optimismMintableERC20Factory: sysCfg.optimismMintableERC20Factory()
+        });
+    }
+
+    /// @notice Public function that must be called by the verification script.
+    function checkSystemConfigUpgrade() public view {
+        SysCfgVars memory got = getSysCfgVars();
+        require(keccak256(abi.encode(got)) == keccak256(abi.encode(expected)), "system-config-100");
+    }
+}
--- tasks/sep/027-holocene-system-config-upgrade-and-init-multi-chain/NestedSignFromJson.s.sol
+++ tasks/sep/027-holocene-system-config-upgrade-and-init-multi-chain/NestedSignFromJson.s.sol
@@ -1,133 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {NestedSignFromJson as OriginalNestedSignFromJson} from "script/NestedSignFromJson.s.sol";
-import {GnosisSafe} from "safe-contracts/GnosisSafe.sol";
-import {Vm, VmSafe} from "forge-std/Vm.sol";
-import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
 import {console2 as console} from "forge-std/console2.sol";
-import {ProxyAdmin} from "@eth-optimism-bedrock/src/universal/ProxyAdmin.sol";
-import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
-import {stdJson} from "forge-std/StdJson.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
+import {NestedSignFromJson as OriginalNestedSignFromJson} from "script/NestedSignFromJson.s.sol";
+import {DisputeGameUpgrade} from "script/verification/DisputeGameUpgrade.s.sol";
+import {CouncilFoundationNestedSign} from "script/verification/CouncilFoundationNestedSign.s.sol";
+import {SuperchainRegistry} from "script/verification/Verification.s.sol";
+import {SystemConfigUpgrade} from "script/verification/SystemConfigUpgrade.s.sol";
 
-/// @title ISemver
-/// @notice ISemver is a simple contract for ensuring that contracts are
-///         versioned using semantic versioning.
-interface ISemver {
-    /// @notice Getter for the semantic version of the contract. This is not
-    ///         meant to be used onchain but instead meant to be used by offchain
-    ///         tooling.
-    /// @return Semver contract version as a string.
-    function version() external view returns (string memory);
-}
-
-contract NestedSignFromJson is OriginalNestedSignFromJson {
+contract NestedSignFromJson is OriginalNestedSignFromJson, CouncilFoundationNestedSign {
+    string constant l1ChainName = "sepolia";
+    string constant release = "v1.8.0-rc.4";
     string[1] l2ChainIds = [
-        "11155420" // op
+        "op" // op
             // "1740", // metal TODO
             // "919", // mode TODO
             // "999999999" // zora TODO
     ];
 
-    // The slot used to store the livenessGuard address in GnosisSafe.
-    // See https://github.com/safe-global/safe-smart-account/blob/186a21a74b327f17fc41217a927dea7064f74604/contracts/base/GuardManager.sol#L30
-    bytes32 livenessGuardSlot = 0x4a204f620c8c5ccdca3fd54d003badd85ba500436a431f0cbda4f558c93c34c8;
+    SystemConfigUpgrade[1] sysCfgUpgrades;
 
-    address newSystemConfigImplAddress = 0x33b83E4C305c908B2Fc181dDa36e230213058d7d;
-
-    // Safe contract for this task.
-    GnosisSafe securityCouncilSafe = GnosisSafe(payable(vm.envAddress("COUNCIL_SAFE")));
-    GnosisSafe fndSafe = GnosisSafe(payable(vm.envAddress("FOUNDATION_SAFE")));
-    GnosisSafe proxyAdminOwnerSafe = GnosisSafe(payable(vm.envAddress("OWNER_SAFE")));
-
-    /// @notice Sets up the contract
-    function setUp() public {}
-
-    function checkProxyAdminproxyAdminOwnerSafe(string memory l2ChainId) internal view {
-        ProxyAdmin proxyAdmin = ProxyAdmin(readAddressFromSuperchainRegistry(l2ChainId, "ProxyAdmin"));
-
-        address proxyAdminOwner = proxyAdmin.owner();
-        require(proxyAdminOwner == address(proxyAdminOwnerSafe), "checkProxyAdminproxyAdminOwnerSafe-260");
-
-        address[] memory owners = proxyAdminOwnerSafe.getOwners();
-        require(owners.length == 2, "checkProxyAdminproxyAdminOwnerSafe-270");
-        require(proxyAdminOwnerSafe.isOwner(address(fndSafe)), "checkProxyAdminproxyAdminOwnerSafe-300");
-        require(proxyAdminOwnerSafe.isOwner(address(securityCouncilSafe)), "checkProxyAdminproxyAdminOwnerSafe-400");
+    constructor() {
+        for (uint256 i = 0; i < l2ChainIds.length; i++) {
+            sysCfgUpgrades[i] = new SystemConfigUpgrade(l1ChainName, l2ChainIds[i], release);
+        }
     }
 
-    /// @notice Checks the correctness of the deployment
-    function _postCheck(Vm.AccountAccess[] memory accesses, Simulation.Payload memory /* simPayload */ )
-        internal
-        view
-        override
-    {
+    function _postCheck(Vm.AccountAccess[] memory accesses, Simulation.Payload memory) internal view override {
         console.log("Running post-deploy assertions");
         checkStateDiff(accesses);
         for (uint256 i = 0; i < l2ChainIds.length; i++) {
-            console.log("Running checks on chain with iD ", l2ChainIds[i]);
-            checkProxyAdminproxyAdminOwnerSafe(l2ChainIds[i]);
-            ISemver systemConfigProxy = ISemver(readAddressFromSuperchainRegistry(l2ChainIds[i], "SystemConfigProxy"));
-            ProxyAdmin proxyAdmin = ProxyAdmin(readAddressFromSuperchainRegistry(l2ChainIds[i], "ProxyAdmin"));
-            require(
-                proxyAdmin.getProxyImplementation(address(systemConfigProxy)) == newSystemConfigImplAddress,
-                "SystemConfigProxy implementation not updated"
-            );
-            require(
-                keccak256(abi.encode(systemConfigProxy.version())) == keccak256(abi.encode("2.3.0")),
-                "Version not updated"
-            );
+            sysCfgUpgrades[i].checkSystemConfigUpgrade();
         }
-
         console.log("All assertions passed!");
     }
 
-    function readAddressFromSuperchainRegistry(string memory chainId, string memory contractName)
-        internal
-        view
-        returns (address)
-    {
-        string memory addressesJson;
-
-        // Read addresses json
-        string memory path = "/lib/superchain-registry/superchain/extra/addresses/addresses.json";
-
-        try vm.readFile(string.concat(vm.projectRoot(), path)) returns (string memory data) {
-            addressesJson = data;
-        } catch {
-            revert(string.concat("Failed to read ", path));
-        }
-
-        return stdJson.readAddress(addressesJson, string.concat("$.", chainId, ".", contractName));
-    }
-
-    function getAllowedStorageAccess() internal view override returns (address[] memory allowed) {
-        address livenessGuard = address(uint160(uint256(vm.load(address(securityCouncilSafe), livenessGuardSlot))));
-
-        allowed = new address[](9);
-
-        for (uint256 i = 0; i < l2ChainIds.length; i++) {
-            address systemConfigProxy = readAddressFromSuperchainRegistry(l2ChainIds[i], "SystemConfigProxy");
-            allowed[i] = systemConfigProxy;
-        }
-        allowed[5] = address(proxyAdminOwnerSafe);
-        allowed[6] = address(securityCouncilSafe);
-        allowed[7] = address(fndSafe);
-        allowed[8] = livenessGuard;
+    function getAllowedStorageAccess() internal view override returns (address[] memory) {
+        return allowedStorageAccess;
     }
 
     function getCodeExceptions() internal view override returns (address[] memory) {
-        address[] memory exceptions = new address[](4 * l2ChainIds.length);
-        for (uint256 i = 0; i < l2ChainIds.length; i += 4) {
-            SystemConfig systemConfigProxy =
-                SystemConfig(readAddressFromSuperchainRegistry(l2ChainIds[i], "SystemConfigProxy"));
-            // The following are all EOAs which will be "updated" (to their original values)
-            // When we reinit the SystemConfig. So we need to except them from the check
-            // which complains when it finds addresses in storage with no code.
-            exceptions[i] = systemConfigProxy.owner(); // NOTE this can be removed for mainnet
-            exceptions[i + 1] = address(uint160(uint256((systemConfigProxy.batcherHash()))));
-            exceptions[i + 2] = systemConfigProxy.unsafeBlockSigner();
-            exceptions[i + 3] = systemConfigProxy.batchInbox();
-        }
-        return exceptions;
+        return codeExceptions;
     }
 }
