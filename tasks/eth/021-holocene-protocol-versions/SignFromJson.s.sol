4a5775953b04a3f1b4c7f33d060dc035c0069811
geoknee <georgeknee@googlemail.com>
2024-12-19 16:44:30 +0000

    clean up unused imports

--- tasks/eth/021-holocene-protocol-versions/SignFromJson.s.sol
+++ tasks/eth/021-holocene-protocol-versions/SignFromJson.s.sol
@@ -4,15 +4,9 @@ pragma solidity ^0.8.15;
 import {SignFromJson as OriginalSignFromJson} from "script/SignFromJson.s.sol";
 import {ProtocolVersionsBump} from "script/verification/ProtocolVersionsBump.s.sol";
 import {SuperchainRegistry} from "script/verification/Verification.s.sol";
-import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
-import {Proxy} from "@eth-optimism-bedrock/src/universal/Proxy.sol";
-import {SystemConfig} from "@eth-optimism-bedrock/src/L1/SystemConfig.sol";
-import {ProtocolVersions, ProtocolVersion} from "@eth-optimism-bedrock/src/L1/ProtocolVersions.sol";
-import {Types} from "@eth-optimism-bedrock/scripts/Types.sol";
 import {console2 as console} from "forge-std/console2.sol";
-import {stdToml} from "forge-std/StdToml.sol";
-import {Vm, VmSafe} from "forge-std/Vm.sol";
-import {LibString} from "solady/utils/LibString.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
 
 contract SignFromJson is OriginalSignFromJson, ProtocolVersionsBump {
     constructor()
