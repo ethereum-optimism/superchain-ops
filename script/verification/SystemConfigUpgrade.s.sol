dd88bab5d0aaedd8158cef451e590dc686f020b1
geoknee <georgeknee@googlemail.com>
2025-01-08 10:52:21 +0000

    add step to validate target semver of SystemConfig

--- script/verification/SystemConfigUpgrade.s.sol
+++ script/verification/SystemConfigUpgrade.s.sol
@@ -6,6 +6,7 @@ import {LibString} from "solady/utils/LibString.sol";
 import {SuperchainRegistry} from "script/verification/Verification.s.sol";
 import {ISystemConfig} from "./ISystemConfig.sol";
 import {IResourceMetering} from "./IResourceMetering.sol";
+import {console2 as console} from "forge-std/console2.sol";
 
 // SystemConfigUpgrade is a contract that can be used to verify that an upgrade of the SystemConfig contract
 // has not changed any of the storage variables that are not expected to change.
@@ -36,12 +37,19 @@ contract SystemConfigUpgrade is SuperchainRegistry {
 
     address public systemConfigAddress;
     SysCfgVars previous;
+    string targetVersion;
 
     constructor(string memory _l1ChainName, string memory _l2ChainName, string memory _release)
         SuperchainRegistry(_l1ChainName, _l2ChainName, _release)
     {
         systemConfigAddress = proxies.SystemConfig;
+        targetVersion = standardVersions.SystemConfig.version;
         previous = getSysCfgVars(); // Set this before the tx is executed.
+    }
+
+    function getSysCfgVersion() internal view returns (string memory) {
+        ISystemConfig sysCfg = ISystemConfig(proxies.SystemConfig);
+        return sysCfg.version();
     }
 
     function getSysCfgVars() internal view returns (SysCfgVars memory) {
@@ -68,6 +76,11 @@ contract SystemConfigUpgrade is SuperchainRegistry {
 
     /// @notice Public function that must be called by the verification script.
     function checkSystemConfigUpgrade() public view virtual {
+        require(
+            keccak256(abi.encode(getSysCfgVersion())) == keccak256(abi.encode(targetVersion)),
+            "system-config-050: targetVersion"
+        );
+        console.log("confirmed SystemConfig upgraded to version", targetVersion);
         SysCfgVars memory got = getSysCfgVars();
         require(keccak256(abi.encode(got)) == keccak256(abi.encode(previous)), "system-config-100");
     }
