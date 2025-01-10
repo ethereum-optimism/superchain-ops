cd7ec86dcb90083996058af5ca57e1233c4374cf
geoknee <georgeknee@googlemail.com>
2025-01-10 10:54:01 +0000

    consolidate interfaces into one file

--- script/verification/HoloceneSystemConfigUpgrade.s.sol
+++ script/verification/HoloceneSystemConfigUpgrade.s.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.15;
 
 import {console2 as console} from "forge-std/console2.sol";
 import {LibString} from "solady/utils/LibString.sol";
-import {ISystemConfig} from "./ISystemConfig.sol";
-import {IResourceMetering} from "./IResourceMetering.sol";
 import {SuperchainRegistry} from "script/verification/Verification.s.sol";
 
 // HoloceneSystemConfigUpgrade is a contract that can be used to verify the Holocene upgrade of the SystemConfig contract.
@@ -195,4 +193,83 @@ contract HoloceneSystemConfigUpgrade is SuperchainRegistry {
             optimismMintableERC20Factory: sysCfg.optimismMintableERC20Factory()
         });
     }
+}
+
+interface IResourceMetering {
+    struct ResourceConfig {
+        uint32 maxResourceLimit;
+        uint8 elasticityMultiplier;
+        uint8 baseFeeMaxChangeDenominator;
+        uint32 minimumBaseFee;
+        uint32 systemTxMaxGas;
+        uint128 maximumBaseFee;
+    }
+}
+
+interface ISystemConfig {
+    struct Addresses {
+        address l1CrossDomainMessenger;
+        address l1ERC721Bridge;
+        address l1StandardBridge;
+        address disputeGameFactory;
+        address optimismPortal;
+        address optimismMintableERC20Factory;
+        address gasPayingToken;
+    }
+
+    function BATCH_INBOX_SLOT() external view returns (bytes32);
+    function DISPUTE_GAME_FACTORY_SLOT() external view returns (bytes32);
+    function L1_CROSS_DOMAIN_MESSENGER_SLOT() external view returns (bytes32);
+    function L1_ERC_721_BRIDGE_SLOT() external view returns (bytes32);
+    function L1_STANDARD_BRIDGE_SLOT() external view returns (bytes32);
+    function OPTIMISM_MINTABLE_ERC20_FACTORY_SLOT() external view returns (bytes32);
+    function OPTIMISM_PORTAL_SLOT() external view returns (bytes32);
+    function START_BLOCK_SLOT() external view returns (bytes32);
+    function UNSAFE_BLOCK_SIGNER_SLOT() external view returns (bytes32);
+    function VERSION() external view returns (uint256);
+    function basefeeScalar() external view returns (uint32);
+    function batchInbox() external view returns (address addr_);
+    function batcherHash() external view returns (bytes32);
+    function blobbasefeeScalar() external view returns (uint32);
+    function disputeGameFactory() external view returns (address addr_);
+    function gasLimit() external view returns (uint64);
+    function eip1559Denominator() external view returns (uint32);
+    function eip1559Elasticity() external view returns (uint32);
+    function gasPayingToken() external view returns (address addr_, uint8 decimals_);
+    function gasPayingTokenName() external view returns (string memory name_);
+    function gasPayingTokenSymbol() external view returns (string memory symbol_);
+    function initialize(
+        address _owner,
+        uint32 _basefeeScalar,
+        uint32 _blobbasefeeScalar,
+        bytes32 _batcherHash,
+        uint64 _gasLimit,
+        address _unsafeBlockSigner,
+        IResourceMetering.ResourceConfig memory _config,
+        address _batchInbox,
+        Addresses memory _addresses
+    ) external;
+    function isCustomGasToken() external view returns (bool);
+    function l1CrossDomainMessenger() external view returns (address addr_);
+    function l1ERC721Bridge() external view returns (address addr_);
+    function l1StandardBridge() external view returns (address addr_);
+    function maximumGasLimit() external pure returns (uint64);
+    function minimumGasLimit() external view returns (uint64);
+    function optimismMintableERC20Factory() external view returns (address addr_);
+    function optimismPortal() external view returns (address addr_);
+    function overhead() external view returns (uint256);
+    function owner() external view returns (address);
+    function renounceOwnership() external;
+    function resourceConfig() external view returns (IResourceMetering.ResourceConfig memory);
+    function scalar() external view returns (uint256);
+    function setBatcherHash(bytes32 _batcherHash) external;
+    function setGasConfig(uint256 _overhead, uint256 _scalar) external;
+    function setGasConfigEcotone(uint32 _basefeeScalar, uint32 _blobbasefeeScalar) external;
+    function setGasLimit(uint64 _gasLimit) external;
+    function setUnsafeBlockSigner(address _unsafeBlockSigner) external;
+    function setEIP1559Params(uint32 _denominator, uint32 _elasticity) external;
+    function startBlock() external view returns (uint256 startBlock_);
+    function transferOwnership(address newOwner) external; // nosemgrep
+    function unsafeBlockSigner() external view returns (address addr_);
+    function version() external pure returns (string memory);
 }
