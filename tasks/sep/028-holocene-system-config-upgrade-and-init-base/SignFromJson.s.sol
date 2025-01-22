a037e61fa46e6dc331e0841b0865e2930c3b6324
George Knee <georgeknee@googlemail.com>
2025-01-22 19:12:42 +0000

    push codeExceptions exceptions into HoloceneSystemConfigUpgrade contract (#478)
    Some ready but not-yet-executed tasks depend on this contract, but they are simulated in CI so if that is green should be OK.

--- tasks/sep/028-holocene-system-config-upgrade-and-init-base/SignFromJson.s.sol
+++ tasks/sep/028-holocene-system-config-upgrade-and-init-base/SignFromJson.s.sol
@@ -28,23 +28,7 @@ contract SignFromJson is OriginalSignFromJson, VerificationBase {
         // The OwnerSafe multisig nonce is incremented.
         addAllowedStorageAccess(ownerSafe);
 
-        // The last call to initialize:
-        //  1. resets the owner temporarily to the caller (ProxyAdmin@0x0389e59aa0a41e4a413ae70f0008e76caa34b1f3) when calling `__Ownable_init()`
-        //     https://github.com/ethereum-optimism/optimism/blob/39e9f1a4693912af8bdef428d246c15a6cf44ec7/packages/contracts-bedrock/src/L1/SystemConfig.sol#L171
-        //     This in turn runs the code at https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/d2ec96a77f25bd095bcf0d0e1e0b56be3f6be711/contracts/access/OwnableUpgradeable.sol#L29
-        //
-        //  2. sets the owner back to the OwnerSafe multisig when calling `transferOwnership(_owner)`
-        //     https://github.com/ethereum-optimism/optimism/blob/39e9f1a4693912af8bdef428d246c15a6cf44ec7/packages/contracts-bedrock/src/L1/SystemConfig.sol#L172
-        //
-        //  /!\ For this reason we need NOT to include the code exception for the OwnerSafe multisig /!\
-        address[] memory codeExceptions = sysCfgUpgrade.getCodeExceptions();
-        for (uint256 i; i < codeExceptions.length; i++) {
-            if (codeExceptions[i] == ownerSafe) {
-                continue;
-            }
-
-            addCodeException(codeExceptions[i]);
-        }
+        addCodeExceptions(sysCfgUpgrade.getCodeExceptions());
     }
 
     function _postCheck(Vm.AccountAccess[] memory accesses, Simulation.Payload memory) internal view override {
