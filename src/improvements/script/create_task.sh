9fc9ba6ccae694f0f9a75588a37945c6a82a4381
Blaine Malone <blainemalone01@gmail.com>
2025-02-04 12:37:02 -0500

    fix: UX improvements

--- src/improvements/script/create_task.sh
+++ src/improvements/script/create_task.sh
@@ -5,16 +5,17 @@ create_task() {
     PS3="Select network: "
     select network in eth sep oeth sep-dev-0 "Other (specify)"; do
         case $network in
-            "Other (specify)")
-                read -r -p "Enter custom network name: " network
-                ;;
-            "")
-                echo "Invalid selection, please enter a network name manually: "
-                read -r network
-                ;;
+        "Other (specify)")
+            read -r -p "Enter custom network name: " network
+            ;;
+        "")
+            echo "Invalid selection, please enter a network name manually: "
+            read -r network
+            ;;
         esac
         break
     done
+    echo -e "\n\033[32mYou selected: $network\033[0m"
 
     echo ""
     mapfile -t templates < <(ls -1 template/)
@@ -23,49 +24,44 @@ create_task() {
     done
 
     PS3="Select template name: "
-    select template in "${templates[@]}"; do
-        case $template in
-            "")
-                echo "Invalid selection, please enter a template name manually: "
-                read -r template
-                ;;
-            *)
+    while true; do
+        select template in "${templates[@]}"; do
+            if [[ -z "$template" ]]; then
+                echo -e "\n\033[31mInvalid selection. Please choose a number from the list.\033[0m\n"
                 break
-                ;;
-        esac
-        break
+            elif [[ " ${templates[*]} " =~ ${template} ]]; then
+                echo -e "\n\033[32mYou selected: $template\033[0m"
+                break 2 # Exit both loops
+            else
+                echo -e "\n\033[31mUnexpected error. Please try again.\033[0m"
+                break
+            fi
+        done
     done
 
+    # This is an ordered list of all the tasks for a given network.
     existing_dirs=$(find "tasks/$network" -maxdepth 1 -type d 2>/dev/null | sort)
     echo ""
-    echo "Existing directories: " 
-    echo "${existing_dirs//$'\n'/$'\n  '}"
 
-    # Find the highest number used in existing directory names
-    # This is used to suggest the next available directory name.
-    # If the number pattern is broken (001-task-name, 002-task-name, ...), this suggestion may not work.
-    highest_num=0
-    for dir in $existing_dirs; do
-        basename=$(basename "$dir")
-        if [[ $basename =~ ([0-9]{3}) ]]; then
-            num=${BASH_REMATCH[1]}
-            if (( 10#$num > highest_num )); then
-                highest_num=$((10#$num))
-            fi
-        fi
-    done
-    next_num=$((highest_num + 1))
-    suggested_name=$(printf "%03d-<enter-task-name>" $next_num)
     most_recent_task_dir=$(echo "$existing_dirs" | tail -n1)
-    
+
+    suggestion=""
+    if [[ -z "$most_recent_task_dir" ]]; then
+        suggestion="Note: this is the first task for this network. Please choose a name that's lexicographically sensible"
+    else
+        suggestion="lexicographically after: $most_recent_task_dir"
+    fi
     while true; do
-        read -r -p "Enter task directory name (suggested: $suggested_name): " dirname
+        read -r -p "Enter task directory name ($suggestion): " dirname
         if [[ -z "$dirname" ]]; then
-            echo "Task directory name cannot be empty. Please try again."
+            echo -e "\n\033[31mTask directory name cannot be empty. Please try again.\033[0m\n"
         elif [[ -n "$most_recent_task_dir" && "$dirname" < "$(basename "$most_recent_task_dir")" ]]; then
-            echo "Error: '$dirname' is lexicographically earlier than existing directories."
-            echo "The last existing directory is: $(basename "$most_recent_task_dir")"
-            echo "Please choose a name that comes after this alphabetically."
+            echo -e "\033[31mError: Task name '$dirname' is lexicographically earlier than existing directories.\033[0m"
+            echo -e "\033[31mThe last existing directory is: $(basename "$most_recent_task_dir")\033[0m"
+            echo -e "\033[31mPlease choose a name that comes after this lexicographically.\033[0m\n"
+        elif [[ -n "$most_recent_task_dir" && "$dirname" == "$(basename "$most_recent_task_dir")" ]]; then
+            echo -e "\033[31mError: Task name '$dirname' already exists.\033[0m"
+            echo -e "\033[31mPlease choose a name that comes after this lexicographically.\033[0m\n"
         else
             break
         fi
@@ -75,8 +71,8 @@ create_task() {
     mkdir -p "$task_path"
     config_path="$task_path/config.toml"
     readme_path="$task_path/README.md" # TODO: Each template should have a README.md
-    echo "templateName = \"${template%.sol}\"" > "${config_path}"
-    echo "# ${dirname}" > "${readme_path}"
+    echo "templateName = \"${template%.sol}\"" >"${config_path}"
+    echo "# ${dirname}" >"${readme_path}"
     echo "Created task directory '${dirname}' for network: ${network}"
 }
 
