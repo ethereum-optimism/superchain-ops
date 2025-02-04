e5700a05c445981860a6adcbab99f0e846c49223
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2025-02-04 10:24:18 -0800

    [PT: 2] Monorepo Integration Testing (#505)
    * add: post task monorepo integration tests
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * add: Runner.sol script
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * add example tasks to runnerConfig toml file
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * add: run function to interface
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * run function override
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * Update src/fps/example/Runner.sol
    
    Co-authored-by: blaine <blainemalone01@gmail.com>
    
    * Update src/fps/example/Runner.sol
    
    Co-authored-by: blaine <blainemalone01@gmail.com>
    
    * update chain name to all lower case
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * update runner to use new shell script
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * fetch tasks using shell script
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * fmt
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * update monorepo-integration-test name to match workflow name
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * use default docker image
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * checkout with mise, correct file path to Runner.sol, use ffi flag
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * add newline to config yml between workflows
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * Update test/task/mock/example/task-02/README.md
    
    * Update test/task/mock/example/task-01/README.md
    
    * move directory, use remappings in imports
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    * remove monorepo_integration_test
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    
    ---------
    
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>
    Co-authored-by: blaine <blainemalone01@gmail.com>

--- src/improvements/task/MultisigTask.sol
+++ src/improvements/task/MultisigTask.sol
@@ -148,7 +148,7 @@ abstract contract MultisigTask is Test, Script, ITask {
     /// @notice Runs the task with the given configuration file path.
     /// Sets the address registry, initializes and simulates the task.
     /// @param taskConfigFilePath The path to the task configuration file.
-    function run(string memory taskConfigFilePath) public {
+    function run(string memory taskConfigFilePath) public override {
         Addresses _addresses = new Addresses(taskConfigFilePath);
 
         _templateSetup(taskConfigFilePath);
