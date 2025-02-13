5fa678ce1c6f1fbeead1d679408ee5478f2d31a1
prateek <prateekgupta105@gmail.com>
2025-02-14 01:01:30 +0530

    add opcm execute test

--- test/tasks/NestedMultisigTask.t.sol
+++ test/tasks/NestedMultisigTask.t.sol
@@ -6,6 +6,7 @@ import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {Test} from "forge-std/Test.sol";
 
 import {MultisigTask} from "src/improvements/tasks/MultisigTask.sol";
+import {OPCMUpgradeTemplate} from "src/improvements/template/OPCMUpgradeTemplate.sol";
 import {DisputeGameUpgradeTemplate} from "src/improvements/template/DisputeGameUpgradeTemplate.sol";
 import {AddressRegistry as Addresses} from "src/improvements/AddressRegistry.sol";
 import {LibSort} from "@solady/utils/LibSort.sol";
@@ -32,10 +33,6 @@ contract NestedMultisigTaskTest is Test {
     /// ProxyAdminOwner safe for task-01 is a nested multisig for Op mainnet L2 chain.
     string taskConfigFilePath = "test/tasks/mock/example/task-01/config.toml";
 
-    function setUp() public {
-        vm.createSelectFork("mainnet");
-    }
-
     function runTask() internal {
         multisigTask = new DisputeGameUpgradeTemplate();
         multisigTask.simulateRun(taskConfigFilePath);
@@ -43,11 +40,13 @@ contract NestedMultisigTaskTest is Test {
     }
 
     function testSafeNested() public {
+        vm.createSelectFork("mainnet");
         runTask();
         assertEq(multisigTask.isNestedSafe(), true, "Expected isNestedSafe to be false");
     }
 
     function testNestedDataToSignAndHashToApprove() public {
+        vm.createSelectFork("mainnet");
         runTask();
         IGnosisSafe parentMultisig = IGnosisSafe(multisigTask.multisig());
         address[] memory childOwnerMultisigs = parentMultisig.getOwners();
@@ -126,6 +125,7 @@ contract NestedMultisigTaskTest is Test {
     }
 
     function testNestedExecuteWithSignatures() public {
+        vm.createSelectFork("mainnet");
         uint256 snapshotId = vm.snapshot();
         runTask();
         address multisig = multisigTask.multisig();
@@ -216,6 +216,94 @@ contract NestedMultisigTaskTest is Test {
             0xf691F8A6d908B58C534B624cF16495b491E633BA,
             "implementation not set"
         );
+    }
+
+    function testNestedExecuteWithSignaturesOPCM() public {
+        vm.createSelectFork("sepolia");
+        uint256 snapshotId = vm.snapshot();
+        multisigTask = new OPCMUpgradeTemplate();
+        string memory opcmTaskConfigFilePath = "test/tasks/mock/example/task-05/config.toml";
+        multisigTask.simulateRun(opcmTaskConfigFilePath);
+        addresses = multisigTask.addresses();
+        address multisig = multisigTask.multisig();
+        address[] memory parentMultisigOwners = IGnosisSafe(multisig).getOwners();
+        bytes[] memory childMultisigDatasToSign = new bytes[](parentMultisigOwners.length);
+        /// store the data to sign for each child multisig
+        for (uint256 i = 0; i < parentMultisigOwners.length; i++) {
+            childMultisigDatasToSign[i] = getNestedDataToSign(parentMultisigOwners[i]);
+        }
+        /// revert to snapshot so that the safe is in the same state as before the task was run
+        vm.revertTo(snapshotId);
+
+        MultiSigOwner[] memory newOwners = new MultiSigOwner[](9);
+        (newOwners[0].walletAddress, newOwners[0].privateKey) = makeAddrAndKey("Owner0");
+        (newOwners[1].walletAddress, newOwners[1].privateKey) = makeAddrAndKey("Owner1");
+        (newOwners[2].walletAddress, newOwners[2].privateKey) = makeAddrAndKey("Owner2");
+        (newOwners[3].walletAddress, newOwners[3].privateKey) = makeAddrAndKey("Owner3");
+        (newOwners[4].walletAddress, newOwners[4].privateKey) = makeAddrAndKey("Owner4");
+        (newOwners[5].walletAddress, newOwners[5].privateKey) = makeAddrAndKey("Owner5");
+        (newOwners[6].walletAddress, newOwners[6].privateKey) = makeAddrAndKey("Owner6");
+        (newOwners[7].walletAddress, newOwners[7].privateKey) = makeAddrAndKey("Owner7");
+        (newOwners[8].walletAddress, newOwners[8].privateKey) = makeAddrAndKey("Owner8");
+
+        for (uint256 i = 0; i < newOwners.length; i++) {
+            privateKeyForOwner[newOwners[i].walletAddress] = newOwners[i].privateKey;
+        }
+
+        for (uint256 i = 0; i < parentMultisigOwners.length; i++) {
+            address childMultisig = parentMultisigOwners[i];
+
+            {
+                /// set the new owners for the child multisig
+                address currentOwner = address(0x1);
+                bytes32 slot;
+                for (uint256 j = 0; j < newOwners.length; j++) {
+                    slot = keccak256(abi.encode(currentOwner, OWNER_MAPPING_STORAGE_OFFSET));
+                    vm.store(childMultisig, slot, bytes32(uint256(uint160(newOwners[j].walletAddress))));
+                    currentOwner = newOwners[j].walletAddress;
+                }
+
+                /// point the final owner back to the sentinel
+                slot = keccak256(abi.encode(currentOwner, OWNER_MAPPING_STORAGE_OFFSET));
+                vm.store(childMultisig, slot, bytes32(uint256(uint160(0x1))));
+            }
+
+            /// set the owners count to 9
+            vm.store(childMultisig, bytes32(OWNER_COUNT_STORAGE_OFFSET), bytes32(uint256(9)));
+
+            /// set the threshold to 4
+            vm.store(childMultisig, bytes32(THRESHOLD_STORAGE_OFFSET), bytes32(uint256(4)));
+
+            address[] memory getNewOwners = IGnosisSafe(childMultisig).getOwners();
+            assertEq(getNewOwners.length, 9, "Expected 9 owners");
+            for (uint256 j = 0; j < newOwners.length; j++) {
+                /// check that the new owners are set correctly
+                assertEq(getNewOwners[j], newOwners[j].walletAddress, "Expected owner");
+            }
+
+            uint256 threshold = IGnosisSafe(childMultisig).getThreshold();
+            assertEq(threshold, 4, "Expected threshold should be updated to mocked value");
+            LibSort.sort(getNewOwners);
+
+            /// sign the approve hash call data to sign with the private keys of the new owners of the child multisig
+            bytes memory packedSignaturesChild;
+            for (uint256 j = 0; j < threshold; j++) {
+                (uint8 v, bytes32 r, bytes32 s) =
+                    vm.sign(privateKeyForOwner[getNewOwners[j]], keccak256(childMultisigDatasToSign[i]));
+                packedSignaturesChild = bytes.concat(packedSignaturesChild, abi.encodePacked(r, s, v));
+            }
+
+            /// execute the approve hash call with the signatures
+            multisigTask = new OPCMUpgradeTemplate();
+            multisigTask.approveFromChildMultisig(opcmTaskConfigFilePath, childMultisig, packedSignaturesChild);
+        }
+
+        /// no offchain signatures for the parent multisig
+        bytes memory packedSignaturesParent;
+
+        /// execute the task
+        multisigTask = new OPCMUpgradeTemplate();
+        multisigTask.executeRun(opcmTaskConfigFilePath, packedSignaturesParent);
     }
 
     function getNestedDataToSign(address owner) internal view returns (bytes memory) {
