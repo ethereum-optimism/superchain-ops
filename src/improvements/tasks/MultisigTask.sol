fbdc5c8021ff72aa32e5059da7b930e579cb8d86
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2025-02-13 16:25:19 -0800

    add checkStateDiff function (#602)
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/improvements/tasks/MultisigTask.sol
+++ src/improvements/tasks/MultisigTask.sol
@@ -840,8 +840,16 @@ abstract contract MultisigTask is Test, Script, ITask {
         }
     }
 
+    /// @notice helper function that can be overridden by template contracts to
+    /// check the state changes applied by the task. This function can check
+    /// that only the nonce changed in the parent multisig when executing a task
+    /// by checking the slot and address where the slot changed.
+    function checkStateDiff(VmSafe.AccountAccess[] memory) internal view virtual {}
+
     /// @notice helper method to get transfers and state changes of task affected addresses
     function _processStateDiffChanges(VmSafe.AccountAccess[] memory accountAccesses) private {
+        /// first check that no tokens or eth were sent that should not have been
+        /// then check that all state changes that happened were in contracts that were allowed to have state changes
         for (uint256 i = 0; i < accountAccesses.length; i++) {
             // process ETH transfer changes
             _processETHTransferChanges(accountAccesses[i]);
@@ -852,6 +860,8 @@ abstract contract MultisigTask is Test, Script, ITask {
             // process state changes
             _processStateChanges(accountAccesses[i].storageAccesses);
         }
+
+        checkStateDiff(accountAccesses);
     }
 
     /// @notice helper method to get eth transfers of task affected addresses
