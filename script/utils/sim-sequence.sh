bf7f8f33fc28d6c264b6bcfdaaff1a448c2d40c6
Ethnical <Ethnicalnightmare@gmail.com>
2025-02-14 21:55:45 +0100

    Adding the Anvil Fork on the multiples tasks. (#438)
    * feat: add the fork method + monitoring of the nonce
    
    * feat: add a simulation entry that broadcast the transaction into a given RPC-URL
    
    * hardcode the owner that contains ETH
    
    * remove the exit
    
    * unset the ETH_RPC_URL to make sure there is not conflit with the current RPC
    
    * add a dummy signature since the threshold is 1 just to go deeper and will revert with GS26 since the signature is not an owner.
    
    * format
    
    * chore: hardcore block number to avoid this
    
    * chore: update this with a real signature to execute in the past
    
    * chore: update the version
    
    * fix: the localhost RPC instead of the $ETH_RPC_URL
    
    * feat: add overrides for overwriting the safe owner.
    
    * chore: add a the anvil.sh to simulate into the anvil
    
    * chore: add dummy sig with the privatekey of the wellknown address
    
    * chore: remove the auto-impersonate
    
    * chore: make the single.just
    
    * feat: add the solo council
    
    * feat: add the foundation + sc signatures
    
    * chore: clean up
    
    * chore: clean up
    
    * chore: single.just is ready to execute the single task
    
    * chore: sim-seq is clean up but still some issue with the nested approval
    
    * chore: clean up + color
    
    * fix multiples bugs with nested but now the nonce increment from 2 instead of 1...
    
    * chore: fix the duplicates nonce bump
    
    * chore: clean again
    
    * chore: add user input to make sure we simulate the correct one.
    
    * chore: fix ci
    
    * revert: change to simulate
    
    * chore: fix CI
    
    * chore: fix the print 3
    
    * chore: revert change to tasks/eth/021-holocene-protocol-versions/SignFromJson.s.sol
    
    * chore: remove anvil.sh

--- script/utils/sim-sequence.sh
+++ script/utils/sim-sequence.sh
@@ -1,5 +1,60 @@
 #!/bin/bash
 
+
+LOGFILE="/tmp/sim-sequence.log"
+
+
+## Nonce Values
+MAX_NONCE_ERROR=9999999
+FUS_BEFORE=$MAX_NONCE_ERROR
+FOS_BEFORE=$MAX_NONCE_ERROR
+SC_BEFORE=$MAX_NONCE_ERROR
+L1PAO_BEFORE=$MAX_NONCE_ERROR
+
+## log Functions
+
+ANVIL_LOCALHOST_RPC="http://localhost:8545"
+# log the nonce with error and exit the script
+log_nonce_error() {
+  echo "est" > /tmp/rce.txt
+  echo -e "\033[0;31mFoundation Upgrade Safe (FuS) [$Foundation_Upgrade_Safe] nonce: "$FUS_BEFORE".\033[0m"
+  echo -e "\033[0;31mFoundation Operation Safe (FoS) [$Foundation_Operation_Safe] nonce: "$FOS_BEFORE".\033[0m"
+  echo -e "\033[0;31mSecurity Council Safe (SC) [$Security_Council_Safe] nonce: "$SC_BEFORE".\033[0m"
+  echo -e "\033[0;31mL1ProxyAdminOwner (L1PAO) [$Proxy_Admin_Owner_Safe] nonce: "$L1PAO_BEFORE".\033[0m"
+  exit 1
+
+}
+
+log_debug() {
+    echo "[-] $(date '+%Y-%m-%d %H:%M:%S') [DEBUG] $1" | tee -a "$LOGFILE"
+}
+
+# Function to log warning messages
+log_warning() {
+    echo "[⚠️] $(date '+%Y-%m-%d %H:%M:%S') [WARNING] $1" | tee -a "$LOGFILE"
+}
+
+# Function to log error messages and exit the script  
+log_error() {
+    echo -e "\033[0;31m[❌] $(date '+%Y-%m-%d %H:%M:%S') [ERROR] $1\033[0m" | tee -a "$LOGFILE" >&2
+}
+
+log_info() {
+    echo -e "\033[0;34m[ℹ️] $(date '+%Y-%m-%d %H:%M:%S') [INFO] $1\033[0m" | tee -a "$LOGFILE"
+}
+
+# create a function that take the task_path and return the last folder
+
+### ADDRESS SHOULD BE GET FROM SUPERCHAIN-OPS IN THE FUTURE ###
+
+
+## ETHEREUM
+Security_Council_Safe=0xc2819DC788505Aac350142A7A707BF9D03E3Bd03
+Foundation_Upgrade_Safe=0x847B5c174615B1B7fDF770882256e2D3E95b9D92
+Foundation_Operation_Safe=0x9BA6e03D8B90dE867373Db8cF1A58d2F7F006b3A
+Proxy_Admin_Owner_Safe=0x5a0Aae59D09fccBdDb6C6CcEB07B7279367C3d2A
+DestroyAnvilAfterExecution=true
+##############################################################
 # Simulates a sequence of tasks for a given network by running them against an Anvil
 # fork with state overrides disabled.
 #
@@ -51,6 +106,84 @@ cleanup() {
   if [[ -f "$backup_file" ]]; then
     mv "$backup_file" "$simulation_file"
   fi
+
+  # Kill the anvil fork at the end if it was started by this script
+  if $DestroyAnvilAfterExecution; then
+    echo "Anvil is still open. To kill it, please use the command below:"
+    echo "ps aux | grep anvil | grep -v grep | awk '{print \$3}' | xargs kill"
+    #TODO: In the future, we should kill the anvil fork here?
+  fi
+}
+createFork() {
+  # Start a fork
+  # check if the port is already open
+  if lsof -Pi :8545 -sTCP:LISTEN -t >/dev/null ; then
+    log_info "Anvil is detected and running on port 8545, we use the current instance of anvil."
+    DestroyAnvilAfterExecution=false
+    # ps aux | grep anvil | grep -v grep | awk '{print $2}' | xargs kill
+
+  else  
+    log_info "No instance of anvil is detected, starting anvil fork on \"$ANVIL_LOCALHOST_RPC\" by forking $RPC_URL."
+    anvil -f $RPC_URL --fork-block-number 21573136 >> /tmp/anvil.logs & 
+    sleep 5
+  fi
+   
+}
+
+NonceDisplayModified(){
+  echo -e "\n$1"
+  if [[ $FUS_BEFORE -eq $MAX_NONCE_ERROR || $FOS_BEFORE -eq $MAX_NONCE_ERROR || $SC_BEFORE -eq $MAX_NONCE_ERROR ]]; then
+    log_error "Nonce values are not available for one or more safes please investigate." 
+    exit 99
+  fi
+  FUS_AFTER=$(cast call $Foundation_Upgrade_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  FOS_AFTER=$(cast call $Foundation_Operation_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  SC_AFTER=$(cast call $Security_Council_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  L1PAO_AFTER=$(cast call $Proxy_Admin_Owner_Safe "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+
+  if [[ $FUS_BEFORE -ne $FUS_AFTER ]]; then
+    echo -e "\033[0;32mFoundation Upgrade Safe (FuS) [$Foundation_Upgrade_Safe] nonce: "$FUS_AFTER" ("$FUS_BEFORE" -> "$FUS_AFTER").\033[0m" 
+  else 
+    echo "Foundation Upgrade Safe (FuS) [$Foundation_Upgrade_Safe] nonce: "$(cast call $Foundation_Upgrade_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  fi 
+
+
+  if [[ $FOS_BEFORE -ne $FOS_AFTER ]]; then
+    echo -e "\033[0;32mFoundation Operation Safe (FoS) [$Foundation_Operation_Safe] nonce: "$FOS_AFTER" ("$FOS_BEFORE" -> "$FOS_AFTER").\033[0m"
+  else 
+    echo "Foundation Operation Safe (FoS) [$Foundation_Operation_Safe] nonce: "$(cast call $Foundation_Operation_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  fi
+  if [[ $SC_BEFORE -ne $SC_AFTER ]]; then
+    echo -e "\033[0;32mSecurity Council Safe (SC) [$Security_Council_Safe] nonce: "$SC_AFTER" ("$SC_BEFORE" -> "$SC_AFTER").\033[0m"
+  else 
+    echo "Security Council Safe (SC) [$Security_Council_Safe] nonce: "$(cast call $Security_Council_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  fi 
+  if [[ $L1PAO_BEFORE -ne $L1PAO_AFTER ]]; then
+    echo -e "\033[0;32mL1ProxyAdminOwner (L1PAO) [$Proxy_Admin_Owner_Safe] nonce: "$L1PAO_AFTER" ("$L1PAO_BEFORE" -> "$L1PAO_AFTER").\033[0m"
+  else 
+    echo "L1ProxyAdminOwner (L1PAO) [$Proxy_Admin_Owner_Safe] nonce: "$(cast call $Proxy_Admin_Owner_Safe "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  fi
+
+
+  # echo "Foundation Upgrade Safe (FuS) [$Foundation_Upgrade_Safe] nonce: "$(cast call $Foundation_Upgrade_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  # echo "Foundation Operation Safe (FoS) [$Foundation_Operation_Safe] nonce: "$(cast call $Foundation_Operation_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  # echo "Security Council Safe (SC) [$Security_Council_Safe] nonce: "$(cast call $Security_Council_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+  # echo "L1ProxyAdminOwner (L1PAO) [$Proxy_Admin_Owner_Safe] nonce: "$(cast call $Proxy_Admin_Owner_Safe "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)"."
+}
+ 
+
+
+
+BeforeNonceDisplay(){
+  echo -e "\n$1"
+  FUS_BEFORE=$(cast call $Foundation_Upgrade_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  FOS_BEFORE=$(cast call $Foundation_Operation_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  SC_BEFORE=$(cast call $Security_Council_Safe  "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  L1PAO_BEFORE=$(cast call $Proxy_Admin_Owner_Safe "nonce()(uint256)" --rpc-url $ANVIL_LOCALHOST_RPC)
+  echo "Foundation Upgrade Safe (FuS) [$Foundation_Upgrade_Safe] nonce: "$FUS_BEFORE"."
+  echo "Foundation Operation Safe (FoS) [$Foundation_Operation_Safe] nonce: "$FOS_BEFORE"."
+  echo "Security Council Safe (SC) [$Security_Council_Safe] nonce: "$SC_BEFORE"."
+  echo "L1ProxyAdminOwner (L1PAO) [$Proxy_Admin_Owner_Safe] nonce: "$L1PAO_BEFORE"."
 }
 
 # Find unique task folder(s) for a given task ID
@@ -82,12 +215,10 @@ fi
 
 network="$1"
 task_ids="$2"
-
 # Determine root directory.
 base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 root_dir="$base_dir/../.."
 task_base_dir="${root_dir}/tasks/${network}"
-
 # Verify task_base_dir exists.
 if [[ ! -d "$task_base_dir" ]]; then
   error_exit "Task base directory '$task_base_dir' does not exist."
@@ -100,29 +231,78 @@ for task_id in $task_ids; do
   task_folders+=("$matching_folders")
 done
 
-echo "Simulating the following tasks in order:"
+source ${task_folders[0]}/.env
+RPC_URL=$ETH_RPC_URL
+unset ETH_RPC_URL
+log_info "Simulating the following tasks in order:"
+
+echo "-------------------------------------------------------"
 for task_folder in "${task_folders[@]}"; do
   echo "  $(realpath "$task_folder")"
 done
-
+echo "-------------------------------------------------------"
+read -p "Are the tasks above are correct? Type 'yes' to proceed with simulation: " confirmation
+if [[ "${confirmation}" != "yes" ]]; then
+  log_info "Simulation aborted by user."
+  exit 0
+fi
+# Create the anvil Fork 
+createFork
 # Disable state overrides and execute tasks.
 disable_state_overrides
 export SIMULATE_WITHOUT_LEDGER=1
+
 for task_folder in "${task_folders[@]}"; do
-  echo -e "\n---- Simulating task $task_folder ----"
+  execution=""
+  echo -e "\n---- Simulating task \"$(basename "$task_folder")\"----"
 
   pushd "$task_folder" >/dev/null || error_exit "Failed to navigate to '$task_folder'."
-
+  # add the RPC_URL to the .env file
+  # echo "ETH_RPC_URL=ANVIL_LOCALHOST_RPC" >> "${PWD}/.env" # Replace with the anvil fork URL
   if [[ -f "${task_folder}/NestedSignFromJson.s.sol" ]]; then
-    echo "Task type: nested"
-    # TODO This currently hardcodes the council but we should also run as Foundation.
-    just --dotenv-path "${PWD}/.env" --justfile "${root_dir}/nested.just" simulate council
-  else
-    echo "Task type: single"
-    just --dotenv-path "${PWD}/.env" --justfile "${root_dir}/single.just" simulate
-  fi
+    log_info "Task type: nested" 
+    BeforeNonceDisplay "(🟧) Before Simulation Nonce Values (🟧)"
+    approvalhashcouncil=$(just \
+      --dotenv-path "${PWD}/.env" \
+      --justfile "${root_dir}/nested.just" \
+      approvehash_in_anvil council)
+   
+    approvalhashfoundation=$(just \
+      --dotenv-path "${PWD}/.env" \
+      --justfile "${root_dir}/nested.just" \
+      approvehash_in_anvil foundation)
 
+    if [[ $approvalhashcouncil == *"GS025"* ]]; then
+      log_error "Execution contains "GS025" meaning the task $task_folder failed during the council approval, please check the nonces below:"
+      log_nonce_error 
+      exit 99
+    fi
+    if [[ $approvalhashfoundation == *"GS025"* ]]; then
+     log_error "Execution contains "GS025" meaning the task $task_folder failed during the foundation approval, please check the nonces below:"
+     log_nonce_error 
+     exit 99
+    fi
+    execution=$(just\
+       --dotenv-path "${PWD}/.env" \
+       --justfile "${root_dir}/nested.just" \
+       execute_in_anvil 0)
+
+  else
+    log_info "Task type detected: single"
+    BeforeNonceDisplay "(🟧) Before Simulation Nonce Values (🟧)"
+    simulate=$(just --dotenv-path "${PWD}/.env" --justfile "${root_dir}/single.just" approvehash_in_anvil 0)
+    execution=$(just --dotenv-path "${PWD}/.env" --justfile "${root_dir}/single.just" execute_in_anvil 0)
+    if [[ $execution == *"GS025"* ]]; then
+     log_error "Execution contains GS025 meaning the task $task_folder failed."
+     exit 99
+    fi
+  fi 
+
+  
+  sleep 0.2
+  NonceDisplayModified "(🟩) After Simulation Nonce Values (🟩)"
+  echo -e "\n---- End of Simulation for task \"$(basename "$task_folder")\" ----"
   popd >/dev/null || error_exit "Failed to return to previous directory."
 done
 
-echo ✅ Success!
+log_info "✅ All tasks has been simulated with success!! ✅"
