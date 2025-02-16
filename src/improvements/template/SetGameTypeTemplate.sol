40146e1194e26fcd965813c025cd60159c137b22
Elliot <elliotfriedman3@gmail.com>
2025-02-15 21:07:25 -0800

    add multisig to _taskStorageWrites
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/improvements/template/SetGameTypeTemplate.sol
+++ src/improvements/template/SetGameTypeTemplate.sol
@@ -39,8 +39,9 @@ contract SetGameTypeTemplate is MultisigTask {
     /// @notice Returns the storage write permissions required for this task
     /// @return Array of storage write permissions
     function _taskStorageWrites() internal pure override returns (string[] memory) {
-        string[] memory storageWrites = new string[](1);
+        string[] memory storageWrites = new string[](2);
         storageWrites[0] = "OptimismPortalProxy";
+        storageWrites[1] = "Challenger";
         return storageWrites;
     }
 
