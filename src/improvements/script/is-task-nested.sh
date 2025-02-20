e7db9831c4b5880d7bb4b83fc024fa84da1ca551
Blaine Malone <blainemalone01@gmail.com>
2025-02-20 10:05:10 -0500

    feat: refactored logic that can be reused later.

--- src/improvements/script/is-task-nested.sh
+++ src/improvements/script/is-task-nested.sh
@@ -6,29 +6,11 @@ isNestedSafe() {
     
     local root_dir
     root_dir="$(git rev-parse --show-toplevel)"
-    local template_dir="$root_dir/src/improvements/template"
     local script_dir="$root_dir/src/improvements/script"
-    local template_name
-    template_name="$(yq -r '.templateName' "$config_file_path")"
-    echo "Template name: $template_name"
     
-    # Opting to only get the first L2 chain id and assume all L2 chains have the same safeAddressString value.
-    # It's not the job of this script to validate the config file.
-    local l2_chain_id
-    l2_chain_id="$(yq -r '.l2chains[0].chainId' "$config_file_path")"
-    echo "L2 chain id: $l2_chain_id"
-
-    local safe_address_string
-    safe_address_string="$(forge script "$template_dir/${template_name}.sol" --sig "safeAddressString" \
-                          | sed -n 's/.*string "\(.*\)".*/\1/p')"
-    echo "Safe address string: $safe_address_string"
-
-    local remote_url="https://raw.githubusercontent.com/ethereum-optimism/superchain-registry/main/superchain/extra/addresses/addresses.json"
-    local addresses_json
-    addresses_json="$(curl -sL "$remote_url")"
+    # Get the owner address for the task
     local owner_address
-    owner_address="$(jq -r ".[\"$l2_chain_id\"].\"$safe_address_string\"" <<< "$addresses_json")"
-    echo "$safe_address_string: $owner_address"
+    owner_address="$(tail -n 1 < <("$script_dir"/get-task-owner.sh "$config_file_path"))"
 
     local contract_signer_count=0
     local eoa_signer_count=0
