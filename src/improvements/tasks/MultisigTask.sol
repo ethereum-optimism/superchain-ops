29490aafd0ffc68e6bc484ddaa406747fddd903a
Elliot <elliotfriedman3@gmail.com>
2025-02-21 00:12:17 -0500

    make _taskStorageWrites pure, pass accountAccesses to _validate function, make checkStateDiff view again
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/improvements/tasks/MultisigTask.sol
+++ src/improvements/tasks/MultisigTask.sol
@@ -148,7 +148,7 @@ abstract contract MultisigTask is Test, Script {
 
     /// @notice abstract function to be implemented by the inheriting contract
     /// specifies the addresses that must have their storage written to
-    function _taskStorageWrites() internal virtual returns (string[] memory);
+    function _taskStorageWrites() internal pure virtual returns (string[] memory);
 
     /// @notice Runs the task with the given configuration file path.
     /// Sets the address registry, initializes and simulates the single multisig
@@ -532,7 +532,7 @@ abstract contract MultisigTask is Test, Script {
         AddressRegistry.ChainInfo[] memory chains = addrRegistry.getChains();
 
         for (uint256 i = 0; i < chains.length; i++) {
-            _validate(chains[i].chainId);
+            _validate(chains[i].chainId, accountAccesses);
         }
 
         // check that state diff is as expected
@@ -825,7 +825,8 @@ abstract contract MultisigTask is Test, Script {
     /// @notice task specific validations
     /// @dev override to add additional task specific validations
     /// @param chainId The l2chainId
-    function _validate(uint256 chainId) internal view virtual;
+    /// @param accountAccesses returned from the simulate or execute run function
+    function _validate(uint256 chainId, VmSafe.AccountAccess[] memory accountAccesses) internal view virtual;
 
     /// @notice validate actions inclusion
     /// default implementation check for duplicate actions
@@ -1010,7 +1011,7 @@ abstract contract MultisigTask is Test, Script {
     /// check the state changes applied by the task. This function can check
     /// that only the nonce changed in the parent multisig when executing a task
     /// by checking the slot and address where the slot changed.
-    function checkStateDiff(VmSafe.AccountAccess[] memory accountAccesses) internal virtual {
+    function checkStateDiff(VmSafe.AccountAccess[] memory accountAccesses) internal view {
         console.log("Running assertions on the state diff");
         require(accountAccesses.length > 0, "No account accesses");
         address[] memory allowedAccesses = getAllowedStorageAccess();
