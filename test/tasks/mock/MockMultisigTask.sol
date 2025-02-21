5c1e130169bd84757eefd1e4e45cafd05aa4595d
Blaine Malone <blainemalone01@gmail.com>
2025-02-21 13:55:31 -0500

    fix: adding in unused import for another PR that I can add commits to.

--- test/tasks/mock/MockMultisigTask.sol
+++ test/tasks/mock/MockMultisigTask.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.15;
 import {IProxyAdmin} from "@eth-optimism-bedrock/interfaces/universal/IProxyAdmin.sol";
 import {Constants} from "@eth-optimism-bedrock/src/libraries/Constants.sol";
 import {IProxy} from "@eth-optimism-bedrock/interfaces/universal/IProxy.sol";
+import {VmSafe} from "forge-std/Vm.sol";
 
 import {MockTarget} from "test/tasks/mock/MockTarget.sol";
 import {MultisigTask} from "src/improvements/tasks/MultisigTask.sol";
