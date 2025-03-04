e4105ec5f7eeb6a9b298e637a11bb209f0159747
Blaine Malone <blainemalone01@gmail.com>
2025-03-04 15:52:46 -0500

    fix: refactor to template for opcm v200 work.

--- test/tasks/mock/template/IncorrectGasConfigTemplate2.sol
+++ test/tasks/mock/template/IncorrectGasConfigTemplate2.sol
@@ -6,7 +6,7 @@ import {GasConfigTemplate} from "test/tasks/mock/template/GasConfigTemplate.sol"
 /// @title IncorrectGasConfigTemplate2
 /// @notice not all allowed storages writes are written to
 contract IncorrectGasConfigTemplate2 is GasConfigTemplate {
-    function _taskStorageWrites(string memory) internal pure override returns (string[] memory) {
+    function _taskStorageWrites() internal pure override returns (string[] memory) {
         string[] memory storageWrites = new string[](1);
         /// expected to be written to
         storageWrites[0] = "SystemConfigOwner";
