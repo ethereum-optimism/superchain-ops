6ebad7ffdf81776a8f09cc5d639c61d2d17e3473
prateek <prateekgupta105@gmail.com>
2025-03-05 01:13:20 +0530

    load hardcoded addresses simple registry

--- src/improvements/SimpleAddressRegistry.sol
+++ src/improvements/SimpleAddressRegistry.sol
@@ -3,11 +3,12 @@ pragma solidity 0.8.15;
 
 import {Vm} from "forge-std/Vm.sol";
 import {stdToml} from "forge-std/StdToml.sol";
+import {StdChains} from "forge-std/StdChains.sol";
 
 /// @notice This contract provides a simple key-value store for addresses which are read in from
 /// the config TOML file. It expects an `[addresses]` section in the TOML file, with each key
 /// being the identifier for the address, and the value being the address.
-contract SimpleAddressRegistry {
+contract SimpleAddressRegistry is StdChains {
     using stdToml for string;
 
     address private constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
@@ -29,15 +30,16 @@ contract SimpleAddressRegistry {
             string memory key = _identifiers[i];
             address who = toml.readAddress(string.concat(".addresses.", key));
 
-            require(bytes(key).length > 0, "SimpleAddressRegistry: empty key");
-            require(who != address(0), string.concat("SimpleAddressRegistry: zero address for ", key));
-            require(addresses[key] == address(0), string.concat("SimpleAddressRegistry: duplicate key ", key));
-            require(bytes(keys[who]).length == 0, string.concat("SimpleAddressRegistry: address already registered"));
-
-            addresses[key] = who;
-            keys[who] = key;
-            vm.label(who, key);
+            _registerAddress(key, who);
         }
+
+        string memory chainKey;
+        if (block.chainid == getChain("mainnet").chainId) chainKey = ".eth";
+        else if (block.chainid == getChain("sepolia").chainId) chainKey = ".sep";
+        else if (block.chainid == getChain("optimism").chainId) chainKey = ".oeth";
+        else revert(string.concat("SimpleAddressRegistry: Unknown task chain ID ", vm.toString(block.chainid)));
+
+        _loadHardcodedAddresses(chainKey);
     }
 
     /// @notice Retrieves an address by its contract identifier.
@@ -53,5 +55,31 @@ contract SimpleAddressRegistry {
             bytes(identifier_).length > 0,
             string.concat("SimpleAddressRegistry: identifier not found for ", vm.toString(_who))
         );
+    }
+
+    /// @notice Reads in hardcoded addresses from the addresses.toml file and registers them.
+    function _loadHardcodedAddresses(string memory chainKey) internal {
+        string memory toml = vm.readFile("./src/improvements/addresses.toml");
+        string[] memory keyStrings = vm.parseTomlKeys(toml, chainKey);
+        require(keyStrings.length > 0, string.concat("SimpleAddressRegistry: no keys found for ", chainKey));
+
+        for (uint256 i = 0; i < keyStrings.length; i++) {
+            string memory key = keyStrings[i];
+            address who = toml.readAddress(string.concat(chainKey, ".", key));
+
+            _registerAddress(key, who);
+        }
+    }
+
+    /// @notice Registers an address with a key.
+    function _registerAddress(string memory key, address who) internal {
+        require(bytes(key).length > 0, "SimpleAddressRegistry: empty key");
+        require(who != address(0), string.concat("SimpleAddressRegistry: zero address for ", key));
+        require(addresses[key] == address(0), string.concat("SimpleAddressRegistry: duplicate key ", key));
+        require(bytes(keys[who]).length == 0, string.concat("SimpleAddressRegistry: address already registered"));
+
+        addresses[key] = who;
+        keys[who] = key;
+        vm.label(who, key);
     }
 }
