e49548263286429a7a03650298dd64bb93543a26
Blaine Malone <blainemalone01@gmail.com>
2025-03-04 23:46:51 -0500

    fix: make internal state var

--- src/improvements/tasks/MultisigTask.sol
+++ src/improvements/tasks/MultisigTask.sol
@@ -39,7 +39,7 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
     address public parentMultisig;
 
     /// @notice struct to store allowed storage accesses read in from config file
-    EnumerableSet.AddressSet public _allowedStorageAccesses;
+    EnumerableSet.AddressSet internal _allowedStorageAccesses;
 
     /// @notice Struct to store information about an action
     /// @param target The address of the target contract
@@ -285,7 +285,7 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
         (addrRegistry, _parentMultisig, multicallTarget) = _configureTask(taskConfigFilePath);
 
         parentMultisig = address(_parentMultisig);
-
+        
         config.allowedStorageWriteAccesses = _taskStorageWrites();
         config.allowedStorageWriteAccesses.push(safeAddressString());
 
