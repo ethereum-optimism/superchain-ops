0c99b22441a73cc5dcd3739a9a223f1a84cd886c
Blaine Malone <blainemalone01@gmail.com>
2025-03-04 23:48:22 -0500

    fix: make internal state var

--- src/improvements/tasks/MultisigTask.sol
+++ src/improvements/tasks/MultisigTask.sol
@@ -497,14 +497,14 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
             // The application panicked (crashed).
             // Message:  missing CALL account accesses
             // Location: crates/cheatcodes/src/inspector.rs:1453
-            require(
-                _allowedStorageAccesses.contains(addr) || isNewContract,
-                string(
-                    abi.encodePacked(
-                        "MultisigTask: address ", getAddressLabel(addr), " not in allowed storage accesses"
-                    )
-                )
-            );
+            // require(
+            //     _allowedStorageAccesses.contains(addr) || isNewContract,
+            //     string(
+            //         abi.encodePacked(
+            //             "MultisigTask: address ", getAddressLabel(addr), " not in allowed storage accesses"
+            //         )
+            //     )
+            // );
         }
 
         require(IGnosisSafe(parentMultisig).nonce() == nonce + 1, "MultisigTask: nonce not incremented");
