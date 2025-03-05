78a3882edd40e11b2d714afdad6687517e2a4a19
Blaine Malone <blainemalone01@gmail.com>
2025-03-04 23:45:11 -0500

    fix: make public state var

--- src/improvements/tasks/MultisigTask.sol
+++ src/improvements/tasks/MultisigTask.sol
@@ -39,8 +39,7 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
     address public parentMultisig;
 
     /// @notice struct to store allowed storage accesses read in from config file
-    /// uses OpenZeppelin EnumerableSet for allowed storage accesses
-    EnumerableSet.AddressSet internal _allowedStorageAccesses;
+    EnumerableSet.AddressSet public _allowedStorageAccesses;
 
     /// @notice Struct to store information about an action
     /// @param target The address of the target contract
@@ -498,14 +497,14 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
             // The application panicked (crashed).
             // Message:  missing CALL account accesses
             // Location: crates/cheatcodes/src/inspector.rs:1453
-            // require(
-            //     _allowedStorageAccesses.contains(addr) || isNewContract,
-            //     string(
-            //         abi.encodePacked(
-            //             "MultisigTask: address ", getAddressLabel(addr), " not in allowed storage accesses"
-            //         )
-            //     )
-            // );
+            require(
+                _allowedStorageAccesses.contains(addr) || isNewContract,
+                string(
+                    abi.encodePacked(
+                        "MultisigTask: address ", getAddressLabel(addr), " not in allowed storage accesses"
+                    )
+                )
+            );
         }
 
         require(IGnosisSafe(parentMultisig).nonce() == nonce + 1, "MultisigTask: nonce not incremented");
