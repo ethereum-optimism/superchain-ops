1b3263c7c7748351aed5ab743a05984e19ca9d98
Elliot <elliotfriedman3@gmail.com>
2025-03-17 16:43:36 -0700

    move decimal parsing logic into library
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- src/improvements/template/FinanceTemplate.sol
+++ src/improvements/template/FinanceTemplate.sol
@@ -12,6 +12,8 @@ import {LibString} from "@solady/utils/LibString.sol";
 import {ERC20} from "@solady/tokens/ERC20.sol";
 import {stdToml} from "lib/forge-std/src/StdToml.sol";
 import {EnumerableSet} from "lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
+import {StringParser} from "src/libraries/StringParser.sol";
+import {DecimalNormalization} from "src/libraries/DecimalNormalization.sol";
 
 /// @notice Template contract for enabling finance transactions
 contract FinanceTemplate is SimpleBase {
@@ -20,13 +22,6 @@ contract FinanceTemplate is SimpleBase {
     using stdToml for string;
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    /// @notice Struct to hold parsed amount components
-    struct AmountComponents {
-        uint256 wholePart;
-        uint256 decimalPart;
-        uint8 decimalPlaces;
-        bool hasDecimals;
-    }
 
     /// @notice Operation struct as read in from the `config.toml` file
     /// @param amount The amount of tokens for the operation, specified
@@ -94,87 +89,16 @@ contract FinanceTemplate is SimpleBase {
         return new string[](0);
     }
 
-    /// @notice add the decimals to the given amount
-    /// @param amount The amount to scale
-    /// @param decimals The number of decimals to scale by
-    function scaleDecimals(uint256 amount, uint8 decimals) public pure returns (uint256) {
-        return amount * (10 ** uint256(decimals));
-    }
-
-    /// @notice Validates that the amount string has a valid format
-    /// @param components The split components of the amount string
-    /// reverts if components are invalid
-    function validateAmountFormat(string[] memory components) internal pure {
-        // Check for invalid format (more than one decimal point)
-        require(components.length <= 2, "invalid amount");
-
-        // If there's a decimal part, ensure it's not empty
-        if (components.length == 2) {
-            require(bytes(components[1]).length != 0, "decimals cannot be 0");
-        }
-    }
-
-    /// @notice Checks if the amount represents a non-zero value
-    /// @param wholePart The whole number part of the amount
-    /// @param decimalPart The decimal part of the amount
-    /// @return True if either part results in a non-zero value
-    function isNonZeroAmount(uint256 wholePart, uint256 decimalPart) internal pure returns (bool) {
-        return wholePart > 0 || decimalPart > 0;
-    }
-
-    /// @notice Parses a string amount into its components
-    /// @param amount The amount string to parse
-    /// @return components The parsed amount components
-    function _parseAmountComponents(string memory amount) internal pure returns (AmountComponents memory components) {
-        string[] memory stringComponents = amount.split(".");
-        validateAmountFormat(stringComponents);
-
-        components.wholePart = vm.parseUint(stringComponents[0]);
-        components.hasDecimals = stringComponents.length == 2;
-
-        if (components.hasDecimals) {
-            components.decimalPart = vm.parseUint(stringComponents[1]);
-            components.decimalPlaces = uint8(bytes(stringComponents[1]).length);
-            require(components.decimalPlaces <= 18, "decimals must be less than or equal to 18");
-        }
-
-        // Ensure the final amount is non-zero
-        require(isNonZeroAmount(components.wholePart, components.decimalPart), "amount must be non-zero");
-
-        return components;
-    }
-
-    /// @notice returns the amount with the length of the decimals parsed
-    /// @param amount The amount to parse
-    /// @return The amount and the number of decimals
-    function parseDecimals(string memory amount) public pure returns (uint256, uint8) {
-        AmountComponents memory components = _parseAmountComponents(amount);
-
-        if (!components.hasDecimals || components.decimalPart == 0) {
-            return (components.wholePart, 0);
-        }
-
-        uint256 finalAmount = scaleDecimals(components.wholePart, components.decimalPlaces) + components.decimalPart;
-
-        return (finalAmount, components.decimalPlaces);
-    }
-
     /// @notice converts string to a scaled up token amount in decimal form
     /// @param amount string representation of the amount
     /// @param token address of the token to send, used discovering the decimals
     /// returns the scaled up token amount
     function getTokenAmount(string memory amount, address token) public view returns (uint256) {
-        // Get the scaled amount and decimals using parseDecimals
-        (uint256 scaledAmount, uint8 parsedDecimals) = parseDecimals(amount);
-
         // Get token decimals
         uint8 tokenDecimals = ERC20(token).decimals();
-
-        // Ensure amount decimals don't exceed token decimals
-        require(parsedDecimals <= tokenDecimals, "amount decimals must be less than or equal to token decimals");
-
-        // Scale the amount to match token decimals
-        return scaleDecimals(scaledAmount, tokenDecimals - parsedDecimals);
+        
+        // Use the DecimalNormalization library to normalize the token amount
+        return DecimalNormalization.normalizeTokenAmount(amount, tokenDecimals);
     }
 
     /// @notice Sets up the template with module configuration from a TOML file
