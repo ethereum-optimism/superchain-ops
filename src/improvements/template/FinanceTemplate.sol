914cefb7f45413a39fbd9d391fe776449589c572
prateek <prateekgupta105@gmail.com>
2025-03-18 21:39:38 +0530

    bubble up return data

--- src/improvements/template/FinanceTemplate.sol
+++ src/improvements/template/FinanceTemplate.sol
@@ -140,8 +140,8 @@ contract FinanceTemplate is SimpleBase {
                 IERC20(token).safeDecreaseAllowance(target, operation.amount);
             } else if (operationTypeEnum == OperationType.Transfer) {
                 if (token == simpleAddrRegistry.get("ETH")) {
-                    (bool success,) = target.call{value: operation.amount}("");
-                    require(success, "Transfer failed");
+                    (bool success, bytes memory data) = target.call{value: operation.amount}("");
+                    require(success, string.concat("Transfer failed: ", vm.toString(data)));
                 } else {
                     IERC20(token).safeTransfer(target, operation.amount);
                 }
