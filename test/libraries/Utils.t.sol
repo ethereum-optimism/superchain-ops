29a6c070eacefc7bec7c346661ef0309a3f6ee65
Michael Amadi <amadimichaeld@gmail.com>
2025-03-26 23:40:11 +0100

    add more tests

--- test/libraries/Utils.t.sol
+++ test/libraries/Utils.t.sol
@@ -10,27 +10,42 @@ contract UtilsTest is Test {
         assertEq(Utils.isFeatureEnabled(randomFeature), false);
 
         // Set it to true
-        vm.setEnv(randomFeature, "true");
-        assertEq(Utils.isFeatureEnabled(randomFeature), true);
+        setAndAssert(randomFeature, "true", true);
+        setAndAssert(randomFeature, "True", true);
+        setAndAssert(randomFeature, "TRUE", true);
+
+        // Boolean with whitespace around it
+        setAndAssert(randomFeature, " true ", false);
 
         // Set it to false
-        vm.setEnv(randomFeature, "false");
-        assertEq(Utils.isFeatureEnabled(randomFeature), false);
+        setAndAssert(randomFeature, "false", false);
+        setAndAssert(randomFeature, "False", false);
+        setAndAssert(randomFeature, "FALSE", false);
+
+        // Empty string
+        setAndAssert(randomFeature, "", false);
 
         // Set it to a non-boolean value
-        vm.setEnv(randomFeature, vm.toString(vm.randomBytes(bound(vm.randomUint(), 1, 10))));
-        assertEq(Utils.isFeatureEnabled(randomFeature), false);
+        setAndAssert(randomFeature, vm.toString(vm.randomBytes(bound(vm.randomUint(), 1, 10))), false);
 
         // Set it to 1
-        vm.setEnv(randomFeature, "1");
-        assertEq(Utils.isFeatureEnabled(randomFeature), true);
+        setAndAssert(randomFeature, "1", true);
 
         // Set it to 0
-        vm.setEnv(randomFeature, "0");
-        assertEq(Utils.isFeatureEnabled(randomFeature), false);
+        setAndAssert(randomFeature, "0", false);
 
         // Set it to a number > 1
-        vm.setEnv(randomFeature, vm.toString(bound(vm.randomUint(), 2, type(uint256).max)));
-        assertEq(Utils.isFeatureEnabled(randomFeature), false);
+        setAndAssert(randomFeature, vm.toString(bound(vm.randomUint(), 2, type(uint256).max)), false);
+
+        // Hex input
+        setAndAssert(randomFeature, "0x1", true);
+
+        // Number prefixed with 0
+        setAndAssert(randomFeature, "01", true);
+    }
+
+    function setAndAssert(string memory feature, string memory value, bool expected) internal {
+        vm.setEnv(feature, value);
+        assertEq(Utils.isFeatureEnabled(feature), expected);
     }
 }
