4c6f3df0446f1072cc9fd334be0bc288bec1d6ae
alcueca <alberto@yield.is>
2025-03-28 10:07:21 +0000

    fix: remove redundant logging

--- src/improvements/script/simulate-task.sh
+++ src/improvements/script/simulate-task.sh
@@ -18,19 +18,17 @@ simulate_task() {
     fi
     
     rpcUrl=$("$root_dir"/src/improvements/script/get-rpc-url.sh "$task")
-    echo "Task: $task"
+
     is_nested=$(forge script "$root_dir"/src/improvements/tasks/TaskRunner.sol --sig "isNestedTask(string)" "$task/config.toml" --rpc-url "$rpcUrl" --json | jq -r '.returns["0"].value')
     echo "Is nested: $is_nested"
     pushd "$task" > /dev/null
     if [ "$is_nested" = "true" ]; then
-        echo "Simulating nested task: $task"
         if [ -z "$nested_safe_name" ]; then
             echo "Error: this task requires a nested safe name e.g. foundation, council, chain-governor."
             exit 1
         fi
         SIMULATE_WITHOUT_LEDGER=1 just --dotenv-path "$(pwd)"/.env --justfile "$nested_just_file" simulate "$nested_safe_name"
     else
-        echo "Simulating single task: $task"
         SIMULATE_WITHOUT_LEDGER=1 just --dotenv-path "$(pwd)"/.env --justfile "$single_just_file" simulate
     fi
 
