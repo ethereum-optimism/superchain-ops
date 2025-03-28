1f3d0f53c8f073556bdb5abf8caf707f4123ffbc
JosepBove <josep@oplabs.co>
2025-03-28 15:33:05 +0100

    more errors

--- src/improvements/template/OPCMUpgradeV200.sol
+++ src/improvements/template/OPCMUpgradeV200.sol
@@ -123,6 +123,15 @@ contract OPCMUpgradeV200 is OPCMBaseTask {
             // In future versions of StandardValidator, we shouldn't be expecting any errors.
             string memory reasons = STANDARD_VALIDATOR_V200.validate({_input: input, _allowFailure: true});
 
+            // PDDG-50: The current mipsImpl on sepolia was deployed without using deterministic create2 deployments, so a new one was deployed.
+            // PDDG-DWETH-40: Delayed WETH delay in permissioned game is not set to 1 week.
+            // PDDG-ANCHORP-40: The anchor state registry's permissioned root is not 0xdead000000000000000000000000000000000000000000000000000000000000
+            // PLDG-50: The current mipsImpl on sepolia was deployed without using deterministic create2 deployments, so a new one was deployed.
+            // PLDG-DWETH-40: Delayed WETH delay in permissionless game is not set to 1 week. Config mismatch.
+            // PLDG-ANCHORP-40: Anchor state registry root must match expected dead root (for permissionless dispute game) - This does not apply to any chain more than 1 week old
+            string memory expectedErrors_11155420 =
+                "PDDG-50,PDDG-DWETH-40,PDDG-ANCHORP-40,PLDG-50,PLDG-DWETH-40,PLDG-ANCHORP-40";
+
             // PROXYA-10: Proxy admin owner must be l1PAOMultisig - This is OK because it is checking for the OP Mainnet PAO.
             // DF-30: Dispute factory owner must be l1PAOMultisig - It is checking for the OP Mainnet PAO.
             // PDDG-ANCHORP-40: Anchor state registry root must match expected dead root (for permissioned dispute game) - This does not apply to any chain more than 1 week old.
@@ -133,7 +142,10 @@ contract OPCMUpgradeV200 is OPCMBaseTask {
             string memory expectedErrors_1301 =
                 "PROXYA-10,DF-30,PDDG-DWETH-30,PDDG-ANCHORP-40,PDDG-120,PLDG-DWETH-30,PLDG-ANCHORP-40";
 
-            require(reasons.eq(expectedErrors_1301), string.concat("Unexpected errors: ", reasons));
+            require(
+                reasons.eq(expectedErrors_1301) || reasons.eq(expectedErrors_11155420),
+                string.concat("Unexpected errors: ", reasons)
+            );
         }
     }
 
