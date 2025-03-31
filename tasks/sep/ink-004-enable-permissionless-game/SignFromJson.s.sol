7a4b9f773855db43ec759bc466885906658754c8
Wazabie <48911235+Wazabie@users.noreply.github.com>
2025-03-31 20:02:06 +0200

    Update SignFromJson.s.sol

--- tasks/sep/ink-004-enable-permissionless-game/SignFromJson.s.sol
+++ tasks/sep/ink-004-enable-permissionless-game/SignFromJson.s.sol
@@ -1,31 +1,24 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
 import {SignFromJson as OriginalSignFromJson} from "script/SignFromJson.s.sol";
+import {SuperchainRegistry} from "script/verification/Verification.s.sol";
 import {Simulation} from "@base-contracts/script/universal/Simulation.sol";
 import {OptimismPortal2, IDisputeGame} from "@eth-optimism-bedrock/src/L1/OptimismPortal2.sol";
 import {Types} from "@eth-optimism-bedrock/scripts/libraries/Types.sol";
 import {Vm, VmSafe} from "forge-std/Vm.sol";
 import {console2 as console} from "forge-std/console2.sol";
-import {stdToml} from "forge-std/StdToml.sol";
 import {LibString} from "solady/utils/LibString.sol";
 import {GnosisSafe} from "safe-contracts/GnosisSafe.sol";
 import "@eth-optimism-bedrock/src/dispute/lib/Types.sol";
 
-contract SignFromJson is OriginalSignFromJson {
+contract SignFromJson is OriginalSignFromJson, SuperchainRegistry {
     using LibString for string;
 
-    // Chains for this task.
-    string constant l1ChainName = "sepolia";
-    string l2ChainName = vm.envString("L2_CHAIN_NAME");
-
-    Types.ContractSet proxies;
+    constructor() SuperchainRegistry("sepolia", vm.envString("L2_CHAIN_NAME"), "v1.8.0-rc.4") {}
 
     /// @notice Sets up the contract
-    function setUp() public {
-        proxies = _getContractSet();
-    }
+    function setUp() public {}
 
     function checkRespectedGameType() internal view {
         OptimismPortal2 portal = OptimismPortal2(payable(proxies.OptimismPortal));
@@ -50,23 +43,5 @@ contract SignFromJson is OriginalSignFromJson {
         checkRespectedGameType();
 
         console.log("All assertions passed!");
-    }
-
-    /// @notice Reads the contract addresses from lib/superchain-registry/superchain/configs/${l1ChainName}/${l2ChainName}.toml
-    function _getContractSet() internal view returns (Types.ContractSet memory _proxies) {
-        string memory chainConfig;
-
-        // Read chain-specific config toml file
-        string memory path = string.concat(
-            "/tasks/sep/ink-004-enable-permissionless-game/", l2ChainName, ".toml"
-        );
-        try vm.readFile(string.concat(vm.projectRoot(), path)) returns (string memory data) {
-            chainConfig = data;
-        } catch {
-            revert(string.concat("Failed to read ", path));
-        }
-
-        // Read the chain-specific OptimismPortalProxy address
-        _proxies.OptimismPortal = stdToml.readAddress(chainConfig, "$.addresses.OptimismPortalProxy");
     }
 }
