578baa253f8d7df0ad6bbc2ab199934f18719a1c
Blaine Malone <blainemalone01@gmail.com>
2025-04-08 15:13:29 -0400

    fix: template errors.

--- src/improvements/template/OPCMUpgradeV300.sol
+++ src/improvements/template/OPCMUpgradeV300.sol
@@ -120,6 +120,16 @@ contract OPCMUpgradeV300 is OPCMTaskBase {
             // PLDG-ANCHORP-40: The anchor state registry's permissionless root is not 0xdead000000000000000000000000000000000000000000000000000000000000
             string memory expectedErrors_11155420 = "PDDG-ANCHORP-40,PLDG-ANCHORP-40";
 
+
+            // PROXYA-10: Proxy admin owner must be l1PAOMultisig - This is OK because it is checking for the OP Sepolia PAO.
+            // DF-30: Dispute factory owner must be l1PAOMultisig - It is checking for the OP Sepolia PAO.
+            // PDDG-50: The current mipsImpl on sepolia was deployed without using deterministic create2 deployments, so a new one was deployed.
+            // PDDG-DWETH-30: Permissioned dispute game's DelayedWETH owner must be l1PAOMultisig - This is OK because it is not checking for the Sepolia PAO.
+            // PDDG-ANCHORP-40: The anchor state registry's permissioned root is not 0xdead000000000000000000000000000000000000000000000000000000000000
+            // PDDG-120: 
+            // PLDG-50: 
+            // PLDG-DWETH-30:
+            // PLDG-ANCHORP-40: Anchor state registry root must match expected dead root (for permissionless dispute game) - This does not apply to any chain more than 1 week old.
             string memory expectedErrors_Base_84532 = "PROXYA-10,DF-30,PDDG-50,PDDG-DWETH-30,PDDG-ANCHORP-40,PDDG-120,PLDG-50,PLDG-DWETH-30,PLDG-ANCHORP-40"; 
 
             require(reasons.eq(expectedErrors_11155420) || reasons.eq(expectedErrors_Base_84532), string.concat("Unexpected errors: ", reasons));
