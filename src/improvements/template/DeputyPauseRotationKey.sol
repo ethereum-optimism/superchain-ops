2a82f2bbad960a147288e927ccd91ab099fb5f1a
ethnical <ethnicalnightmare@gmail.com>
2025-04-09 00:54:59 +0200

    chore: add the template

--- src/improvements/template/DeputyPauseRotationKey.sol
+++ src/improvements/template/DeputyPauseRotationKey.sol
@@ -15,10 +15,7 @@ import {
 contract DeputyPauseRotationKey is L2TaskBase {
     using stdToml for string;
     /// @notice Struct to store inputs for deputyPauseModule.setDeputy() function L1. 
-    struct DeputyPauseModuleSetDeputyInputs {
-        address new_deputy;
-        bytes new_deputy_signature;
-    }
+    
     // Store the inputs
     address new_deputy;
     bytes new_deputy_signature;
@@ -40,25 +37,22 @@ contract DeputyPauseRotationKey is L2TaskBase {
     function _templateSetup(string memory taskConfigFilePath) internal override {
         super._templateSetup(taskConfigFilePath);
 
+        string memory file = vm.readFile(taskConfigFilePath);
+        // newModule = vm.parseTomlAddress(file, ".newModule");
+        // assertNotEq(newModule.code.length, 0, "new module must have code");
         // Load the inputs from the task config
-        DeputyPauseModuleSetDeputyInputs memory inputs = abi.decode(
-            vm.parseBytes(vm.parseToml(taskConfigFilePath).get(".inputs")), 
-            (DeputyPauseModuleSetDeputyInputs)
-        );
-
-        // Store the inputs
-        new_deputy = inputs.new_deputy;
-        new_deputy_signature = inputs.new_deputy_signature;
+        new_deputy = vm.parseTomlAddress(file, ".newDeputy");
+        new_deputy_signature = vm.parseTomlBytes(file, ".newDeputySignature");
     }
 
     /// @notice Write the calls that you want to execute for the task.
     function _build() internal override {
         // Load the DeputyPauseModule contract.
         IDeputyPauseModule dpm = IDeputyPauseModule(superchainAddrRegistry.get("DeputyPauseModule"));
-        // 1. In the future, check that the DeputyPauseModule address is the one that is enabled in the foundation safe. 
+        // 1. In the future, check that the DeputyPauseModule address is the one that is enabled in the foundation safe.
         // assertEq(fos.enabledmodule(address(dpm)), true, "ERR100: DeputyPauseModule is should be enabled");
         // 2. Check that the input are valid.     
-        assertEq(dpm.deputy(), address(0), "ERR101: DeputyPauseModule should have no deputy");
+        assertNotEq(dpm.deputy(), address(0), "ERR101: DeputyPauseModule should have already a deputy set in the past.");
         assertNotEq(dpm.deputy(), new_deputy, "ERR102: DeputyPauseModule should have a new deputy");
         
         // Check that the rotation signature is valid
@@ -73,7 +67,7 @@ contract DeputyPauseRotationKey is L2TaskBase {
         // This serves as a pre-flight check before actual execution
         
         // Verify the new_deputy_signature is exactly 65 bytes (r, s, v format)
-        require(new_deputy_signature.length, 65, "ERR104: Invalid signature length");
+        require(new_deputy_signature.length == 65, "ERR104: Invalid signature length");
         
         // 3. Rotate the new deputy and signature.
         dpm.setDeputy(new_deputy, new_deputy_signature);
