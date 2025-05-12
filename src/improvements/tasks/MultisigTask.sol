b2441a6d6175d777b65d98dd9319bd9dd07220ec
Maurelian <maurelian@protonmail.ch>
2025-05-12 12:38:34 -0400

    Enable reading safeAddressString from config

--- src/improvements/tasks/MultisigTask.sol
+++ src/improvements/tasks/MultisigTask.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+import { console2 as console } from "forge-std/console2.sol";
+
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {console} from "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
@@ -143,7 +145,7 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
 
     /// @notice Specifies the safe address string to run the template from. This string refers
     /// to a named contract, where the name is read from an address registry contract.
-    function safeAddressString() public pure virtual returns (string memory);
+    function safeAddressString() public view virtual returns (string memory);
 
     /// @notice Returns an array of strings that refer to contract names in the address registry.
     /// Contracts with these names are expected to have their storage written to during the task.
@@ -286,7 +288,7 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
     /// by performing various setup functions e.g. setting the address registry and multicall target.
     function _taskSetup(string memory taskConfigFilePath, address optionalChildMultisig) internal {
         require(bytes(config.safeAddressString).length == 0, "MultisigTask: already initialized");
-        config.safeAddressString = safeAddressString();
+        config.safeAddressString = getOrReadSafeAddressString(taskConfigFilePath);
         IGnosisSafe _parentMultisig; // TODO parentMultisig should be of type IGnosisSafe
         (addrRegistry, _parentMultisig, multicallTarget) = _configureTask(taskConfigFilePath);
 
@@ -294,7 +296,7 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
         childMultisig = optionalChildMultisig;
 
         config.allowedStorageKeys = _taskStorageWrites();
-        config.allowedStorageKeys.push(safeAddressString());
+        config.allowedStorageKeys.push(config.safeAddressString);
 
         _templateSetup(taskConfigFilePath);
         // Both parent and child nonce are set here.
@@ -309,6 +311,19 @@ abstract contract MultisigTask is Test, Script, StateOverrideManager {
 
         vm.label(AddressRegistry.unwrap(addrRegistry), "AddrRegistry");
         vm.label(address(this), "MultisigTask");
+    }
+
+    /// @notice Get the safe address string from the config file.
+    /// If the string is not found, use the value from the template.
+    function getOrReadSafeAddressString(string memory taskConfigFilePath) internal view returns (string memory) {
+        string memory file = vm.readFile(taskConfigFilePath);
+        try vm.parseTomlString(file, ".safeAddressString") returns (string memory _safeAddressString) {
+            console.log("Safe address string found in config file: %s", _safeAddressString);
+            return _safeAddressString;
+        } catch (bytes memory) {
+            console.log("Error parsing safeAddressString from config file, using value from template.");
+            return safeAddressString();
+        }
     }
 
     /// @notice Get the calldata to be executed by safe.
