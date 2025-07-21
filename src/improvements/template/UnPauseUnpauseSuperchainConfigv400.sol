49b770664acb7dcfe6df54893832a6f8faa9c3ae
ethnical <ethnicalnightmare@gmail.com>
2025-07-21 16:46:48 +0200

    fix: update the name of the template with the version

--- src/improvements/template/UnPauseUnpauseSuperchainConfigv400.sol
+++ src/improvements/template/UnPauseUnpauseSuperchainConfigv400.sol
@@ -8,8 +8,8 @@ import {L2TaskBase} from "src/improvements/tasks/types/L2TaskBase.sol";
 
 import {Action} from "src/libraries/MultisigTypes.sol";
 
-/// @title UnPauseSuperchainConfigAfterU16 template designed to unpause the SuperchainConfig contract.
-contract UnPauseSuperchainConfigAfterU16 is L2TaskBase {
+/// @title UnpauseSuperchainConfigv400 template designed to unpause the SuperchainConfig contract after the U16 upgrade.
+contract UnpauseSuperchainConfigv400 is L2TaskBase {
     using stdToml for string;
 
     /// @notice Identifier of the eth_lockbox used in the SuperchainConfig loaded from TOML and identifier is an address of 20 bytes that has an lockbox contract associated.
@@ -24,14 +24,22 @@ contract UnPauseSuperchainConfigAfterU16 is L2TaskBase {
     }
 
     /// @notice Returns string identifiers for addresses that are expected to have their storage written to.
-    function _taskStorageWrites() internal pure override returns (string[] memory) {
+    function _taskStorageWrites()
+        internal
+        pure
+        override
+        returns (string[] memory)
+    {
         string[] memory storageWrites = new string[](1);
         storageWrites[0] = "SuperchainConfig";
         return storageWrites;
     }
 
     /// @notice Sets up the template with implementation configurations from a TOML file.
-    function _templateSetup(string memory _taskConfigFilePath, address _rootSafe) internal override {
+    function _templateSetup(
+        string memory _taskConfigFilePath,
+        address _rootSafe
+    ) internal override {
         super._templateSetup(_taskConfigFilePath, _rootSafe);
         string memory file = vm.readFile(_taskConfigFilePath);
         identifier = vm.parseTomlAddress(file, ".identifier"); // Get the identifier of the eth_lockbox from the TOML file
@@ -47,11 +55,17 @@ contract UnPauseSuperchainConfigAfterU16 is L2TaskBase {
     }
 
     /// @notice This method performs all validations and assertions that verify the calls executed as expected.
-    function _validate(VmSafe.AccountAccess[] memory, Action[] memory, address) internal view override {
+    function _validate(
+        VmSafe.AccountAccess[] memory,
+        Action[] memory,
+        address
+    ) internal view override {
         // Validate that the SuperchainConfig contract is unpaused.
         // 1. check that the Superchain Config is not paused anymore with the identifier provided.
         assertEq(
-            sc.paused(identifier), false, "ERR100: SuperchainConfig should be unpaused for the identifier provided."
+            sc.paused(identifier),
+            false,
+            "ERR100: SuperchainConfig should be unpaused for the identifier provided."
         );
         if (
             identifier != address(0) // If the identifier is 0 this indicates a superchain-wide pause so we don't need to check the lockbox associated
@@ -65,7 +79,13 @@ contract UnPauseSuperchainConfigAfterU16 is L2TaskBase {
     }
 
     /// @notice Override to return a list of addresses that should not be checked for code length.
-    function _getCodeExceptions() internal view virtual override returns (address[] memory) {
+    function _getCodeExceptions()
+        internal
+        view
+        virtual
+        override
+        returns (address[] memory)
+    {
         address[] memory codeExceptions = new address[](0);
         return codeExceptions;
     }
