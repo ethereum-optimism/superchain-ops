c456d312e9097ac695b32458bfe52410ca5d85df
blaine <blainemalone01@gmail.com>
2025-09-23 13:29:04 -0400

    fix: get-rpc-url (#1220)

--- src/script/get-rpc-url.sh
+++ src/script/get-rpc-url.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NETWORK="${1:-}"
-if [ -z "${NETWORK}" ]; then
-    echo "Error: Must provide a network short name (e.g., eth, sep, oeth, opsep)." >&2
-    exit 1
-fi
-
-# The keys correspond to the task directory names and are official short names defined here: https://chainid.network/shortNameMapping.json
-# The values correspond to the RPC URL keys in foundry.toml
-declare -A net_to_key=(
-    [sep]=sepolia
-    [eth]=mainnet
-    [oeth]=opMainnet
-    [opsep]=opSepolia
-)
-
-key="${net_to_key[${NETWORK}]:-}"
-if [ -z "${key}" ]; then
-    echo "Error: Must provide a valid network, '${NETWORK}' is not valid." >&2
-    exit 1
-fi
-
-echo "${key}"
+case "${1:-}" in
+    eth)
+        echo "mainnet"
+        ;;
+    sep)
+        echo "sepolia"
+        ;;
+    oeth)
+        echo "opMainnet"
+        ;;
+    opsep)
+        echo "opSepolia"
+        ;;
+    "")
+        echo "Error: Must provide a network (eth|sep|oeth|opsep)" >&2
+        exit 1
+        ;;
+    *)
+        echo "Error: Invalid network '$1' (use eth|sep|oeth|opsep)" >&2
+        exit 1
+        ;;
+esac
