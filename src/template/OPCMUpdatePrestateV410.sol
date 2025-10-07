3c3fc5431eb3b97a31fdde59c3054e682d226081
Wazabie <48911235+Wazabie@users.noreply.github.com>
2025-10-07 16:43:13 +0200

    Update env gas

--- src/template/OPCMUpdatePrestateV410.sol
+++ src/template/OPCMUpdatePrestateV410.sol
@@ -108,13 +108,12 @@ contract OPCMUpdatePrestateV410 is OPCMTaskBase {
             address proxyAdmin = superchainAddrRegistry.getAddress("ProxyAdmin", chainId);
             address sysCfg = superchainAddrRegistry.getAddress("SystemConfigProxy", chainId);
 
-            IOPContractsManagerStandardValidator.ValidationInput memory input =
-                IOPContractsManagerStandardValidator.ValidationInput({
-                    proxyAdmin: IProxyAdmin(proxyAdmin),
-                    sysCfg: ISystemConfig(sysCfg),
-                    absolutePrestate: expAbsolutePrestate,
-                    l2ChainID: chainId
-                });
+            IOPContractsManagerStandardValidator.ValidationInput memory input = IOPContractsManagerStandardValidator.ValidationInput({
+                proxyAdmin: IProxyAdmin(proxyAdmin),
+                sysCfg: ISystemConfig(sysCfg),
+                absolutePrestate: expAbsolutePrestate,
+                l2ChainID: chainId
+            });
 
             string memory errors = STANDARD_VALIDATOR.validateWithOverrides({
                 _input: input,
