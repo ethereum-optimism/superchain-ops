9c5f1a2237dfa98bd91b8297b239572d4a63c1d7
IamFlux <175354924+0xiamflux@users.noreply.github.com>
2025-10-16 09:14:44 -0400

    test: add basic validation for fees depositor template (#32)

--- src/template/DeployFeesDepositor.sol
+++ src/template/DeployFeesDepositor.sol
@@ -15,6 +15,9 @@ import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 interface IFeesDepositor {
     function initialize(uint96 _minDepositAmount, address _l2Recipient, address _messenger, uint32 _gasLimit)
         external;
+    function minDepositAmount() external view returns (uint96);
+    function l2Recipient() external view returns (address);
+    function gasLimit() external view returns (uint32);
 }
 
 /// @notice Interface for the CREATE2 deployer contract.
@@ -122,7 +125,14 @@ contract DeployFeesDepositor is SimpleTaskBase {
     }
 
     /// @notice This method performs all validations and assertions that verify the calls executed as expected.
-    function _validate(VmSafe.AccountAccess[] memory, Action[] memory, address) internal pure override {}
+    function _validate(VmSafe.AccountAccess[] memory, Action[] memory, address) internal view override {
+        require(
+            IFeesDepositor(payable(_proxyCalculatedAddress)).minDepositAmount() == minDepositAmount,
+            "minDepositAmount mismatch"
+        );
+        require(IFeesDepositor(payable(_proxyCalculatedAddress)).l2Recipient() == l2Recipient, "l2Recipient mismatch");
+        require(IFeesDepositor(payable(_proxyCalculatedAddress)).gasLimit() == gasLimit, "gasLimit mismatch");
+    }
 
     /// @notice Override to return a list of addresses that should not be checked for code length.
     function _getCodeExceptions() internal view virtual override returns (address[] memory) {
