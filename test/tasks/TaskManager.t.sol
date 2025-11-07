9f68ea36b8345f6b05a221e6f82728a07dfded19
Wazabie <48911235+Wazabie@users.noreply.github.com>
2025-11-07 01:07:23 +0100

    U17 OP, Soneium, Ink Mainnet (#1268)
    * U17 OP, Sony, Ink Mainnet
    
    * remove files temporarily
    
    * Re add initial files
    
    * Update .env
    
    * Update .env
    
    * Update .env
    
    * Comment fork block number of old tasks
    
    * Update fork number
    
    * Revert "Update fork number"
    
    This reverts commit 2a2318c35b7a685f951a8bdfa2692ac18948493a.
    
    * Revert "Comment fork block number of old tasks"
    
    This reverts commit 96199fada775a0fa9f6ea835020119174b06a13c.
    
    * Update .env
    
    * Adding fork block number
    
    * fix stack-simulation
    
    * fix stack-simulation
    
    * Update prestate
    
    * Update with correct prestate and opcm
    
    * Set fork block numbers back
    
    * Revert "Set fork block numbers back"
    
    This reverts commit f6020e59d54ff0129fb0b5d53f48993ad556db43.
    
    * removing test
    
    ---------
    
    Co-authored-by: Michael Amadi <amadimichaeld@gmail.com>
    Co-authored-by: soyboy <soyboy@oplabs.co>

--- test/tasks/TaskManager.t.sol
+++ test/tasks/TaskManager.t.sol
@@ -45,9 +45,8 @@ contract TaskManagerUnitTest is StateOverrideManager, Test {
         assertEq(vm.envString("TENDERLY_GAS"), "16000000");
 
         // Test loading multiple environment variables including FORK_BLOCK_NUMBER
-        tm.setEnv("./src/tasks/eth/022-U16a-opcm-upgrade-v410-base/");
-        assertEq(vm.envString("TENDERLY_GAS"), "15000000");
-        assertEq(vm.envString("FORK_BLOCK_NUMBER"), "23491327");
+        tm.setEnv("./src/tasks/eth/032-U17-main-op-sony-ink/");
+        assertEq(vm.envString("TENDERLY_GAS"), "30000000");
     }
 
     function createStateDiff(address who, bytes32 slot, bytes32 oldValue, bytes32 newValue)
