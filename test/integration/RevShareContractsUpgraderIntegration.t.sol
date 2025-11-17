0eafac6af65358d75cf338a2e628b716a3b7552e
0xchin <77933451+0xChin@users.noreply.github.com>
2025-11-17 17:29:36 -0300

    test: add disbursement with assertions

--- test/integration/RevShareContractsUpgraderIntegration.t.sol
+++ test/integration/RevShareContractsUpgraderIntegration.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.15;
 import {RevShareContractsUpgrader} from "src/RevShareContractsUpgrader.sol";
 import {RevShareUpgradeAndSetup} from "src/template/RevShareUpgradeAndSetup.sol";
 import {IntegrationBase} from "./IntegrationBase.t.sol";
+import {Predeploys} from "@eth-optimism-bedrock/src/libraries/Predeploys.sol";
 import {Test} from "forge-std/Test.sol";
 
 // Interfaces
@@ -18,6 +19,9 @@ import {ISuperchainRevSharesCalculator} from "src/interfaces/ISuperchainRevShare
 contract RevShareContractsUpgraderIntegrationTest is IntegrationBase {
     RevShareContractsUpgrader public revShareUpgrader;
     RevShareUpgradeAndSetup public revShareTask;
+
+    // Events for testing
+    event WithdrawalInitiated(address indexed recipient, uint256 amount);
 
     // Fork IDs
     uint256 internal _mainnetForkId;
@@ -124,13 +128,36 @@ contract RevShareContractsUpgraderIntegrationTest is IntegrationBase {
         _fundVaults(1 ether, _inkMainnetForkId);
 
         // Disburse fees in both chains and expect the L1Withdrawer to trigger the withdrawal
+        // Expected L1Withdrawer share = 3 ether * 15% = 0.45 ether
+        // For details on the rev share calculation, check the SuperchainRevSharesCalculator contract.
+        // https://github.com/ethereum-optimism/optimism/blob/f392d4b7e8bc5d1c8d38fcf19c8848764f8bee3b/packages/contracts-bedrock/src/L2/SuperchainRevSharesCalculator.sol#L67-L101
+        uint256 expectedWithdrawalAmount = 0.45 ether;
+
         vm.selectFork(_opMainnetForkId);
         vm.warp(block.timestamp + IFeeSplitter(FEE_SPLITTER).feeDisbursementInterval() + 1);
+
+        uint256 balanceBefore = Predeploys.L2_TO_L1_MESSAGE_PASSER.balance;
+
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawalInitiated(OP_L1_WITHDRAWAL_RECIPIENT, expectedWithdrawalAmount);
         IFeeSplitter(FEE_SPLITTER).disburseFees();
+
+        uint256 balanceAfter = Predeploys.L2_TO_L1_MESSAGE_PASSER.balance;
+
+        assertEq(balanceAfter - balanceBefore, expectedWithdrawalAmount);
 
         vm.selectFork(_inkMainnetForkId);
         vm.warp(block.timestamp + IFeeSplitter(FEE_SPLITTER).feeDisbursementInterval() + 1);
+
+        balanceBefore = Predeploys.L2_TO_L1_MESSAGE_PASSER.balance;
+
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawalInitiated(INK_L1_WITHDRAWAL_RECIPIENT, expectedWithdrawalAmount);
         IFeeSplitter(FEE_SPLITTER).disburseFees();
+
+        balanceAfter = Predeploys.L2_TO_L1_MESSAGE_PASSER.balance;
+
+        assertEq(balanceAfter - balanceBefore, expectedWithdrawalAmount);
     }
 
     function _fundVaults(uint256 _amount, uint256 _forkId) internal {
